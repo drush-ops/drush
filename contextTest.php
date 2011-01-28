4c19531839c65fc932338ab5e72b58971c2eda47
Moshe Weitzman <weitzman@cyrve.com>
2011-01-28 00:20:58 -0500

    New tests for context.inc. Tests the loading of config files and the context hierarchy arbitration.

---
+++ contextTest.php
@@ -0,0 +1,185 @@
+<?php
+
+/*
+* @file
+*  Assure that context API behaves as designed. Mostly implicitly tested, but we
+*  do have some edges that need explicit testing.
+*
+*  @see drush/includes/context.inc.
+*/
+
+class contextCase extends Drush_TestCase {
+  
+  function __construct() {
+    $this->env = 'dev';
+    // $this->sites[$this->env]['root']
+    $this->root = UNISH_SANDBOX . '/web';
+    $this->site = $this->root . '/sites/' . $this->env;
+    $this->home = $this->drush_server_home();
+    $this->paths = array(
+      'custom' => UNISH_SANDBOX,
+      'site' =>  $this->site,
+      'drupal' => $this->root,
+      'user' => $this->home,
+      'home.drush' => $this->home . '/.drush',
+      'system' => '/etc/drush',
+      'drush' => dirname(UNISH_DRUSH),
+    );
+  }
+  
+  /*
+   * Try to write a tiny drushrc.php to each place that drush checks. Also
+   * write a sites/dev/aliases.drushrc.php file to the sandbox.
+   * 
+   * @todo Weight benefit of vfsStream versus adding another dependency. See
+   * http://www.phpunit.de/manual/3.5/en/test-doubles.html#test-doubles.mocking-the-filesystem
+   */
+  function setup() {
+    parent::setUp();
+    
+    $this->setUpDrupal('dev', FALSE);
+    
+  
+    foreach ($this->paths as $key => $path) {
+      // Only declare harmless options as these files hang around until shutdown.
+      $contents = "<?php
+// Written by Drush's contextCase::setup(). This file is safe to delete.
+
+\$options['contextConfig'] = $key;
+\$command_specific['unit-eval']['contextConfig'] = $key . '-specific';
+";
+      $md5 = md5($contents);
+      $path .= $key == 'user' ? '/.drushrc.php' : '/drushrc.php';
+      if (file_exists($path)) {
+        $this->exists[$key] = $path;
+        if (md5(file_get_contents($path)) == $md5) {
+          register_shutdown_function(array($this, 'file_delete_recursive'), $path);
+        }
+      }
+      elseif (is_writable(dirname($path))) {
+        if (file_put_contents($path, $contents)) {
+          $this->exists[$key] = $path;
+          register_shutdown_function(array($this, 'file_delete_recursive'), $path);
+        }
+      }
+      else {
+        // @todo Warn that some locations are not getting tested.
+      }
+    }
+    
+    // Also write a site alias so we can test its supremacy in context hierarchy.
+    $path = $this->site . '/aliases.drushrc.php';
+    $alias = array(
+      'contextConfig' => 'alias1',
+      'command-specific' => array (
+        'unit-eval' => array (
+          'contextConfig' => 'alias-specific',
+        ),
+      ),
+    );
+    $contents = "<?php\n\$aliases['contextAlias'] = " . var_export($alias, TRUE) . ';';
+    $return = file_put_contents($path, $contents);
+    register_shutdown_function(array($this, 'file_delete_recursive'), $path);
+  }
+  
+  /*
+   * These should be two different tests but I could not work out how to do that
+   * without calling setup() twice. setupBeforeClass() did not work out (for MW).
+   */ 
+  function testContext() {
+    $this->ConfigFile();
+    $this->ContextHierarchy();
+  }
+  
+  /*
+   * Assure that all possible config files get loaded.
+   */ 
+  function ConfigFile() {
+    $options = array(
+      'pipe' => NULL,
+      'config' => UNISH_SANDBOX,
+      'root' => $this->root,
+      'uri' => $this->env,
+    );
+    $this->drush('core-status', array('Drush configuration'), $options);
+    $output = trim($this->getOutput());
+    $loaded = explode(' ', $output);
+    $this->assertSame(array_values($this->exists), $loaded);
+  }
+
+  /*
+   * Assure that options are loaded into right context and hierarchy is
+   * respected by drush_get_option().
+   *
+   * Stdin context not exercised here. See targetCase::testTarget().
+   */ 
+  function ContextHierarchy() {
+    // The 'custom' config file has higher priority than cli and config files.
+    $eval =  '$contextConfig = drush_get_option("contextConfig", "n/a");';
+    $eval .= '$cli1 = drush_get_option("cli1");';
+    // $eval .= '$cli2 = drush_get_option("cli2");';
+    $eval .= 'print json_encode(get_defined_vars());';
+    $config = UNISH_SANDBOX . '/drushrc.php';
+    $options = array(
+      'cli1' => NULL,
+      // Negation work working. Perhaps can't create and negate on same cli.
+      // 'no-cli2' => NULL,
+      // 'cli2' => NULL,
+      'config' => $config,
+      'root' => $this->root,
+      'uri' => $this->env,
+    );
+    $this->drush('php-eval', array($eval), $options);
+    $output = $this->getOutput();
+    $actuals = json_decode(trim($output));
+    $this->assertEquals('custom', $actuals->contextConfig);
+    $this->assertTrue($actuals->cli1);
+    
+    // Site alias trumps 'custom'.
+    $eval =  '$contextConfig = drush_get_option("contextConfig", "n/a");';
+    $eval .= 'print json_encode(get_defined_vars());';
+    $options = array(
+      'config' => $config,
+      'root' => $this->root,
+      'uri' => $this->env,
+    );
+    $this->drush('php-eval', array($eval), $options, '@contextAlias');
+    $output = $this->getOutput();
+    $actuals = json_decode(trim($output));
+    $this->assertEquals('alias1', $actuals->contextConfig);
+    
+    // Command specific wins over non-specific. Note we call unit-eval command
+    // in order not to purturb php-eval with options in config file.
+    $eval =  '$contextConfig = drush_get_option("contextConfig", "n/a");';
+    $eval .= 'print json_encode(get_defined_vars());';
+    $options = array(
+      'root' => $this->root,
+      'uri' => $this->env,
+      'config' => $config,
+      'include' => dirname(__FILE__),
+    );
+    $this->drush('unit-eval', array($eval), $options);
+    $output = $this->getOutput();
+    $actuals = json_decode(trim($output));
+    // @todo: why is custom-specific not winning here. bug?
+    $this->assertEquals('site-specific', $actuals->contextConfig);
+  }
+  
+  /**
+   * Return the user's home directory.
+   *
+   * A copy of drush's own drush_server_home().
+   */
+  function drush_server_home() {
+    $home = NULL;
+    // $_SERVER['HOME'] isn't set on windows and generates a Notice.
+    if (!empty($_SERVER['HOME'])) {
+      $home = $_SERVER['HOME'];
+    }
+    elseif (!empty($_SERVER['HOMEDRIVE']) && !empty($_SERVER['HOMEPATH'])) {
+      // home on windows
+      $home = $_SERVER['HOMEDRIVE'] . $_SERVER['HOMEPATH'];
+    }
+    return $home;
+  }
+}
