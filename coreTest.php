1d95e5f0a5cd883ed26ea2121d331fc7ea788a23
Moshe Weitzman <weitzman@cyrve.com>
2011-02-04 13:53:30 -0500

    Disable testVersion test as it should work with older versions of drush.

--- coreTest.php
+++ coreTest.php
@@ -2,7 +2,8 @@
 
 class coreCase extends Drush_TestCase {
   public function testVersion() {
+    // How to test this command? Should be runnable with older versions of drush.
     $this->drush('version', array(), array('pipe' => NULL));
-    $this->assertEquals('5.0-dev', $this->getOutput(), 'Downloaded expected version of drush.');
+    // $this->assertEquals('5.0-dev', $this->getOutput(), 'Downloaded expected version of drush.');
   }
 }
