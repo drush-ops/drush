f8404e2c5f78dc0c0aa245624bbc8bc6a2d94f63
Moshe Weitzman <weitzman@cyrve.com>
2011-02-06 23:28:54 -0500

    Assure that standalone drush scripts work as designed.

--- coreTest.php
+++ coreTest.php
@@ -1,9 +1,20 @@
 <?php
 
+/*
+ * @file
+ *   Tests for core commands.
+ */
 class coreCase extends Drush_TestCase {
-  public function testVersion() {
-    // How to test this command? Should be runnable with older versions of drush.
-    $this->drush('version', array(), array('pipe' => NULL));
-    // $this->assertEquals('5.0-dev', $this->getOutput(), 'Downloaded expected version of drush.');
+
+  /*
+   * Test standalone php-script scripts. Assure that script args and options work.
+   */
+  public function testStandaloneScript() {
+    $this->drush('version', array('drush_version'), array('pipe' => NULL));
+    $standard = $this->getOutput();
+
+    $this->execute(dirname(__FILE__) . '/hellounish.script drush_version --pipe');
+    $standalone = $this->getOutput();
+    $this->assertEquals($standard, $standalone);
   }
 }
