0f61e3b2a0ea9ff6a4c67e5609f38e1788071532
Moshe Weitzman <weitzman@tejasa.com>
2011-03-24 12:46:58 -0400

    Issue #1103568 by moshe weitzman: Fixed Environment pollution in the unit tests.

--- tests/contextTest.php
+++ tests/contextTest.php
@@ -11,45 +11,37 @@
 class contextCase extends Drush_TestCase {
 
   function setUpPaths() {
-    $this->env = 'dev';
-    // $this->sites[$this->env]['root']
-    $this->root = UNISH_SANDBOX . '/web';
+    $this->root = $this->sites[$this->env]['root'];
     $this->site = $this->root . '/sites/' . $this->env;
-    $this->home = $this->drush_server_home();
+    $this->home = UNISH_SANDBOX . '/home';
     $this->paths = array(
       'custom' => UNISH_SANDBOX,
       'site' =>  $this->site,
       'drupal' => $this->root,
       'user' => $this->home,
       'home.drush' => $this->home . '/.drush',
-      'system' => '/etc/drush',
-      'drush' => dirname(realpath(UNISH_DRUSH)),
+      'system' => UNISH_SANDBOX . '/etc/drush',
+      // We don't want to write a file into drush dir since it is not in the sandbox.
+      // 'drush' => dirname(realpath(UNISH_DRUSH)),
     );
     // Run each path through realpath() since the paths we'll compare against
     // will have already run through drush_load_config_file().
     foreach ($this->paths as $key => $path) $this->paths[$key] = realpath($path);
-
-    $this->paths_delete_candidates = array('user', 'home.drush', 'system', 'drush');
   }
 
   /*
    * Try to write a tiny drushrc.php to each place that drush checks. Also
    * write a sites/dev/aliases.drushrc.php file to the sandbox.
-   *
-   * @todo Weight benefit of vfsStream versus adding another dependency. See
-   * http://www.phpunit.de/manual/3.5/en/test-doubles.html#test-doubles.mocking-the-filesystem
    */
   function setup() {
     parent::setUp();
 
-    $this->setUpPaths();
+    $this->env = 'dev';
     $this->setUpDrupal($this->env, FALSE);
-    $root = $this->sites[$this->env]['root'];
-    $site = "$root/sites/$this->env";
+    $this->setUpPaths();
 
-
+    // These files are only written to sandbox so get automatically cleaned up.
     foreach ($this->paths as $key => $path) {
-      // Only declare harmless options as these files hang around until shutdown.
       $contents = <<<EOD
 <?php
 // Written by Drush's contextCase::setup(). This file is safe to delete.
@@ -59,22 +51,8 @@ class contextCase extends Drush_TestCase {
 
 EOD;
       $path .= $key == 'user' ? '/.drushrc.php' : '/drushrc.php';
-      if (file_exists($path)) {
-        $this->exists[$key] = $path;
-        if ($this->should_delete($path, $contents, $key)) {
-          register_shutdown_function('unlink', $path);
-        }
-      }
-      elseif (is_writable(dirname($path))) {
-        if (file_put_contents($path, $contents)) {
-          $this->exists[$key] = $path;
-          if ($this->should_delete($path, $contents, $key)) {
-            register_shutdown_function('unlink', $path);
-          }
-        }
-      }
-      else {
-        // @todo Unwritable. Warn that some locations are not getting tested.
+      if (file_put_contents($path, $contents)) {
+        $this->written[] = $path;
       }
     }
 
@@ -89,7 +67,6 @@ EOD;
       ),
     );
     $contents = $this->file_aliases($aliases);
-    // This file is in the sandbox so gets deleted at end/start of a test run.
     $return = file_put_contents($path, $contents);
   }
 
@@ -115,17 +92,17 @@ EOD;
     $this->drush('core-status', array('Drush configuration'), $options);
     $output = trim($this->getOutput());
     $loaded = explode(' ', $output);
-    $this->assertSame(array_values($this->exists), $loaded);
+    $this->assertSame($this->written, $loaded);
   }
 
   /*
    * Assure that options are loaded into right context and hierarchy is
    * respected by drush_get_option().
    *
-   * Stdin context not exercised here. See targetCase::testTarget().
+   * Stdin context not exercised here. See backendCase::testTarget().
    */
   function ContextHierarchy() {
-    // The 'custom' config file has higher priority than cli and config files.
+    // The 'custom' config file has higher priority than cli and regular config files.
     $eval =  '$contextConfig = drush_get_option("contextConfig", "n/a");';
     $eval .= '$cli1 = drush_get_option("cli1");';
     $eval .= 'print json_encode(get_defined_vars());';
@@ -155,41 +132,19 @@ EOD;
     $actuals = json_decode(trim($output));
     $this->assertEquals('alias1', $actuals->contextConfig);
 
-    // Command specific wins over non-specific. Note we call unit-eval command
-    // in order not to purturb php-eval with options in config file.
+    // Command specific wins over non-specific. If it did not, $expected would
+    // be 'site'. Note we call unit-eval command in order not to purturb
+    // php-eval with options in config file.
     $eval =  '$contextConfig = drush_get_option("contextConfig", "n/a");';
     $eval .= 'print json_encode(get_defined_vars());';
     $options = array(
       'root' => $this->root,
       'uri' => $this->env,
-      'config' => $config,
-      'include' => dirname(__FILE__),
+      'include' => dirname(__FILE__), // Find unit.drush.inc commandfile.
     );
     $this->drush('unit-eval', array($eval), $options);
     $output = $this->getOutput();
     $actuals = json_decode(trim($output));
-    $this->assertEquals('custom-specific', $actuals->contextConfig);
-  }
-
-  /**
-   * Return the user's home directory.
-   *
-   * A copy of drush's own drush_server_home().
-   */
-  function drush_server_home() {
-    $home = NULL;
-    // $_SERVER['HOME'] isn't set on windows and generates a Notice.
-    if (!empty($_SERVER['HOME'])) {
-      $home = $_SERVER['HOME'];
-    }
-    elseif (!empty($_SERVER['HOMEDRIVE']) && !empty($_SERVER['HOMEPATH'])) {
-      // home on windows
-      $home = $_SERVER['HOMEDRIVE'] . $_SERVER['HOMEPATH'];
-    }
-    return $home;
-  }
-
-  function should_delete($path, $contents, $key) {
-    return in_array($key, $this->paths_delete_candidates) && file_get_contents($path) == $contents;
+    $this->assertEquals('site-specific', $actuals->contextConfig);
   }
 }
