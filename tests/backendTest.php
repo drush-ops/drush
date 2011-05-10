adb729687399e10488ad39fdbba2d8aa5515dad8
Greg Anderson <greg.1.anderson@greenknowe.org>
2011-05-09 23:15:30 -0700

    #1151464 by greg.1.anderson: escape parameters in drush tests

--- tests/backendTest.php
+++ tests/backendTest.php
@@ -24,7 +24,7 @@ class backendCase extends Drush_TestCase {
    * General handling of site aliases will be in sitealiasTest.php.
    */
   function testOrigin() {
-    $exec = sprintf('%s %s version --simulate --ssh-options=%s | grep ssh', escapeshellcmd(UNISH_DRUSH), escapeshellarg('user@server/path/to/drupal#sitename'), escapeshellarg('-i mysite_dsa'));
+    $exec = sprintf('%s %s version --simulate --ssh-options=%s | grep ssh', Drush_TestCase::unish_escapeshellarg(UNISH_DRUSH), Drush_TestCase::unish_escapeshellarg('user@server/path/to/drupal#sitename'), Drush_TestCase::unish_escapeshellarg('-i mysite_dsa'));
     $this->execute($exec);
     // $expected might be different on non unix platforms. We shall see.
     $expected = "Simulating backend invoke: ssh -i mysite_dsa 'user'@'server' 'drush  --uri='\''sitename'\'' --root='\''/path/to/drupal'\'' --simulate version 2>&1'  > `tty` 2>&1";
@@ -41,7 +41,7 @@ class backendCase extends Drush_TestCase {
   */
   function testTarget() {
     $stdin = json_encode(array('filter'=>'sql'));
-    $exec = sprintf('echo %s | %s help --backend', escapeshellarg($stdin), escapeshellcmd(UNISH_DRUSH));
+    $exec = sprintf('echo %s | %s help --backend', Drush_TestCase::unish_escapeshellarg($stdin), Drush_TestCase::unish_escapeshellarg(UNISH_DRUSH));
     $this->execute($exec);
     $parsed = $this->parse($this->getOutput());
     $this->assertTrue((bool) $parsed, 'Successfully parsed backend output');
@@ -54,7 +54,7 @@ class backendCase extends Drush_TestCase {
     $this->assertEquals('Bootstrap to phase 0.', $parsed['log'][0]['message']);
 
     // Check error propogation by requesting an invalid command (missing Drupal site).
-    $exec = sprintf('%s core-cron --backend', escapeshellcmd(UNISH_DRUSH));
+    $exec = sprintf('%s core-cron --backend', Drush_TestCase::unish_escapeshellarg(UNISH_DRUSH));
     $this->execute($exec, self::EXIT_ERROR);
     $parsed = $this->parse($this->getOutput());
     $this->assertEquals(1, $parsed['error_status']);
