eaf2448f80fea8f84393376e21fa038bc7cf4ae5
Moshe Weitzman <weitzman@tejasa.com>
2011-07-14 23:18:04 -0400

    Move variable tests to own file.

--- tests/pmEnDisUnListTest.php
+++ tests/pmEnDisUnListTest.php
@@ -2,11 +2,12 @@
 
 /*
  * @file
- *   Tests for enable, disable, uninstall, pm-list, and variable-* commands.
+ *   Tests for enable, disable, uninstall, pm-list commands.
  */
-class EnDisUnListVarCase extends Drush_TestCase {
 
-  public function testEnDisUnListVar() {
+class EnDisUnListCase extends Drush_TestCase {
+
+  public function testEnDisUnList() {
     $this->setUpDrupal('dev', TRUE);
     $options = array(
       'yes' => NULL,
@@ -25,24 +26,12 @@ class EnDisUnListVarCase extends Drush_TestCase {
     $this->assertTrue(in_array('devel', $list));
     $this->assertTrue(in_array('bartik', $list), 'Themes are in the pm-list');
 
-    // A brief detour to test variable-*.
-    $this->drush('variable-set', array('devel_query_display', TRUE), $options);
-    $this->drush('variable-get', array('devel'), $options); // Wildcard get.
-    $var_export = $this->getOutput();
-    eval($var_export);
-    $this->assertEquals('1', $variables['devel_query_display'], 'Variable was successfully set and get.');
-    $this->drush('variable-set', array('site_name', 'unish'), $options + array('always-set' => NULL));
-    $this->drush('variable-get', array('site_name'), $options);
-    $var_export = $this->getOutput();
-    eval($var_export);
-    $this->assertEquals('unish', $variables['site_name'], '--always-set option works as expected.');
-    $this->drush('variable-delete', array('site_name'), $options);
-    $output = $this->getOutput();
-    $this->assertEmpty($output, 'Variable was successfully deleted.');
-
     $this->drush('pm-list', array(), $options + array('package' => 'Core'));
     $list = $this->getOutputAsList();
     $this->assertFalse(in_array('devel', $list), 'Devel is not part of core package');
+
+    // For testing uninstall later.
+    $this->drush('variable-set', array('devel_query_display', 1), $options);
 
     $this->drush('pm-disable', array('devel'), $options);
     $this->drush('pm-list', array(), $options + array('status' => 'disabled'));
@@ -53,6 +42,7 @@ class EnDisUnListVarCase extends Drush_TestCase {
     $this->drush('pm-list', array(), $options + array('status' => 'not installed', 'type' => 'module'));
     $list = $this->getOutputAsList();
     $this->assertTrue(in_array('devel', $list));
+
 
     // We expect an exit code of 1 so just call execute() directly.
     $exec = sprintf('%s variable-get %s --pipe --root=%s --uri=%s', UNISH_DRUSH, 'devel_query_display', $options['root'], $options['uri']);
