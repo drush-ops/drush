63c3212fc91f97c114f1ab92a76dd9b08d79948f
greg1anderson <greg1anderson@438598.no-reply.drupal.org>
2011-09-07 22:31:21 -0400

    Issue #1255544 by greg.1.anderson, grayside: Added Read drush configuration file from a 'drush' folder at root of git repository.
    Conflicts:
    
    	examples/example.drushrc.php

--- examples/example.drushrc.php
+++ examples/example.drushrc.php
@@ -239,3 +239,14 @@ $options['skip-tables'] = array(
 #   'theme_default' => 'minnelli',
 #   'anonymous' => 'Visitor',
 # );
+
+// Load a drushrc.php file from the 'drush' folder at the root
+// of the current git repository. Customize as desired.
+// (Script by grayside; @see: http://grayside.org/node/93)
+#exec('git rev-parse --git-dir 2> /dev/null', $output);
+#
+#if (!empty($output)) {
+#  $repo = $output[0];
+#  $options['config'] = $repo . '/../drush/drushrc.php';
+#  $options['include'] = $repo . '/../drush';
+#}
