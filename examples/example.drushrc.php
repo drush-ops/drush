1adf73a7d7fc3048bf771cdbd7633101c201cc74
greg1anderson <greg1anderson@438598.no-reply.drupal.org>
2011-09-07 22:31:21 -0400

    Issue #1255544 by greg.1.anderson, grayside: Added Read drush configuration file from a 'drush' folder at root of git repository.

--- examples/example.drushrc.php
+++ examples/example.drushrc.php
@@ -230,3 +230,14 @@
 // you can use the regular shell command by typing in the full path
 // to the command (e.g. /bin/grep).
 # $command_specific['core-cli'] = array('override' => 'help,dd,sa');
+
+// Load a drushrc.php file from the 'drush' folder at the root
+// of the current git repository. Customize as desired.
+// (Script by grayside; @see: http://grayside.org/node/93)
+#exec('git rev-parse --git-dir 2> /dev/null', $output);
+#
+#if (!empty($output)) {
+#  $repo = $output[0];
+#  $options['config'] = $repo . '/../drush/drushrc.php';
+#  $options['include'] = $repo . '/../drush';
+#}
