89fac3dafe88283a1ea7c74d41142afedd482a01
Greg Anderson <greg.1.anderson@greenknowe.org>
2011-11-06 19:51:43 -0800

    #1332818 by moshe and greg.1.anderson: make shell aliases pass options through, and adjustments for remote alias handling

--- tests/shellAliasTest.php
+++ tests/shellAliasTest.php
@@ -6,21 +6,71 @@
  */
 class shellAliasesCase extends Drush_CommandTestCase {
 
-  /*
-   * Assure that shell aliases expand as expected.
+  /**
+   * Write a config file that contains the shell-aliases array.
    */
-  public function testShellAliases() {
+  static function setupBeforeClass() {
+    parent::setUpBeforeClass();
     $contents = "
       <?php
 
-      \$options['shell-aliases'] = array('glopts' => 'topic core-global-options');
+      \$options['shell-aliases'] = array(
+        'glopts' => 'topic core-global-options',
+        'pull' => '!git pull',
+      );
     ";
     file_put_contents(UNISH_SANDBOX . '/drushrc.php', $contents);
+  }
+
+  /*
+   * Test shell aliases to Drush commands.
+   */
+  public function testShellAliasDrushLocal() {
     $options = array(
       'config' => UNISH_SANDBOX,
     );
     $this->drush('glopts', array('php_configuration'), $options);
     $output = $this->getOutput();
-    $this->assertContains('These options are applicable to most drush commands.', $output);
+    $this->assertContains('These options are applicable to most drush commands.', $output, 'Successfully executed local shell alias to drush command');
+  }
+
+  /*
+   * Test shell aliases to Bash commands. Assure we pass along extra arguments
+   * and options.
+   */
+  public function testShellAliasBashLocal() {
+    $options = array(
+      'config' => UNISH_SANDBOX,
+      'simulate' => NULL,
+      'rebase' => NULL,
+    );
+    $this->drush('pull', array('origin'), $options);
+    $output = $this->getOutput();
+    $this->assertContains('Calling system(git pull origin --rebase);', $output);
+  }
+
+  public function testShellAliasDrushRemote() {
+    $options = array(
+      'config' => UNISH_SANDBOX,
+      'simulate' => NULL,
+    );
+    $this->drush('glopts', array('php_configuration'), $options, 'user@server/path/to/drupal#sitename');
+    // $expected might be different on non unix platforms. We shall see.
+    $expected = "Simulating backend invoke: ssh user@server 'drush --simulate --uri=sitename --root=/path/to/drupal topic core-global-options --invoke 2>&1' 2>&1";
+    $output = $this->getOutput();
+    $this->assertEquals($expected, $output, 'Expected remote shell alias to a drush command was built');
+  }
+
+  public function testShellAliasBashRemote() {
+    $options = array(
+      'config' => UNISH_SANDBOX,
+      'simulate' => NULL,
+      'rebase' => NULL,
+    );
+    $this->drush('pull', array('origin'), $options, 'user@server/path/to/drupal#sitename');
+    // $expected might be different on non unix platforms. We shall see.
+    $expected = "Calling proc_open(ssh user@server \"cd '/path/to/drupal' && git pull origin --rebase\");";
+    $output = $this->getOutput();
+    $this->assertEquals($expected, $output, 'Expected remote shell alias to a bash command was built');
   }
 }
