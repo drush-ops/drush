9a111cf900df6d6e7f93fa802ff6150924e0302c
Moshe Weitzman <weitzman@tejasa.com>
2011-11-15 22:41:37 -0500

    Add more tests for --select.

--- tests/pmDownloadTest.php
+++ tests/pmDownloadTest.php
@@ -12,7 +12,7 @@ class pmDownloadCase extends Drush_CommandTestCase {
   // @todo Test pure drush commandfile projects. They get special destination.
   public function testDestination() {
     // Setup two Drupal sites. Skip install for speed.
-    $sites = $this->setUpDrupal(2);
+    $sites = $this->setUpDrupal(2, FALSE);
     $uri = key($sites);
     $root = $this->webroot();
 
@@ -27,10 +27,30 @@ class pmDownloadCase extends Drush_CommandTestCase {
     $this->drush('pm-download', array('devel'), $options);
     $this->assertFileExists($root . '/sites/all/modules/devel/README.txt');
 
+    //  --use-site-dir
+    $this->drush('pm-download', array('devel'), $options + array('use-site-dir' => NULL));
+    $this->assertFileExists("$root/sites/$uri/modules/devel/README.txt");
+    unish_file_delete_recursive("$root/sites/$uri/modules/devel");
+
     // If we are in site specific dir, then download belongs there.
     $path_stage = "$root/sites/$uri";
-    mkdir("$path_stage/modules");
+    // gets created by --use-site-dir above,
+    // mkdir("$path_stage/modules");
     $this->drush('pm-download', array('devel'), array('cache' => NULL, 'skip' => NULL, 'invoke' => NULL), NULL, $path_stage);
     $this->assertFileExists($path_stage . '/modules/devel/README.txt');
+
+   // --select. Specify 6.x since that has so many releases.
+    $this->drush('pm-download', array('devel-6.x'), array('cache' => NULL, 'no' => NULL, 'select' => NULL));
+    $items = $this->getOutputAsList();
+    $output = $this->getOutput();
+    $this->assertLessThanOrEqual(7, count($items), '--select offerred no more than 3 options.');
+    $this->assertContains('dev', $output, 'Dev release was shown by --select.');
+
+    // --select --all. Specify 6.x since that has so many releases.
+    $this->drush('pm-download', array('devel-6.x'), array('cache' => NULL, 'no' => NULL, 'all' => NULL, 'select' => NULL));
+    $items = $this->getOutputAsList();
+    $output = $this->getOutput();
+    $this->assertGreaterThanOrEqual(20, count($items), '--select --all offerred at least 16 options.');
+    $this->assertContains('6.x-1.5', $output, 'Assure that --all lists very old releases.');
   }
 }
