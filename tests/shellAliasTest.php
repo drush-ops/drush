63b2a4e09162c0b58d4fc484ca7a896bdd16b18c
Greg Anderson <greg.1.anderson@greenknowe.org>
2011-11-17 10:32:59 -0800

    #1342940 by greg.1.anderson: Support replacements in shell alias

--- tests/shellAliasTest.php
+++ tests/shellAliasTest.php
@@ -17,9 +17,23 @@ class shellAliasesCase extends Drush_CommandTestCase {
       \$options['shell-aliases'] = array(
         'glopts' => 'topic core-global-options',
         'pull' => '!git pull',
+        'echotest' => '!echo {{@target}} {{%root}} {{%mypath}}',
       );
     ";
     file_put_contents(UNISH_SANDBOX . '/drushrc.php', trim($contents));
+    $contents = "
+      <?php
+
+      \$aliases['myalias'] = array (
+        'root' => '/path/to/drupal',
+        'uri' => 'mysite.org',
+        '#peer' => '@live',
+        'path-aliases' => array (
+          '%mypath' => '/srv/data/mypath',
+        ),
+      );
+    ";
+    file_put_contents(UNISH_SANDBOX . '/aliases.drushrc.php', trim($contents));
   }
 
   /*
@@ -46,7 +60,7 @@ class shellAliasesCase extends Drush_CommandTestCase {
     );
     $this->drush('pull', array('origin'), $options);
     $output = $this->getOutput();
-    $this->assertContains('Calling system(git pull origin --rebase);', $output);
+    $this->assertContains('Calling proc_open(git pull origin --rebase);', $output);
   }
 
   public function testShellAliasDrushRemote() {
@@ -74,5 +88,32 @@ class shellAliasesCase extends Drush_CommandTestCase {
     $expected = "Calling proc_open(ssh  user@server 'cd /path/to/drupal && git pull origin --rebase');";
     $output = $this->getOutput();
     $this->assertEquals($expected, $output, 'Expected remote shell alias to a bash command was built');
+  }
+  
+  /*
+   * Test shell aliases with replacements -- no alias.
+   */
+  public function testShellAliasReplacementNoAlias() {
+    $options = array(
+      'config' => UNISH_SANDBOX,
+    );
+    $this->drush('echotest', array(), $options);
+    $expected = "@none";
+    $output = $this->getOutput();
+    $this->assertEquals($expected, $output, 'Expected echo test returned "@none"');
+  }
+  
+  /*
+   * Test shell aliases with replacements -- alias.
+   */
+  public function testShellAliasReplacementWithAlias() {
+    $options = array(
+      'config' => UNISH_SANDBOX,
+      'alias-path' => UNISH_SANDBOX,
+    );
+    $this->drush('echotest', array(), $options, '@myalias');
+    $expected = "@myalias /path/to/drupal /srv/data/mypath";
+    $output = $this->getOutput();
+    $this->assertEquals($expected, $output, 'Expected echo test returned "' . $expected . '"');
   }
 }
