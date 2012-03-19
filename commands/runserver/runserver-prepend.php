0967287d2fb057765e5455aac69690ca3e12238d
Owen Barton <drupal@owenbarton.com>
2012-03-19 17:10:07 -0600

    Various improvements to runserver, now can use the php 5.4 built in web server (in addition to the cgi based one), fixed and streamlined user/conf passing and watchdog logging.

--- commands/runserver/runserver-prepend.php
+++ commands/runserver/runserver-prepend.php
@@ -3,22 +3,23 @@
 // We set the base_url so that Drupal generates correct URLs for runserver
 // (e.g. http://127.0.0.1:8888/...), but can still select and serve a specific
 // site in a multisite configuration (e.g. http://mysite.com/...).
-$base_url = $_SERVER['RUNSERVER_BASE_URL'];
+$base_url = runserver_env('RUNSERVER_BASE_URL');
 
-// We hijack system_boot (which core system module does not implement) as
-// a convenient place to affect mid-bootstrap changes.
-if (!function_exists('system_boot')) {
+// We hijack filter_init (which core filter module does not implement) as
+// a convenient place to affect early changes.
+if (!function_exists('filter_init')) {
   // Check function_exists as a safety net in case it is added in future.
-  function system_boot() {
+  function filter_init() {
     global $conf, $user;
     // Inject values into the $conf array - will apply to all sites.
     // This can be a useful place to apply generic development settings.
-    $conf_inject = unserialize(urldecode($_SERVER['RUNSERVER_CONF']));
+    $conf_inject = unserialize(urldecode(runserver_env('RUNSERVER_CONF')));
     // Merge in the injected conf, overriding existing items.
     $conf = array_merge($conf, $conf_inject);
-    if (isset($_SERVER['RUNSERVER_USER'])) {
+    $uid = runserver_env('RUNSERVER_USER');
+    if (isset($_GET['login']) && $user->uid == 0 && !empty($uid)) {
       // If a user was provided, log in as this user.
-      $user = unserialize(urldecode($_SERVER['RUNSERVER_USER']));
+      $user = user_load($uid);
       if (function_exists('drupal_session_regenerate')) {
         // Drupal 7
         drupal_session_regenerate();
@@ -36,16 +37,25 @@ if (!function_exists('system_boot')) {
 if (!function_exists('system_watchdog')) {
   // Check function_exists as a safety net in case it is added in future.
   function system_watchdog($log_entry = array()) {
-    static $logs = array();
-    $logs[] = $log_entry;
-    $data = urlencode(serialize($logs));
-    if (function_exists('drupal_add_http_header')) {
-      // Drupal 7
-      drupal_add_http_header('X-Runserver-Watchdog', $data);
-    }
-    else {
-      // Drupal 6
-      drupal_set_header('X-Runserver-Watchdog: ' . $data);
-    }
+    $message = strtr('Watchdog: !message | severity: !severity | type: !type | uid: !uid | !ip | !request_uri | !referer | !link', array(
+      '!message'     => strip_tags(!isset($log_entry['variables']) ? $log_entry['message'] : strtr($log_entry['message'], $log_entry['variables'])),
+      '!severity'    => $log_entry['severity'],
+      '!type'        => $log_entry['type'],
+      '!ip'          => $log_entry['ip'],
+      '!request_uri' => $log_entry['request_uri'],
+      '!referer'     => $log_entry['referer'],
+      '!uid'         => $log_entry['user']->uid,
+      '!link'        => strip_tags($log_entry['link']),
+    ));
+    error_log($message);
+  }
+}
+
+function runserver_env($key) {
+  if (isset($_SERVER[$key])) {
+    return $_SERVER[$key];
+  }
+  else {
+    return getenv($key);
   }
 }
