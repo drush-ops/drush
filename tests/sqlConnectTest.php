a55ed8de15de15b4fe1497c57c5b16d980110a8f
Moshe Weitzman <weitzman@tejasa.com>
2012-11-03 19:39:59 -0400

    Start supporting more DB types in sqlConnectTest

--- tests/sqlConnectTest.php
+++ tests/sqlConnectTest.php
@@ -5,7 +5,6 @@
  *   Tests sql-connect command
  *
  *   Installs Drupal and checks that the given URL by sql-connect is correct.
- *   @TODO: test Postgre-SQL and Sqlite.
  *
  * @group commands
  */
@@ -25,11 +24,19 @@ class SqlConnectCase extends Drush_CommandTestCase {
     $this->assertContains('admin', $output);
 
     // Get the connection details with sql-connect and check its structure.
-    if (strpos(UNISH_DB_URL, 'mysql') === FALSE) {
-      $this->markTestSkipped('sql-connect tests need updating for non-mysql UNISH_DB_URL.');
-    }
     $this->drush('sql-connect', array(), $options);
     $output = $this->getOutput();
-    $this->assertRegExp('/^mysql --database=[^\s]+ --host=[^\s]+ --user=[^\s]+ --password=.*$/', $output);
+    if (strpos(UNISH_DB_URL, 'mysql') !== FALSE) {
+      $this->assertRegExp('/^mysql --database=[^\s]+ --host=[^\s]+ --user=[^\s]+ --password=.*$/', $output);
+    }
+    elseif (strpos(UNISH_DB_URL, 'sqlite') !== FALSE) {
+      $this->assertContains('sqlite3', $output);
+    }
+    elseif (strpos(UNISH_DB_URL, 'pgsql') !== FALSE) {
+      $this->markTestSkipped('sql-connect test does not recognize postgres database type.');
+    }
+    else {
+      $this->markTestSkipped('sql-connect test does not recognize database type in UNISH_DB_URL.');
+    }
   }
 }
