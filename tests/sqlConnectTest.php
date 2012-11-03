cdd1ead568626f14dc9da2f1daf1745121e86aaa
Moshe Weitzman <weitzman@tejasa.com>
2012-11-03 19:08:42 -0400

    end of sql-connect test should skip if not using mysql.

--- tests/sqlConnectTest.php
+++ tests/sqlConnectTest.php
@@ -19,14 +19,17 @@ class SqlConnectCase extends Drush_CommandTestCase {
       'uri' => key($sites),
     );
 
-    // Get the connection details with sql-connect and check its structure.
-    $this->drush('sql-connect', array(), $options);
-    $output = $this->getOutput();
-    $this->assertRegExp('/^mysql --database=[^\s]+ --host=[^\s]+ --user=[^\s]+ --password=.*$/', $output);
-
     // Issue a query and check the result to verify the connection.
     $this->execute($output . ' -e "select name from users where uid = 1;"');
     $output = $this->getOutput();
     $this->assertContains('admin', $output);
+
+    // Get the connection details with sql-connect and check its structure.
+    if (strpos(UNISH_DB_URL, 'mysql') === FALSE) {
+      $this->markTestSkipped('sql-connect tests need updating for non-mysql UNISH_DB_URL.');
+    }
+    $this->drush('sql-connect', array(), $options);
+    $output = $this->getOutput();
+    $this->assertRegExp('/^mysql --database=[^\s]+ --host=[^\s]+ --user=[^\s]+ --password=.*$/', $output);
   }
 }
