f40dba5eecfda9cf6fa052c0ae6b66fd670524b5
Greg Anderson <greg.1.anderson@greenknowe.org>
2013-01-10 10:45:12 -0800

    Remove spurrious line in drushScriptTest

--- tests/drushScriptTest.php
+++ tests/drushScriptTest.php
@@ -22,7 +22,6 @@ class drushScriptCase extends Drush_CommandTestCase {
     );
     $this->drush('ev', array('print ini_get("default_mimetype");'), $options, NULL, NULL, self::EXIT_SUCCESS, NULL, $env);
     $output = $this->getOutput();
-    $expected = sprintf('Calling proc_open(ssh -o PasswordAuthentication=no %s@%s);', self::escapeshellarg('user'), self::escapeshellarg('server'));
     $this->assertEquals('text/drush', $output);
   }
 }
