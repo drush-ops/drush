ccd0cd9067b59d8fabf7a71e8ce403f943d9b3f9
Greg Anderson <greg.1.anderson@greenknowe.org>
2013-04-26 21:17:07 -0700

    Issue #1957974 by greg.1.anderson: Strict option commands assume unrecognized keys from a site alias belong at the end of the command

--- tests/siteAliasTest.php
+++ tests/siteAliasTest.php
@@ -7,6 +7,54 @@
  * @group base
  */
 class saCase extends Drush_CommandTestCase {
+  /*
+   * Covers the following responsibilities.
+   *   - Dispatching a Drush command that uses strict option handling
+   *     using a site alias that contains a generic option (e.g. 'site'
+   *     or 'env') that is not a recognized Drush option (maybe for
+   *     use in an init hook, etc.) places the generic option BEFORE
+   *     the command name.
+   *   - Dispatching a Drush command that uses strict option handling
+   *     using a site alias that contains a command-specific option
+   *     places said option AFTER the command name.
+   */
+  function testDispatchStrictOptions() {
+    $aliasPath = UNISH_SANDBOX . '/aliases';
+    mkdir($aliasPath);
+    $aliasFile = $aliasPath . '/bar.aliases.drushrc.php';
+    $aliasContents = <<<EOD
+  <?php
+  // Writtne by Unish. This file is safe to delete.
+  \$aliases['test'] = array(
+    'remote-host' => 'fake.remote-host.com',
+    'remote-user' => 'www-admin',
+    'root' => '/fake/path/to/root',
+    'uri' => 'default',
+    'site' => 'stage',
+    'command-specific' => array(
+      'rsync' => array(
+        'delete' => TRUE,
+      ),
+    ),
+  );
+EOD;
+    file_put_contents($aliasFile, $aliasContents);
+    $options = array(
+      'alias-path' => $aliasPath,
+      'include' => dirname(__FILE__), // Find unit.drush.inc commandfile.
+      'simulate' => TRUE,
+    );
+    $this->drush('core-rsync', array('/a', '/b'), $options, '@test');
+    $output = $this->getOutput();
+    $command_position = strpos($output, 'core-rsync');
+    $special_option_position = strpos($output, '--site=stage');
+    $command_specific_position = strpos($output, '--delete');
+    $this->assertTrue($command_position !== FALSE);
+    $this->assertTrue($special_option_position !== FALSE);
+    $this->assertTrue($command_specific_position !== FALSE);
+    $this->assertTrue($command_position > $special_option_position);
+    $this->assertTrue($command_position < $command_specific_position);
+  }
 
   /*
    * Assure that site lists work as expected.
