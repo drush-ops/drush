50605f024e864aaaf6c91d0211cfdc92a4aae160
helmo <helmo@449000.no-reply.drupal.org>
2013-07-07 17:00:48 -0400

    Issue #2014519 by helmo, clemens.tolboom: Fixed SQLite test fails due to missing path while restoring database.

--- tests/sqlConnectTest.php
+++ tests/sqlConnectTest.php
@@ -22,21 +22,24 @@ class SqlConnectCase extends Drush_CommandTestCase {
     $this->drush('sql-connect', array(), $options);
     $output = $this->getOutput();
 
+    // Not all drivers need -e option like sqlite
+    $shell_options = "-e";
     if (strpos(UNISH_DB_URL, 'mysql') !== FALSE) {
       $this->assertRegExp('/^mysql --database=[^\s]+ --host=[^\s]+ --user=[^\s]+ --password=.*$/', $output);
     }
     elseif (strpos(UNISH_DB_URL, 'sqlite') !== FALSE) {
       $this->assertContains('sqlite3', $output);
+      $shell_options = '';
     }
     elseif (strpos(UNISH_DB_URL, 'pgsql') !== FALSE) {
       $this->assertRegExp('/^psql --dbname=[^\s]+ --host=[^\s]+ --port=[^\s] --username=[^\s]+/', $output);
     }
     else {
-      $this->markTestSkipped('sql-connect test does not recognize database type in UNISH_DB_URL.');
+      $this->markTestSkipped('sql-connect test does not recognize database type in ' . UNISH_DB_URL);
     }
 
     // Issue a query and check the result to verify the connection.
-    $this->execute($output . ' -e "select name from users where uid = 1;"');
+    $this->execute($output . ' ' . $shell_options . ' "select name from users where uid = 1;"');
     $output = $this->getOutput();
     $this->assertContains('admin', $output);
 
