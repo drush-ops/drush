09003d93eb386d46a33d361c03bf583fc4c3f3e6
Moshe Weitzman <weitzman@tejasa.com>
2013-07-16 10:47:58 -0400

    Use getOutputFromJSON() in cache tests

--- tests/cacheCommandTest.php
+++ tests/cacheCommandTest.php
@@ -16,25 +16,23 @@ class cacheCommandCase extends Drush_CommandTestCase {
 
     // Test the cache get command.
     $this->drush('cache-get', array('schema'), $options + array('format' => 'json'));
-    $schema = json_decode($this->getOutput());
-    $this->assertObjectHasAttribute('data', $schema);
+    $schema = $this->getOutputFromJSON('data');
+    $this->assertNotEmpty($schema);
 
     // Test that get-ing a non-existant cid fails.
     $this->drush('cache-get', array('test-failure-cid'), $options + array('format' => 'json'), NULL, NULL, self::EXIT_ERROR);
-    $output = json_decode($this->getOutput());
-    $this->assertEmpty($output);
 
     // Test setting a new cache item.
-    $cache_test_value = 'cache test string';
-    $this->drush('cache-set', array('cache-test-cid', $cache_test_value), $options);
+    $expected = 'cache test string';
+    $this->drush('cache-set', array('cache-test-cid', $expected), $options);
     $this->drush('cache-get', array('cache-test-cid'), $options + array('format' => 'json'));
-    $cache_value = json_decode($this->getOutput());
-    $this->assertEquals($cache_test_value, $cache_value->data);
+    $data = $this->getOutputFromJSON('data');
+    $this->assertEquals($expected, $data);
 
     // Test cache-clear all.
     $this->drush('cache-clear', array('all'), $options);
     $this->drush('cache-get', array('cache-test-cid'), $options + array('format' => 'json'), NULL, NULL, self::EXIT_ERROR);
-    $output = json_decode($this->getOutput());
+    $output = $this->getOutputFromJSON('data');
     $this->assertEmpty($output);
   }
 }
