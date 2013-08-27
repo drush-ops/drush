4625fab50fb94baef5859d6b52ec391c40314e85
Moshe Weitzman <weitzman@tejasa.com>
2013-08-27 14:24:41 -0400

    Remove duplicate test.

--- tests/siteSetTest.php
+++ tests/siteSetTest.php
@@ -20,17 +20,11 @@ class siteSetCommandCase extends Drush_CommandTestCase {
     $this->drush('site-set', array($alias));
     $expected = 'Site set to ' . $alias;
     $output = $this->getOutput();
-
     $this->assertEquals($expected, $output);
 
     $this->drush('site-set', array());
     $output = $this->getOutput();
     $this->assertEquals('Site set to @none', $output);
-
-    $this->drush('ev', array("drush_invoke('site-set', '$alias'); print drush_sitealias_site_get();"));
-    $output = $this->getOutput();
-    $this->assertEquals("Site set to $alias
-$alias", $output);
 
     $this->drush('ev', array("drush_invoke('site-set', '$alias'); drush_invoke('site-set', '@none'); drush_invoke('site-set', '-'); print drush_sitealias_site_get();"));
     $output = $this->getOutput();
