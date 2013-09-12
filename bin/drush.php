03ec63b133d2e5aed5d9d256a1d2ae515a71de62
Patrick Connolly <patrick@myplanetdigital.com>
2013-09-12 14:28:22 -0400

    Adds bootstrap file.

--- bin/drush.php
+++ bin/drush.php
@@ -1,13 +1,16 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
+require(dirname(__FILE__) . '/../includes/bootstrap.inc');
 
 use Symfony\Component\Console\Application;
 use Drupal\Drush\Version;
 use Drupal\Drush\Command\Core\ArchiveDumpCommand;
 use Drupal\Drush\Command\Core\ArchiveRestoreCommand;
+use Drupal\Drush\Command\Core\StatusCommand;
 
 $cli = new Application('Drush Command Line Interface', Version::VERSION);
 $cli->add(new ArchiveDumpCommand);
 $cli->add(new ArchiveRestoreCommand);
+$cli->add(new StatusCommand);
 $cli->run();
