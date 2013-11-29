a2e548e9c087987cd8a1b713668c19dc3f48338f
Clemens Tolboom <clemens@build2be.com>
2013-11-29 15:05:11 +0100

    Add UML Diagram generation to tests.

---
+++ tests/bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+
+$loader = (require __DIR__ . '/../vendor/autoload.php');
+$loader->add('Provision_', 'Provision');
+$loader->add('Provision_', 'http/Provision');
+$loader->add('Provision_', 'db/Provision');
