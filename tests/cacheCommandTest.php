c400639949e46921e71883a3136492c01d2d595d
Moshe Weitzman <weitzman@tejasa.com>
2013-12-06 17:41:41 -0500

    Convert the $expire argument to a valid value. Handle cases were $data is an object, or neither an object or array.

--- tests/cacheCommandTest.php
+++ tests/cacheCommandTest.php
@@ -30,6 +30,16 @@ class cacheCommandCase extends Drush_CommandTestCase {
     $data = $this->getOutputFromJSON('data');
     $this->assertEquals($expected, $data);
 
+    // Test cache-set using all arguments and many options.
+    $expected = array('key' => 'value');
+    $input = array('data'=> $expected);
+    $stdin = json_encode($input);
+    $exec = sprintf('echo %s | %s cache-set %s %s my_cache_id - cache CACHE_PERMANENT --format=json --cache-get 2>/dev/null', self::escapeshellarg($stdin), UNISH_DRUSH, "--root=" . self::escapeshellarg($options['root']), '--uri=' . $options['uri']);
+    $return = $this->execute($exec);
+    $this->drush('cache-get', array('my_cache_id', 'cache'), $options + array('format' => 'json'));
+    $data = $this->getOutputFromJSON('data');
+    $this->assertEquals((object)$expected, $data);
+
     // Test cache-clear all.
     $this->drush('cache-clear', array('all'), $options);
     $this->drush('cache-get', array('cache-test-cid'), $options + array('format' => 'json'), NULL, NULL, self::EXIT_ERROR);
