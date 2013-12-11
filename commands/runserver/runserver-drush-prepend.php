3e6c08bc5fb1690159b227c61b3b388dd4b15e84
Kosta Harlan <kostajh@gmail.com>
2013-12-11 09:22:53 -0500

    Set the Content-Type to JSON when appropriate; set response code depending on Drush response

--- commands/runserver/runserver-drush-prepend.php
+++ commands/runserver/runserver-drush-prepend.php
@@ -11,15 +11,24 @@ $command = sprintf('%s %s --backend', $drush_executable, $drush_args);
 error_log(sprintf('Drush Server: Running command %s', $command));
 $data = shell_exec($command);
 $output = runserver_drush_backend_parse_output($data);
+$response_code = 200;
 if ($output['error_log']) {
+  header('Content-Type: application/json');
+  $response_code = 500;
   echo json_encode($output['error_log']);
 }
 elseif (!$output['output']) {
-  echo json_encode(sprintf('Successfully ran the command: !request', $drush_args));
+  header('Content-Type: application/json');
+  echo json_encode(sprintf('Successfully ran the command: %s', $drush_args));
 }
 else {
+  // Check if Drush output can be decoded as JSON and set headers appropriately.
+  if (json_decode($output['output'])) {
+    header('Content-Type: application/json');
+  }
   echo $output['output'];
 }
+http_response_code($response_code);
 return;
 
 /**
