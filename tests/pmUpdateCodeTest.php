dfe5de4270d3aafb0f7d1034241304b902cc2410
Moshe Weitzman <weitzman@tejasa.com>
2014-01-04 09:31:24 -0500

    Backport improvements to updatecodeTest

--- tests/pmUpdateCodeTest.php
+++ tests/pmUpdateCodeTest.php
@@ -13,11 +13,32 @@
  */
 class pmUpdateCode extends Drush_CommandTestCase {
 
+  /*
+   * An array of modules to be downloaded and enabled.
+   */
+  public $modules;
+
   /**
    * Download old core and older contrib releases which will always need updating.
    */
   public function setUp() {
-    $sites = $this->setUpDrupal(1, TRUE, '7.0-rc3');
+    if (UNISH_DRUPAL_MAJOR_VERSION >= 8) {
+      $core = '8.0-alpha6';
+      $modules_str = 'instagram_block-8.x-1.0,honeypot-8.x-1.14-beta5';
+      $this->modules = array('block', 'instagram_block', 'honeypot');
+    }
+    elseif (UNISH_DRUPAL_MAJOR_VERSION == 7) {
+      $core = '7.0-rc3';
+      $modules_str = 'devel-7.x-1.0-rc1,webform-7.x-3.4-beta1';
+      $this->modules = array('menu', 'devel', 'webform');
+    }
+    else {
+      $core = '6.28';
+      $modules_str = 'devel-6.x-1.26,webform-6.x-3.18';
+      $this->modules = array('menu', 'devel', 'webform');
+    }
+
+    $sites = $this->setUpDrupal(1, TRUE, $core);
     $options = array(
       'root' => $this->webroot(),
       'uri' => key($sites),
@@ -27,40 +48,47 @@ class pmUpdateCode extends Drush_CommandTestCase {
       'skip' => NULL, // No FirePHP
       'strict' => 0, // invoke from script: do not verify options
     );
-    $this->drush('pm-download', array('devel-7.x-1.0-rc1,webform-7.x-3.4-beta1'), $options);
-    $this->drush('pm-enable', array('menu', 'devel', 'webform'), $options);
+
+    $this->drush('pm-download', array($modules_str), $options);
+    $this->drush('pm-enable', $this->modules, $options);
   }
 
   function testUpdateCode() {
+    $first = $this->modules[1];
+    $second = $this->modules[2];
+
     $options = array(
       'root' => $this->webroot(),
       'uri' => key($this->sites), // Have to access class property since $sites in in setUp().
       'yes' => NULL,
       'backup-dir' => UNISH_SANDBOX . '/backups',
+      'cache' => NULL,
+      'check-updatedb' => 0,
+      'strict' => 0,
     );
 
     // Try to upgrade a specific module.
-    $this->drush('pm-updatecode', array('devel'), $options + array());
-    // Assure that devel was upgraded and webform was not.
-    $this->drush('pm-updatecode', array(), $options + array('pipe' => NULL));
+    $this->drush('pm-updatecode', array($first), $options + array());
+    // Assure that first was upgraded and second was not.
+    $this->drush('pm-updatestatus', array(), $options + array());
     $all = $this->getOutput();
-    $this->assertNotContains('devel', $all);
-    $this->assertContains('webform', $all);
+    $this->assertNotContains($first, $all);
+    $this->assertContains($second, $all);
 
-    // Lock webform, and update core.
-    $this->drush('pm-updatecode', array(), $options + array('lock' => 'webform'));
+    // Lock second, and update core.
+    $this->drush('pm-updatecode', array(), $options + array('lock' => $second));
     $list = $this->getOutputAsList(); // For debugging.
-    $this->drush('pm-updatecode', array(), $options + array('pipe' => NULL));
+    $this->drush('pm-updatestatus', array(), $options + array());
     $all = $this->getOutput();
     $this->assertNotContains('drupal', $all, 'Core was updated');
-    $this->assertContains('webform', $all, 'Webform was skipped.');
+    $this->assertContains($second, $all, 'Second was skipped.');
 
-    // Unlock webform, update, and check.
-    $this->drush('pm-updatecode', array(), $options + array('unlock' => 'webform', 'no-backup' => NULL));
+    // Unlock second, update, and check.
+    $this->drush('pm-updatecode', array(), $options + array('unlock' => $second, 'no-backup' => NULL));
     $list = $this->getOutputAsList();
-    $this->drush('pm-updatecode', array(), $options + array('pipe' => NULL));
+    $this->drush('pm-updatestatus', array(), $options + array());
     $all = $this->getOutput();
-    $this->assertNotContains('webform', $all, 'Webform was updated');
+    $this->assertNotContains($second, $all, 'Second was updated');
 
     // Verify that we keep backups as instructed.
     $backup_dir = UNISH_SANDBOX . '/backups';
@@ -68,23 +96,21 @@ class pmUpdateCode extends Drush_CommandTestCase {
     $Iterator = new RecursiveIteratorIterator($Directory);
     $found = FALSE;
     foreach ($Iterator as $item) {
-      if (basename($item) == 'devel.module') {
+      if (basename($item) == $first . '.module') {
         $found = TRUE;
         break;
       }
     }
-    $this->assertTrue($found, 'Backup exists and contains devel module.');
-
-
+    $this->assertTrue($found, 'Backup exists and contains the first module.');
 
     $Iterator = new RecursiveIteratorIterator($Directory);
     $found = FALSE;
     foreach ($Iterator as $item) {
-      if (basename($item) == 'webform.module') {
+      if (basename($item) == $second . '.module') {
         $found = TRUE;
         break;
       }
     }
-    $this->assertFalse($found, 'Backup exists and does not contain webformmodule.');
+    $this->assertFalse($found, 'Backup exists and does not contain the second module.');
   }
 }
