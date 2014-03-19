d31c0a8e92bb300f25163bea3c643f65ca04d3c0
Moshe Weitzman <weitzman@tejasa.com>
2014-03-19 13:21:49 -0400

    Move shared parts of get_db_spec() into drush_sql_get_class(). Fixes #536.

--- lib/Drush/Sql/Sql8.php
+++ lib/Drush/Sql/Sql8.php
@@ -6,13 +6,11 @@ use Drupal\Core\Database\Database;
 class Sql8 extends Sql7 {
   public function get_db_spec() {
     $db_spec = NULL;
-    if (!$db_spec = parent::get_db_spec()) {
-      if (drush_bootstrap_max(DRUSH_BOOTSTRAP_DRUPAL_CONFIGURATION)) {
-        $database = drush_get_option('database', 'default');
-        $target = drush_get_option('target', 'default');
-        if ($info = Database::getAllConnectionInfo()) {
-          return $info[$database][$target];
-        }
+    if (drush_bootstrap_max(DRUSH_BOOTSTRAP_DRUPAL_CONFIGURATION)) {
+      $database = drush_get_option('database', 'default');
+      $target = drush_get_option('target', 'default');
+      if ($info = Database::getAllConnectionInfo()) {
+        return $info[$database][$target];
       }
     }
     return $db_spec;
--- lib/Drush/Sql/SqlVersion.php
+++ lib/Drush/Sql/SqlVersion.php
@@ -3,21 +3,13 @@
 namespace Drush\Sql;
 
 class SqlVersion {
-  public function get_db_spec() {
-    $db_spec = NULL;
-    $database = drush_get_option('database', 'default');
-    $target = drush_get_option('target', 'default');
-
-    if ($url = drush_get_option('db-url')) {
-      $url =  is_array($url) ? $url[$database] : $url;
-      $db_spec = drush_convert_db_from_db_url($url);
-      $db_spec['db_prefix'] = drush_get_option('db-prefix');
-    }
-    elseif (($databases = drush_get_option('databases')) && (array_key_exists($database, $databases)) && (array_key_exists($target, $databases[$database]))) {
-      $db_spec = $databases[$database][$target];
-    }
-    return $db_spec;
-  }
+  /*
+   * Determine $db_spec by inspecting the global environment (D6/7) or the DB API (D8+).
+   *
+   * @return array $db_spec
+   *   An array specifying a database connection.
+   */
+  public function get_db_spec() {}
 
   public function valid_credentials() {
     $db_spec = $this->get_db_spec();
