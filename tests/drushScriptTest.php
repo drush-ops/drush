e25a7196a13239cca0db221f30a066ea01a0cf26
Moshe Weitzman <weitzman@tejasa.com>
2014-04-24 21:32:06 -0400

    Code standards.

--- tests/drushScriptTest.php
+++ tests/drushScriptTest.php
@@ -11,16 +11,13 @@ class drushScriptCase extends CommandUnishTestCase {
    * Test `PHP_OPTIONS=... drush`
    */
   public function testPhpOptionsTest() {
-    // todo: could probably run this test on mingw
+    // @todo: could probably run this test on mingw
     if ($this->is_windows()) {
-      $this->markTestSkipped('environment variable tests not currently functional on Windows.');
+      $this->markTestSkipped('Environment variable tests not currently functional on Windows.');
     }
 
-    $options = array(
-    );
-    $env = array(
-      'PHP_OPTIONS' => '-d default_mimetype="text/drush"',
-    );
+    $options = array();
+    $env = array('PHP_OPTIONS' => '-d default_mimetype="text/drush"');
     $this->drush('ev', array('print ini_get("default_mimetype");'), $options, NULL, NULL, self::EXIT_SUCCESS, NULL, $env);
     $output = $this->getOutput();
     $this->assertEquals('text/drush', $output);
