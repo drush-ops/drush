0a715b002d1e343293f5009403089a28844f3dac
Moshe Weitzman <weitzman@tejasa.com>
2014-05-23 12:19:18 -0400

    We now pass -q to psql. Hope thats OK. Adjusting test.

--- tests/sqlConnectTest.php
+++ tests/sqlConnectTest.php
@@ -35,7 +35,7 @@ class SqlConnectCase extends CommandUnishTestCase {
       $shell_options = '';
     }
     elseif ($db_driver == 'pgsql') {
-      $this->assertRegExp('/^psql --dbname=[^\s]+ --host=[^\s]+ --port=[^\s]+ --username=[^\s]+/', $output);
+      $this->assertRegExp('/^psql -q --dbname=[^\s]+ --host=[^\s]+ --port=[^\s]+ --username=[^\s]+/', $output);
     }
     else {
       $this->markTestSkipped('sql-connect test does not recognize database type in ' . UNISH_DB_URL);
