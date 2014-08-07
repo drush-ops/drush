435c2a2521a16c737402f196b92040628a463428
Greg Anderson <greg.1.anderson@greenknowe.org>
2014-08-06 21:03:16 -0700

    Carefully bootstrap Drupal in the Drush sql commands, so that operations on the sql database (especially sql-create) will work correctly, even if Drupal is not fully initialized.

--- lib/Drush/Sql/Sql7.php
+++ lib/Drush/Sql/Sql7.php
@@ -5,16 +5,15 @@ namespace Drush\Sql;
 class Sql7 extends SqlVersion {
   public function get_db_spec() {
     $db_spec = NULL;
-    if (drush_bootstrap_max(DRUSH_BOOTSTRAP_DRUPAL_CONFIGURATION)) {
-      $database = drush_get_option('database', 'default');
-      $target = drush_get_option('target', 'default');
-      // We don't use DB API here `sql-sync` would have to messily addConnection.
-      if (!isset($GLOBALS['databases']) || !array_key_exists($database, $GLOBALS['databases']) || !array_key_exists($target, $GLOBALS['databases'][$database])) {
-        // Do nothing
-      }
-      else {
-        $db_spec = $GLOBALS['databases'][$database][$target];;
-      }
+    drush_sql_bootstrap_database_configuration();
+    $database = drush_get_option('database', 'default');
+    $target = drush_get_option('target', 'default');
+    // We don't use DB API here `sql-sync` would have to messily addConnection.
+    if (!isset($GLOBALS['databases']) || !array_key_exists($database, $GLOBALS['databases']) || !array_key_exists($target, $GLOBALS['databases'][$database])) {
+      // Do nothing
+    }
+    else {
+      $db_spec = $GLOBALS['databases'][$database][$target];;
     }
     return $db_spec;
   }
