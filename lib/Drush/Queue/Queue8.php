f430375e5a9f84388401f0556ad55ee74314b45c
Jonathan Hedstrom <jhedstrom@gmail.com>
2014-11-13 11:59:02 -0800

    Queue system updates for D8.
    - Fixes issue #232

--- lib/Drush/Queue/Queue8.php
+++ lib/Drush/Queue/Queue8.php
@@ -5,12 +5,25 @@ namespace Drush\Queue;
 class Queue8 extends QueueBase {
 
   /**
+   * @var \Drupal\Core\Queue\QueueWorkerManager
+   */
+  protected $workerManager;
+
+  /**
+   * Set the queue worker manager.
+   */
+  public function __construct(QueueWorkerManager $manager = NULL) {
+    $this->workerManager = $manager ?: \Drupal::service('plugin.manager.queue_worker');
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function getQueues() {
     if (!isset($this->queues)) {
-      $this->queues = \Drupal::moduleHandler()->invokeAll('queue_info');
-      \Drupal::moduleHandler()->alter('queue_info', $this->queues);
+      foreach ($this->workerManager->getDefinitions() as $name => $info) {
+        $this->queues[$name] = $info;
+      }
     }
     return $this->queues;
   }
@@ -20,6 +33,22 @@ class Queue8 extends QueueBase {
    */
   public function getQueue($name) {
     return \Drupal::queue($name);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function run($name) {
+    $this->prepareEnvironment();
+    $info = $this->getInfo($name);
+    $worker = $this->workerManager->createInstance($name);
+    $end = time() + (isset($info['cron']['time']) ? $info['cron']['time'] : 15);
+    $queue = $this->getQueue($name);
+    while (time() < $end && ($item = $queue->claimItem())) {
+      $worker->processItem($item->data);
+      $queue->deleteItem($item);
+    }
+
   }
 
 }
