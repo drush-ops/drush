e4ca43bd12bc25d59b21f3e656015e2c1ecc4eb2
Rob Bayliss <rob@lastcallmedia.com>
2014-12-08 11:02:32 -0500

    Fix user_load_by_name and user_load_by_mail do not exist in Drupal 6

--- lib/Drush/User/User6.php
+++ lib/Drush/User/User6.php
@@ -4,4 +4,18 @@ namespace Drush\User;
 
 class User6 extends User7 {
 
+  /**
+   * {@inheritdoc}
+   */
+  public function load_by_name($name) {
+    return user_load(array('name' => $name));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function load_by_mail($mail) {
+    return user_load(array('mail' => $mail));
+  }
+
 }
