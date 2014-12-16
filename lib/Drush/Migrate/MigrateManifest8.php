896b32dd0808e69d5a319d8e4414c6ddb09105c6
benjy <ben@dougherty.net.au>
2014-12-16 20:55:15 +0800

    Use the array keys for calculating the missing migrations.

--- lib/Drush/Migrate/MigrateManifest8.php
+++ lib/Drush/Migrate/MigrateManifest8.php
@@ -99,7 +99,7 @@ class MigrateManifest8 implements MigrateInterface {
     }
 
     // Warn the user if any migrations were not found.
-    $nonexistent_migrations = array_diff($this->migrationList, array_keys($migrations));
+    $nonexistent_migrations = array_diff(array_keys($migration_ids), array_keys($migrations));
     if (count($nonexistent_migrations) > 0) {
       drush_log(dt('The following migrations were not found: !migrations', array(
         '!migrations' => implode(', ', $nonexistent_migrations),
