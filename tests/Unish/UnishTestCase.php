5eefdce74fba93c89e55c845470cb8a46a3fa37a
Moshe Weitzman <weitzman@tejasa.com>
2015-03-17 14:59:34 -0400

    Verify MD5 for D8 once again now that we have better ftp.drupal.org architecture.

--- tests/Unish/UnishTestCase.php
+++ tests/Unish/UnishTestCase.php
@@ -290,7 +290,6 @@ abstract class UnishTestCase extends \PHPUnit_Framework_TestCase {
     if ($version_string == 8) {
       // We want to track Drupal 8 very closely.
       $version_string = '8.0.x';
-      $options['no-md5'] = NULL;
     }
 
     // Download Drupal if not already present.
