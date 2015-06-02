8d94641da9ad6c44e60fd4b1cb9e12b27705c5e9
Greg Anderson <greg.1.anderson@greenknowe.org>
2015-06-02 15:45:41 -0700

    Remove revealing echo accidentally left in previous commit.

--- children.sh
+++ children.sh
@@ -8,8 +8,7 @@ then
   for CHILD in $TEST_CHILDREN
   do
     BUILD_NUM=$(curl -s "https://api.travis-ci.org/repos/$CHILD/builds" | grep -o '^\[{"id":[0-9]*,' | grep -o '[0-9]' | tr -d '\n')
-    echo "Build number for $CHILD is $BUILD_NUM"
-    echo curl -X POST https://api.travis-ci.org/builds/$BUILD_NUM/restart --header "Authorization: token "$AUTH_TOKEN
+    echo "Restarting build $BUILD_NUM for $CHILD"
     curl -X POST https://api.travis-ci.org/builds/$BUILD_NUM/restart --header "Authorization: token "$AUTH_TOKEN
   done
 fi
