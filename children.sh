86a83583bb12d226e60824b3688246d533f66538
Greg Anderson <greg.1.anderson@greenknowe.org>
2015-06-02 15:17:12 -0700

    Test secure envrionment variables again; apparently, these are not set for pull requests from foreign repositories -- which is good.

--- children.sh
+++ children.sh
@@ -1,15 +1,11 @@
-TOKEN="$1"
-shift
-
-echo "Auth Token is $AUTH_TOKEN"
-echo "Token is $TOKEN"
-echo "----------"
+# After a travis build succeeds, run tests from any child repository defined in $TEST_CHILDREN
 echo "Printenv:"
 printenv
-echo "----------"
-for CHILD in $@
+echo "-----------"
+for CHILD in $TEST_CHILDREN
 do
   BUILD_NUM=$(curl -s 'https://api.travis-ci.org/repos/$CHILD/builds' | grep -o '^\[{"id":[0-9]*,' | grep -o '[0-9]' | tr -d '\n')
   echo "Build number for $CHILD is $BUILD_NUM"
-  echo curl -X POST https://api.travis-ci.org/builds/$BUILD_NUM/restart --header "Authorization: token $TOKEN"
+  echo curl -X POST https://api.travis-ci.org/builds/$BUILD_NUM/restart --header "Authorization: token "$AUTH_TOKEN
+  curl -X POST https://api.travis-ci.org/builds/$BUILD_NUM/restart --header "Authorization: token "$AUTH_TOKEN
 done
