642bc92218e259f9fc6c0751c5a4f759afb61a88
Greg Anderson <greg.1.anderson@greenknowe.org>
2015-06-02 14:34:44 -0700

    Try again with the Auth token.

--- children.sh
+++ children.sh
@@ -1,7 +1,11 @@
+TOKEN="$1"
+shift
+
 echo "Auth Token is $AUTH_TOKEN"
+echo "Token is $TOKEN"
 for CHILD in $@
 do
   BUILD_NUM=$(curl -s 'https://api.travis-ci.org/repos/$CHILD/builds' | grep -o '^\[{"id":[0-9]*,' | grep -o '[0-9]' | tr -d '\n')
   echo "Build number for $CHILD is $BUILD_NUM"
-  echo curl -X POST https://api.travis-ci.org/builds/$BUILD_NUM/restart --header "Authorization: token "$AUTH_TOKEN"
+  echo curl -X POST https://api.travis-ci.org/builds/$BUILD_NUM/restart --header "Authorization: token $TOKEN"
 done
