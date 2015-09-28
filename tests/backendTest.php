763e27d2fd641fb4d06cfc632e7bb576101399ab
Moshe Weitzman <weitzman@tejasa.com>
2015-09-28 12:51:03 -0400

    More debugging in test_script. Also a couple uses of bit_bucket().

--- tests/backendTest.php
+++ tests/backendTest.php
@@ -56,7 +56,7 @@ EOD;
    * General handling of site aliases will be in sitealiasTest.php.
    */
   function testOrigin() {
-    $exec = sprintf('%s %s version arg1 arg2 --simulate --ssh-options=%s 2>/dev/null | grep ssh', UNISH_DRUSH, self::escapeshellarg('user@server/path/to/drupal#sitename'), self::escapeshellarg('-i mysite_dsa'));
+    $exec = sprintf('%s %s version arg1 arg2 --simulate --ssh-options=%s 2>%s | grep ssh', UNISH_DRUSH, self::escapeshellarg('user@server/path/to/drupal#sitename'), self::escapeshellarg('-i mysite_dsa'), self::escapeshellarg($this->bit_bucket()));
     $this->execute($exec);
     $bash = $this->escapeshellarg('drush  --uri=sitename --root=/path/to/drupal  version arg1 arg2 2>&1');
     $expected = "Simulating backend invoke: ssh -i mysite_dsa user@server $bash 2>&1";
@@ -73,7 +73,7 @@ EOD;
   */
   function testTarget() {
     $stdin = json_encode(array('filter'=>'sql'));
-    $exec = sprintf('echo %s | %s version --backend 2>/dev/null', self::escapeshellarg($stdin), UNISH_DRUSH);
+    $exec = sprintf('echo %s | %s version --backend 2>%s', self::escapeshellarg($stdin), UNISH_DRUSH, self::escapeshellarg($this->bit_bucket()));
     $this->execute($exec);
     $parsed = $this->parse_backend_output($this->getOutput());
     $this->assertTrue((bool) $parsed, 'Successfully parsed backend output');
