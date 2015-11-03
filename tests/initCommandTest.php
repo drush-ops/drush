6896aaf2fa8401124c9334cf598de2cf9ced2365
Greg Anderson <greg.1.anderson@greenknowe.org>
2015-11-03 09:29:33 -0800

    Check to see if sourcing the .bashrc file defines Drush's bash aliases after running 'core-init'.

--- tests/initCommandTest.php
+++ tests/initCommandTest.php
@@ -24,5 +24,11 @@ class initCommandCase extends CommandUnishTestCase {
     $this->assertTrue(is_file("$home/.drush/drushrc.php"), "The expected ~/.drush/drushrc.php file does not exist");
     $this->assertTrue(is_file("$home/.drush/drush.bashrc"), "The expected ~/.drush/drush.bashrc file does not exist");
     $this->assertTrue(is_file("$home/.bashrc"), "The expected ~/.bashrc file does not exist");
+
+    // Non-interactive shells behave differently than interactive login shells,
+    // so we will explicitly source the .bashrc file for our test.
+    $exec = sprintf("bash -c '. %s; alias ddd'", self::escapeshellarg("$home/.bashrc"));
+    $this->execute($exec);
+    $this->assertEquals("alias ddd='drush drupal-directory'", $this->getOutput());
   }
 }
