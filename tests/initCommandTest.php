62b08faf3e49a65d4cc5f610985b698a6d71846a
Greg Anderson <greg.1.anderson@greenknowe.org>
2015-11-11 09:45:13 -0800

    Minor clean-up to core-init command. Also, add a --add-path option to core-init, mostly so that we can force the path writing in our tests and assert that it happened correctly.

--- tests/initCommandTest.php
+++ tests/initCommandTest.php
@@ -12,7 +12,7 @@ class initCommandCase extends CommandUnishTestCase {
 
   function testInitCommand() {
     // Call `drush core-init`
-    $this->drush('core-init', array(), array('backend' => NULL));
+    $this->drush('core-init', array(), array('backend' => NULL, 'add-path' => TRUE));
     $parsed = $this->parse_backend_output($this->getOutput());
     // First test to ensure that the command claimed to have made the expected progress
     $this->assertLogHasMessage($parsed['log'], "Copied example Drush configuration file", 'ok');
@@ -25,8 +25,10 @@ class initCommandCase extends CommandUnishTestCase {
     $this->assertFileExists("$home/.drush/drush.bashrc");
     $this->assertFileExists("$home/.bashrc");
 
-    // Check to see if the .bashrc file sources our drush.bashrc file.
+    // Check to see if the .bashrc file sources our drush.bashrc file,
+    // and whether it adds the path to UNISH_DRUSH to the $PATH
     $bashrc_contents = file_get_contents("$home/.bashrc");
     $this->assertContains('drush.bashrc', $bashrc_contents);
+    $this->assertContains(dirname(UNISH_DRUSH), $bashrc_contents);
   }
 }
