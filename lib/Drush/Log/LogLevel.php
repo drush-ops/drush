c64ccd3ec9b2030c082e2bc91a381ebe4b382458
Greg Anderson <greg.1.anderson@greenknowe.org>
2015-11-25 14:57:19 -0800

    Add Drush-specific log levels.

---
+++ lib/Drush/Log/LogLevel.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Drush\Log;
+
+/**
+ * Additional log levels that Drush uses for historical reasons.
+ * Standard log levels should be preferred.
+ */
+class LogLevel extends Psr\Log\LogLevel
+{
+    // Notice that the user is cancelling an operation. Like 'warning'
+    const CANCEL = 'cancel';
+
+    // Something did not work. Like 'error'. Deprecated.
+    const FAILED = 'failed'; // Deprecated. Use drush_set_error
+
+    // Various 'success' messages.  Like 'notice'
+    const OK = 'ok';
+    const COMPLETED = 'completed'; // Deprecated: use 'SUCCESS' or drush_set_error
+
+    // Means the command was successful. Should appear at most once
+    // per command (perhaps more if subcommands are executed, though).
+    // Like 'notice'.
+    const SUCCESS = 'success';
+
+    // synonyms for 'notice'
+    const STATUS = 'status'; // Deprecated. Use 'notice'
+    const MESSAGE = 'message'; // Deprecated. Use 'notice'
+}
--- lib/Drush/Log/Logger.php
+++ lib/Drush/Log/Logger.php
@@ -6,6 +6,21 @@
  *
  * This is the actual Logger for Drush that is responsible
  * for logging messages.
+ *
+ * This logger is designed such that it can be provided to
+ * other libraries that log to a Psr\Log\LoggerInterface.
+ * As such, it takes responsibility for passing log messages
+ * to backend invoke, as necessary (c.f. drush_backend_packet()).
+ *
+ * Drush supports all of the required log levels from Psr\Log\LogLevel,
+ * and also defines its own. See Drush\Log\LogLevel.
+ *
+ * Those who may wish to change the way logging works in Drush
+ * should therefore NOT attempt to replace this logger with their
+ * own LoggerInterface, as it will not work.  It would be okay
+ * to extend Drush\Log\Logger, or perhaps we could provide a way
+ * to set an output I/O object here, in case output redirection
+ * was the only thing that needed to be swapped out.
  */
 
 namespace Drush\Log;
