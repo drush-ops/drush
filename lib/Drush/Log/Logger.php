2632c37ae898b770f3ce70a36a60c70032b15d91
Greg Anderson <greg.1.anderson@greenknowe.org>
2015-11-25 13:15:05 -0800

    Fix initialization order in Logger.

--- lib/Drush/Log/Logger.php
+++ lib/Drush/Log/Logger.php
@@ -15,6 +15,11 @@ use Psr\Log\AbstractLogger;
 class Logger extends AbstractLogger {
 
     public function log($level, $message, array $context = array()) {
+      // Convert to old $entry array for b/c calls
+      $entry = $context;
+      $entry['type'] = $level;
+      $entry['message'] = $message;
+
       // Drush\Log\Logger should take over all of the responsibilities
       // of drush_log, including caching the log messages and sending
       // log messages along to backend invoke.
@@ -25,9 +30,6 @@ class Logger extends AbstractLogger {
 
       // Just send this back to _drush_print_log for now.
       // TODO: move _drush_print_log implementation inside this class.
-      $entry = $context;
-      $entry['type'] = $level;
-      $entry['message'] = $message;
       _drush_print_log($entry);
     }
 }
