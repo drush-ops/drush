d64fdf785bebe848cbcd4da43ee445ff32c9635b
Greg Anderson <greg.1.anderson@greenknowe.org>
2015-11-25 14:23:50 -0800

    Delete _drush_pring_log, and move implementation to Drush\Log\Logger.

--- lib/Drush/Log/Logger.php
+++ lib/Drush/Log/Logger.php
@@ -28,8 +28,84 @@ class Logger extends AbstractLogger {
       $log[] = $entry;
       drush_backend_packet('log', $entry);
 
-      // Just send this back to _drush_print_log for now.
-      // TODO: move _drush_print_log implementation inside this class.
-      _drush_print_log($entry);
+      if (drush_get_context('DRUSH_NOCOLOR')) {
+        $red = "[%s]";
+        $yellow = "[%s]";
+        $green = "[%s]";
+      }
+      else {
+        $red = "\033[31;40m\033[1m[%s]\033[0m";
+        $yellow = "\033[1;33;40m\033[1m[%s]\033[0m";
+        $green = "\033[1;32;40m\033[1m[%s]\033[0m";
+      }
+
+      $verbose = drush_get_context('DRUSH_VERBOSE');
+      $debug = drush_get_context('DRUSH_DEBUG');
+
+      switch ($level) {
+        case 'warning' :
+        case 'cancel' :
+          $type_msg = sprintf($yellow, $level);
+          break;
+        case 'failed' :
+        case 'error' :
+          $type_msg = sprintf($red, $level);
+          break;
+        case 'ok' :
+        case 'completed' :
+        case 'success' :
+        case 'status':
+          // In quiet mode, suppress progress messages
+          if (drush_get_context('DRUSH_QUIET')) {
+            return TRUE;
+          }
+          $type_msg = sprintf($green, $level);
+          break;
+        case 'notice' :
+        case 'message' :
+        case 'info' :
+          if (!$verbose) {
+            // print nothing. exit cleanly.
+            return TRUE;
+          }
+          $type_msg = sprintf("[%s]", $level);
+          break;
+        default :
+          if (!$debug) {
+            // print nothing. exit cleanly.
+            return TRUE;
+          }
+          $type_msg = sprintf("[%s]", $level);
+          break;
+      }
+
+      // When running in backend mode, log messages are not displayed, as they will
+      // be returned in the JSON encoded associative array.
+      if (drush_get_context('DRUSH_BACKEND')) {
+        return;
+      }
+
+      $columns = drush_get_context('DRUSH_COLUMNS', 80);
+
+      $width[1] = 11;
+      // Append timer and memory values.
+      if ($debug) {
+        $timer = sprintf('[%s sec, %s]', round($entry['timestamp']-DRUSH_REQUEST_TIME, 2), drush_format_size($entry['memory']));
+        $entry['message'] = $entry['message'] . ' ' . $timer;
+      }
+
+      $width[0] = ($columns - 11);
+
+      $format = sprintf("%%-%ds%%%ds", $width[0], $width[1]);
+
+      // Place the status message right aligned with the top line of the error message.
+      $message = wordwrap($entry['message'], $width[0]);
+      $lines = explode("\n", $message);
+      $lines[0] = sprintf($format, $lines[0], $type_msg);
+      $message = implode("\n", $lines);
+      drush_print($message, 0, STDERR);
+
     }
+
+
 }
