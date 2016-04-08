8e7a40e56dbf04a6dcb05890ad92a64ecc6cf504
Moshe Weitzman <weitzman@tejasa.com>
2016-04-08 10:07:33 -0400

    Start testing with Drupal 8.1.x instead of 8.0.x.

--- tests/Unish/UnishTestCase.php
+++ tests/Unish/UnishTestCase.php
@@ -320,7 +320,7 @@ abstract class UnishTestCase extends \PHPUnit_Framework_TestCase {
 
     if ($version_string == 8) {
       // We want to track Drupal 8 very closely.
-      $version_string = '8.0.x';
+      $version_string = '8.1.x';
       $options['no-md5'] = NULL;
     }
 
