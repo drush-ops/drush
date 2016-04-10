3a2552a0cf35dd52de7f83c8c29a29a56cad8c28
Moshe Weitzman <weitzman@tejasa.com>
2016-04-10 00:07:26 -0400

    D8 got rid of batch table (3rd try).

--- tests/sqlDumpTest.php
+++ tests/sqlDumpTest.php
@@ -50,7 +50,7 @@ class SqlDumpTest extends CommandUnishTestCase {
     $this->assertFileExists($full_dump_file_path);
     $full_dump_file = file_get_contents($full_dump_file_path);
     // Test that we have sane contents.
-    $this->assertContains('batch', $full_dump_file);
+    $this->assertContains('queue', $full_dump_file);
     // Test skip-files-list and wildcard expansion.
     $this->assertNotContains('history', $full_dump_file);
     // Next, set up an alias file and run a couple of simulated
@@ -62,7 +62,7 @@ class SqlDumpTest extends CommandUnishTestCase {
     $this->assertFileExists($full_dump_file_path);
     $full_dump_file = file_get_contents($full_dump_file_path);
     // Test that we have sane contents.
-    $this->assertContains('batch', $full_dump_file);
+    $this->assertContains('queue', $full_dump_file);
     // Test skip-files-list and wildcard expansion.
     $this->assertContains('history', $full_dump_file);
 
