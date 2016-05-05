a356903c4d3028a0a58f0e66e40fa88eafc0fe43
Jonathan Hedstrom <jhedstrom@gmail.com>
2016-05-05 11:39:46 -0700

    Merge pull request #1876 from matthijs-va/make-update-fixed-core
    Unwanted core upgrade when using make-update
    
    Signed-off-by: Jonathan Hedstrom <jhedstrom@gmail.com>

--- lib/Drush/UpdateService/StatusInfoDrush.php
+++ lib/Drush/UpdateService/StatusInfoDrush.php
@@ -90,7 +90,8 @@ class StatusInfoDrush implements StatusInfoInterface {
         continue;
       }
       drush_log(dt('Checking available update data for !project.', array('!project' => $project['label'])), LogLevel::OK);
-      $request = pm_parse_request($project_name, NULL, $projects);
+      $request = $project_name . (isset($project['core']) ? '-' . $project['core'] : '');
+      $request = pm_parse_request($request, NULL, $projects);
       $project_release_info = $release_info->get($request);
       if ($project_release_info) {
         $available[$project_name] = $project_release_info;
