953cfa29308bb93a6b80975a1179466a0f402e70
Damian Lee <damian@tag1consulting.com>
2016-05-11 10:43:29 +0100

    Move queue deletion test into a separate test method

--- tests/queueTest.php
+++ tests/queueTest.php
@@ -45,6 +45,32 @@ class QueueCase extends CommandUnishTestCase {
     $output = trim($this->getOutput());
     $parts = explode(",", $output);
     $this->assertEquals(str_replace('%items', 0, $expected), $output, 'Queue item processed.');
+  }
+
+  /**
+   * Tests the queue-delete command.
+   */
+  public function testQueueDelete() {
+    if (UNISH_DRUPAL_MAJOR_VERSION == 6) {
+      $this->markTestSkipped("Queue API not available in Drupal 6.");
+    }
+
+    if (UNISH_DRUPAL_MAJOR_VERSION == 7) {
+      $expected = 'aggregator_feeds,%items,SystemQueue';
+    }
+    else {
+      $expected = 'aggregator_feeds,%items,Drupal\Core\Queue\DatabaseQueue';
+    }
+
+    $sites = $this->setUpDrupal(1, TRUE);
+    $options = array(
+      'yes' => NULL,
+      'root' => $this->webroot(),
+      'uri' => key($sites),
+    );
+
+    // Enable aggregator since it declares a queue.
+    $this->drush('pm-enable', array('aggregator'), $options);
 
     // Add another item to the queue and make sure it was deleted.
     $this->drush('php-script', array('queue_script-D' . UNISH_DRUPAL_MAJOR_VERSION), $options + array('script-path' => dirname(__FILE__) . '/resources'));
@@ -58,4 +84,5 @@ class QueueCase extends CommandUnishTestCase {
     $output = trim($this->getOutput());
     $this->assertEquals(str_replace('%items', 0, $expected), $output, 'Queue was successfully deleted.');
   }
+
 }
