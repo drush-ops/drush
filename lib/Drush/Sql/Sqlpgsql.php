7039feff0fa6dfe35fe054d5e8cb3b51109cae8b
Miklós Kasza <kaszi@jokeman.hu>
2016-07-12 19:40:44 +0200

    Changed single quotes to double quotes in extra parameters (Sqlpgsql::$query_extra) for psql queries under windows. (#2252)

--- lib/Drush/Sql/Sqlpgsql.php
+++ lib/Drush/Sql/Sqlpgsql.php
@@ -6,7 +6,7 @@ define('PSQL_SHOW_TABLES', "SELECT tablename FROM pg_tables WHERE schemaname='pu
 
 class Sqlpgsql extends SqlBase {
 
-  public $query_extra = "--no-align --field-separator='\t' --pset tuples_only=on";
+  public $query_extra = "--no-align --field-separator=\"\t\" --pset tuples_only=on";
 
   public $query_file = "--file";
 
