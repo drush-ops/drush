e15e3499ac96daee6ea229f1f80cfea97c9a5ace
Moshe Weitzman <weitzman@tejasa.com>
2016-08-20 10:21:31 -0400

    Up PHP requirement to 5.6+ since 5.5 is now EOL. We should still code for 5.4+, in order to ease backporting to 8.x branch.

--- drush.php
+++ drush.php
@@ -5,7 +5,7 @@
  * @file
  * Drush is a PHP script implementing a command line shell for Drupal.
  *
- * @requires PHP CLI 5.5.0, or newer.
+ * @requires PHP CLI 5.6.0, or newer.
  */
 
 require __DIR__ . '/includes/preflight.inc';
