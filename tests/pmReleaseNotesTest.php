189a0803ddc6ee84634590d067f0a9752ae96147
Moshe Weitzman <weitzman@tejasa.com>
2016-08-31 19:57:07 -0400

    Break up testReleaseNotes for better Windows compat.

--- tests/pmReleaseNotesTest.php
+++ tests/pmReleaseNotesTest.php
@@ -13,15 +13,9 @@ class pmReleaseNotesCase extends CommandUnishTestCase {
   public function testReleaseNotes() {
     $this->drush('pm-releasenotes', array('drupal-7.1'));
     $output = $this->getOutput();
-    $expected = <<< EXPECTED
-------------------------------------------------------------------------------
-> RELEASE NOTES FOR 'DRUPAL' PROJECT, VERSION 7.1:
-> Last updated:  May 25, 2011 - 20:59.
-> Security
-------------------------------------------------------------------------------
-EXPECTED;
-    $this->assertContains($expected, $output, 'Header is fine.');
-    $this->assertContains('SA-CORE-2011-001 - Drupal core - Multiple vulnerabilities', $output, 'Release notes includes SA reference.');
+    $this->assertContains("RELEASE NOTES FOR 'DRUPAL' PROJECT, VERSION 7.1", $output);
+    $this->assertContains('Last updated:  May 25, 2011 - 20:59', $output);
+    $this->assertContains('SA-CORE-2011-001 - Drupal core - Multiple vulnerabilities', $output);
   }
 }
 
