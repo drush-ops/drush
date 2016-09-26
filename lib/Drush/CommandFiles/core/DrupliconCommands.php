1a1bf284b49ce9762f77e535ef72a9395410191a
Moshe Weitzman <weitzman@tejasa.com>
2016-09-26 12:25:17 -0400

    Use latest release from annotatedCommand.

--- lib/Drush/CommandFiles/core/DrupliconCommands.php
+++ lib/Drush/CommandFiles/core/DrupliconCommands.php
@@ -3,24 +3,13 @@ namespace Drush\CommandFiles\core;
 
 class DrupliconCommands {
 
-  /*
-   * @todo not working until we modernize drush_get_global_options().
-   * @hook alter help
-   */
-  public function help($command) {
-    if ($command['command'] == 'global-options' && $command['#brief'] === FALSE) {
-      $command['options']['druplicon'] = [
-        'description' => 'Shows the druplicon as glorious ASCII art.',
-      ];
-    }
-  }
-
   /**
    * Print druplicon as post-command output.
    *
-   * @hook post-command
+   * @hook post-command *
+   * @option druplicon Shows the druplicon as glorious ASCII art.
    */
-  public function append() {
+  public function druplicon() {
     if (drush_get_option('druplicon')) {
       $misc_dir = DRUSH_BASE_PATH . '/misc';
       if (drush_get_context('DRUSH_NOCOLOR')) {
