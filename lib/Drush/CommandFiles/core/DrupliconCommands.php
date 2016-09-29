50cc46b4786d5ea78f52f26072bd9f4efaf68814
Greg Anderson <greg.1.anderson@greenknowe.org>
2016-09-29 15:46:14 -0700

    Don't call drush_get_option from within a @hook.

--- lib/Drush/CommandFiles/core/DrupliconCommands.php
+++ lib/Drush/CommandFiles/core/DrupliconCommands.php
@@ -9,8 +9,8 @@ class DrupliconCommands {
    * @hook post-command *
    * @option $druplicon Shows the druplicon as glorious ASCII art.
    */
-  public function druplicon() {
-    if (drush_get_option('druplicon')) {
+  public function druplicon($result, $argsAndOptions, $annotationData) {
+    if ($argsAndOptions['options']['druplicon']) {
       $misc_dir = DRUSH_BASE_PATH . '/misc';
       if (drush_get_context('DRUSH_NOCOLOR')) {
         $content = file_get_contents($misc_dir . '/druplicon-no_color.txt');
