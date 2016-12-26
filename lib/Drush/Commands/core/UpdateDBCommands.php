67c9fca05aaf432ec4857aebfd259ce7ce7d1230
Moshe Weitzman <weitzman@tejasa.com>
2016-12-26 11:11:20 -0500

    Fix bootstrap level for updbst.

--- lib/Drush/Commands/core/UpdateDBCommands.php
+++ lib/Drush/Commands/core/UpdateDBCommands.php
@@ -66,7 +66,7 @@ class UpdateDBCommands extends DrushCommands {
    * @command updatedb-status
    * @option cache-clear Set to 0 to suppress normal cache clearing; the caller should then clear if needed.
    * @option entity-updates Run automatic entity schema updates at the end of any update hooks. Defaults to --no-entity-updates.
-   * @bootstrap DRUSH_BOOTSTRAP_DRUPAL_SITE
+   * @bootstrap DRUSH_BOOTSTRAP_DRUPAL_FULL
    * @aliases updbst
    * @field-labels
    *   module: Module
