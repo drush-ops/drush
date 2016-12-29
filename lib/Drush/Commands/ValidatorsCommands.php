6a62ea97dd76ecdf4047b28a93598b97ca818d9f
Moshe Weitzman <weitzman@tejasa.com>
2016-12-28 23:54:45 -0500

    Validate arguments for image commands

--- lib/Drush/Commands/ValidatorsCommands.php
+++ lib/Drush/Commands/ValidatorsCommands.php
@@ -17,12 +17,34 @@ class ValidatorsCommands {
    * @param \Consolidation\AnnotatedCommand\CommandData $commandData
    * @return \Consolidation\AnnotatedCommand\CommandError|null
    */
-  public function validate_entity_load(CommandData $commandData) {
+  public function validateEntityLoad(CommandData $commandData) {
     list($entity_type, $arg_name) = explode(' ', $commandData->annotationData()->get('validate-entity-load', NULL));
     $names = _convert_csv_to_array($commandData->input()->getArgument($arg_name));
     $loaded = \Drupal::entityTypeManager()->getStorage($entity_type)->loadMultiple($names);
     if ($missing = array_diff($names, array_keys($loaded))) {
       $msg = dt('Unable to load the !type: !str', ['!type' => $entity_type, '!str' => implode(', ', $missing)]);
+      return new CommandError($msg);
+    }
+  }
+
+  /**
+   * Validate that the file path exists.
+
+   * @hook validate @validate-file-exists
+   * @param \Consolidation\AnnotatedCommand\CommandData $commandData
+   * @return \Consolidation\AnnotatedCommand\CommandError|null
+   */
+  public function validateFileExists(CommandData $commandData) {
+    $arg_names = _convert_csv_to_array($commandData->annotationData()->get('validate-file-exists', NULL));
+    foreach ($arg_names as $arg_name) {
+      $path = $commandData->input()->getArgument($arg_name);
+      if (!file_exists($path)) {
+        $missing[] = $path;
+      }
+    }
+
+    if ($missing) {
+      $msg = dt('File(s) not found: !paths', ['!paths' => implode(', ', $missing)]);
       return new CommandError($msg);
     }
   }
--- lib/Drush/Commands/core/ImageCommands.php
+++ lib/Drush/Commands/core/ImageCommands.php
@@ -10,7 +10,7 @@ class ImageCommands extends DrushCommands {
    * Flush all derived images for a given style.
    *
    * @command image-flush
-   * @param $style An image style machine name. If not provided, user may choose from a list of names.
+   * @param $stylename An image style machine name. If not provided, user may choose from a list of names.
    * @option all Flush all derived images
    * @usage drush image-flush
    *   Pick an image style and then delete its images.
@@ -18,34 +18,62 @@ class ImageCommands extends DrushCommands {
    *   Delete all thumbnail images.
    * @usage drush image-flush --all
    *   Flush all derived images. They will be regenerated on the fly.
+   * @validate-entity-load image_style style
    * @aliases if
    * @bootstrap DRUSH_BOOTSTRAP_DRUPAL_FULL
    * @todo @complete
    */
-  public function flush($style = NULL, $options = ['all' => FALSE]) {
-    drush_include_engine('drupal', 'image');
+  public function flush($stylename = NULL, $options = ['all' => FALSE]) {
     if ($options['all']) {
-      $style_name = 'all';
-    }
-
-    if (empty($style_name)) {
-      $styles = array_keys(drush_image_styles());
-      $choices = array_combine($styles, $styles);
-      $choices = array_merge(array('all' => 'all'), $choices);
-      $style_name = drush_choice($choices, dt("Choose a style to flush."));
-      if ($style_name === FALSE) {
-        return drush_user_abort();
-      }
-    }
-
-    if ($style_name == 'all') {
-      foreach (drush_image_styles() as $style_name => $style) {
-        drush_image_flush_single($style_name);
+      foreach (\Drupal::entityTypeManager()->getStorage('image_style')->loadMultiple() as $style_name => $style) {
+        if ($style = \Drupal::entityTypeManager()->getStorage('image_style')->load($style_name)) {
+          $style->flush();
+          $this->logger()->success(dt('Image style !style_name flushed', array('!style_name' => $style_name)));
+        }
       }
       $this->logger()->success(dt('All image styles flushed'));
     }
     else {
-      drush_image_flush_single($style_name);
+      if (empty($style_name)) {
+        $styles = array_keys(\Drupal::entityTypeManager()
+          ->getStorage('image_style')
+          ->loadMultiple());
+        $choices = array_combine($styles, $styles);
+        $choices = array_merge(array('all' => 'all'), $choices);
+        $style_name = drush_choice($choices, dt("Choose a style to flush."));
+        if ($style_name === FALSE) {
+          return drush_user_abort();
+        }
+      }
+
+      if ($style = \Drupal::entityTypeManager()->getStorage('image_style')->load($style_name)) {
+        $style->flush();
+        $this->logger()->success(dt('Image style !style_name flushed', array('!style_name' => $style_name)));
+      }
+      elseif ($style_name == 'all') {
+        self::flush($style_name, ['all' => TRUE]);
+      }
+    }
+  }
+
+  /**
+   * Create an image derivative.
+   *
+   * @command image-derive
+   * @param $style_name An image style machine name.
+   * @param $source Path to a source image. Optionally prepend stream wrapper scheme.
+   * @usage drush image-derive thumbnail core/themes/bartik/screenshot.png
+   *   Save thumbnail sized derivative of logo image.
+   * @validate-entity-load image_style style
+   * @validate-file-exists source
+   * @bootstrap DRUSH_BOOTSTRAP_DRUPAL_FULL
+   * @aliases id
+   */
+  public function derive($style_name, $source)  {
+    $image_style = \Drupal::entityTypeManager()->getStorage('image_style')->load($style_name);
+    $derivative_uri = $image_style->buildUri($source);
+    if ($image_style->createDerivative($source, $derivative_uri)) {
+      return $derivative_uri;
     }
   }
 
