af3aeb3cddb41270a5b0295dd65c63d7a63068c0
Moshe Weitzman <weitzman@tejasa.com>
2017-02-05 06:33:00 -0500

    Rename valid_credentials(() to hasPDO()

--- lib/Drush/Sql/SqlVersion.php
+++ lib/Drush/Sql/SqlVersion.php
@@ -22,12 +22,11 @@ class SqlVersion {
   public function getAll() {}
 
   /*
-   * Validate that Drupal can connect to the DB without actually using Drupal to do so. Called
-   * by drush_valid_db_credentials().
+   * Validate that required PDO PHP extension is available.
    */
-  public function valid_credentials($db_spec) {
-    // Drupal >=7 requires PDO and Drush requires php 5.6+ which ships with PDO
-    // but it may be compiled with --disable-pdo.
+  public function hasPDO() {
+    // Drupal requires PDO, and Drush requires php 5.6+ which ships with PDO
+    // but PHP may be compiled with --disable-pdo.
     if (!class_exists('\PDO')) {
       drush_log(dt('PDO support is required.'), LogLevel::BOOTSTRAP);
       return FALSE;
