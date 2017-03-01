dc7541a664960073f971c97b4ed7862c5d783319
Moshe Weitzman <weitzman@tejasa.com>
2017-03-01 17:05:45 -0500

    Remove legacy api examples.

--- drush.api.php
+++ drush.api.php
@@ -1,208 +1,6 @@
 <?php
 
 /**
- * @file
- * Documentation of the Drush API.
- */
-
-/**
- * Declare a new command.
- */
-function hook_drush_command() {
-  // To learn more, run `drush topic docs-commands` and
-  // `drush topic docs-examplecommand`.
-}
-
-/**
- * All Drush commands are invoked in a specific order, using
- * drush-made hooks, very similar to the Drupal hook system. See drush_invoke()
- * for the actual implementation.
- *
- * For any commandfile named "hook", the following hooks are called, in
- * order, for the command "COMMAND":
- *
- * 0. drush_COMMAND_init()
- * 1. drush_hook_COMMAND_pre_validate()
- * 2. drush_hook_COMMAND_validate()
- * 3. drush_hook_pre_COMMAND()
- * 4. drush_hook_COMMAND()
- * 5. drush_hook_post_COMMAND()
- *
- * For example, here are the hook opportunities for a mysite.drush.inc file
- * that wants to hook into the `pm-download` command.
- *
- * 1. drush_mysite_pm_download_pre_validate()
- * 2. drush_mysite_pm_download_validate()
- * 3. drush_mysite_pre_pm_download()
- * 4. drush_mysite_pm_download()
- * 5. drush_mysite_post_pm_download()
- *
- * Note that the drush_COMMAND_init() hook is only for use by the
- * commandfile that defines the command.
- *
- * If any of hook function fails, either by calling drush_set_error
- * or by returning FALSE as its function result, then the rollback
- * mechanism is called.  To fail with an error, call drush_set_error:
- *
- *   return drush_set_error('MY_ERROR_CODE', dt('Error message.'));
- *
- * To allow the user to confirm or cancel a command, use drush_confirm
- * and drush_user_abort:
- *
- *   if (!drush_confirm(dt('Are you sure?'))) {
- *     return drush_user_abort();
- *   }
- *
- * The rollback mechanism will call, in reverse, all _rollback hooks.
- * The mysite command file can implement the following rollback hooks:
- *
- * 1. drush_mysite_post_pm_download_rollback()
- * 2. drush_mysite_pm_download_rollback()
- * 3. drush_mysite_pre_pm_download_rollback()
- * 4. drush_mysite_pm_download_validate_rollback()
- * 5. drush_mysite_pm_download_pre_validate_rollback()
- *
- * Before any command is called, hook_drush_init() is also called.
- * hook_drush_exit() is called at the very end of command invocation.
- *
- * @see includes/command.inc
- *
- * @see hook_drush_init()
- * @see drush_COMMAND_init()
- * @see drush_hook_COMMAND_pre_validate()
- * @see drush_hook_COMMAND_validate()
- * @see drush_hook_pre_COMMAND()
- * @see drush_hook_COMMAND()
- * @see drush_hook_post_COMMAND()
- * @see drush_hook_post_COMMAND_rollback()
- * @see drush_hook_COMMAND_rollback()
- * @see drush_hook_pre_COMMAND_rollback()
- * @see drush_hook_COMMAND_validate_rollback()
- * @see drush_hook_COMMAND_pre_validate_rollback()
- * @see hook_drush_exit()
- */
-
-/**
- * @addtogroup hooks
- * @{
- */
-
-/**
- * Take action before any command is run.
- *
- * Logging an error stops command execution.
- */
-function hook_drush_init() {
-
-}
-
-/**
- * Initialize a command prior to validation.
- *
- * If a command needs to bootstrap to a higher level, this is best done in the
- * command init hook.  It is permisible to bootstrap in any hook, but note that
- * if bootstrapping adds more commandfiles (*.drush.inc) to the commandfile
- * list, the newly-added commandfiles will not have any hooks called until the
- * next phase.  For example, a command that calls drush_bootstrap_max() in
- * drush_hook_COMMAND() would only permit commandfiles from modules enabled in
- * the site to participate in drush_hook_post_COMMAND() hooks.
- */
-function drush_COMMAND_init() {
-  drush_bootstrap_max();
-}
-
-/**
- * Run before a specific command validates.
- *
- * Logging an error stops command execution, and the rollback function (if any)
- * for each hook implementation is invoked.
- *
- * @see drush_hook_COMMAND_pre_validate_rollback()
- */
-function drush_hook_COMMAND_pre_validate() {
-
-}
-
-/**
- * Run before a specific command executes.
- *
- * Logging an error stops command execution, and the rollback function (if any)
- * for each hook implementation is invoked.
- *
- * @see drush_hook_COMMAND_validate_rollback()
- */
-function drush_hook_COMMAND_validate() {
-
-}
-
-/**
- * Run before a specific command executes.
- *
- * Logging an error stops command execution, and the rollback function (if any)
- * for each hook implementation is invoked, in addition to the validate
- * rollback.
- *
- * @see drush_hook_pre_COMMAND_rollback()
- * @see drush_hook_COMMAND_validate_rollback()
- */
-function drush_hook_pre_COMMAND() {
-
-}
-
-/**
- * Implementation of the actual drush command.
- *
- * This is where most of the stuff should happen.
- *
- * Logging an error stops command execution, and the rollback function (if any)
- * for each hook implementation is invoked, in addition to pre and
- * validate rollbacks.
- *
- * @return mixed|false
- *   The return value will be passed along to the caller if --backend option is
- *   present. A boolean FALSE indicates failure and rollback will be inititated.
- *
- * @see drush_hook_COMMAND_rollback()
- * @see drush_hook_pre_COMMAND_rollback()
- * @see drush_hook_COMMAND_validate_rollback()
- */
-function drush_hook_COMMAND() {
-
-}
-
-/**
- * Run after a specific command executes.
- *
- * Logging an error stops command execution, and the rollback function (if any)
- * for each hook implementation is invoked, in addition to pre, normal
- * and validate rollbacks.
- *
- * @see drush_hook_post_COMMAND_rollback()
- * @see drush_hook_COMMAND_rollback()
- * @see drush_hook_pre_COMMAND_rollback()
- * @see drush_hook_COMMAND_validate_rollback()
- */
-function drush_hook_post_COMMAND() {
-
-}
-
-/**
- * Take action after any command is run.
- */
-function hook_drush_exit() {
-
-}
-
-/**
- * Adjust the contents of any command structure prior to dispatch.
- *
- * @see core_drush_command_alter()
- */
-function hook_drush_command_alter(&$command) {
-
-}
-
-/**
  * Adjust the contents of a site alias.
  */
 function hook_drush_sitealias_alter(&$alias_record) {
@@ -223,12 +21,6 @@ function hook_drush_sitealias_alter(&$alias_record) {
  */
 function hook_drush_pm_post_download($project, $release) {
 
-}
-
-/**
- * Take action after a project has been updated.
- */
-function hook_pm_post_update($project_name, $installed_release, $project) {
 }
 
 /**
@@ -280,7 +72,6 @@ function drush_hook_pre_pm_enable() {
  */
 function sanitize() {}
 function messages() {}
-}
 
 /**
  * Add help components to a command.
@@ -296,19 +87,6 @@ function hook_drush_help_alter(&$command) {
       $command['options']['myglobaloption'] = 'Description of option used globally in all commands (e.g. in a commandfile init hook)';
     }
   }
-}
-
-/**
- * Add/edit options to cache-clear command.
- *
- * @param array $types
- *   Adjust types as needed. Is passed by reference.
- *
- * @param bool $include_bootstrapped_types
- *   If FALSE, omit types which require a FULL bootstrap.
- */
-function hook_drush_cache_clear(&$types, $include_bootstrapped_types) {
-  $types['views'] = 'views_invalidate_cache';
 }
 
 /*
