3b207a5b99b45c4c0b0fbe7c839a67a6c648afc4
Moshe Weitzman <weitzman@tejasa.com>
2017-03-15 23:44:56 -0400

    Move code into Formatter

--- src/Commands/internal/HelpCommands.php
+++ src/Commands/internal/HelpCommands.php
@@ -4,8 +4,6 @@ namespace Drush\Commands\internal;
 use Drush\Commands\core\TopicCommands;
 use Drush\Commands\DrushCommands;
 use Symfony\Component\Console\Application;
-use Symfony\Component\Console\Helper\Table;
-use Symfony\Component\Console\Helper\TableCell;
 
 class HelpCommands extends DrushCommands {
 
@@ -108,60 +106,6 @@ class HelpCommands extends DrushCommands {
     $xml->appendChild($xml_aliases);
 
     return $xml;
-
-
-    // Everything below will move to a custom output formatter.
-    $table = new Table($this->output());
-    $table->setStyle('compact');
-
-    // How best to output this?
-    drush_print($command->getDescription());
-
-    if ($usages = $command->getExampleUsages()) {
-      $table->addRow(['','']);
-      $table->addRow([new TableCell('Examples:', array('colspan' => 2))]);
-      foreach ($usages as $key => $description) {
-        $table->addRow(['  ' . $key, $description]);
-      }
-    }
-
-    if ($arguments = $def->getArguments()) {
-      $table->addRow(['','']);
-      $table->addRow([new TableCell('Arguments:', array('colspan' => 2))]);
-      foreach ($arguments as $argument) {
-        $formatted = $this->formatArgumentName($argument);
-        $description = $argument->getDescription();
-        if ($argument->getDefault()) {
-          $description .= ' [default: ' . $argument->getDefault() . ']';
-        }
-        $table->addRow(['  ' . $formatted, $description]);
-      }
-    }
-
-    if ($options = $def->getOptions()) {
-      $table->addRow(['','']);
-      $table->addRow([new TableCell('Options:', array('colspan' => 2))]);
-      foreach ($options as $option) {
-        $formatted = $this->formatOption($option);
-        $table->addRow(['  ' . $formatted, $option->getDescription()]);
-      }
-    }
-
-    if ($topics = $command->getTopics()) {
-      $table->addRow(['','']);
-      $table->addRow([new TableCell('Topics:', array('colspan' => 2))]);
-      foreach ($topics as $topic) {
-        // @todo deal with long descriptions
-        $table->addRow(['  ' . $topic, substr($allTopics[$topic]['description'], 0, 30)]);
-      }
-    }
-
-    if ($aliases = $command->getAliases()) {
-      $table->addRow(['','']);
-      $table->addRow([new TableCell('Aliases: '. implode(' ', $aliases), array('colspan' => 2))]);
-    }
-
-    $table->render();
   }
 
   function formatOption($option) {
