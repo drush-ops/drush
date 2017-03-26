0728bdfd5603aa7df3d178856fb4b31e1f88d477
Moshe Weitzman <weitzman@tejasa.com>
2017-03-26 08:56:52 -0400

    Test against stable Drupal and highest Drupal (8.4.x) (#2690)

--- unish.sut.php
+++ unish.sut.php
@@ -18,35 +18,11 @@ function unish_validate() {
     fwrite(STDERR, 'The drush directory must end in /drush in order to run the tests. This is due to the "path" repository in tests/resources/composer.json');
     exit(1);
   }
-
-  /**
-   * Assure that the composer.lock and the composer.json in the /tests directory
-   * are in sync.
-   *
-   * Based on http://stackoverflow.com/a/28730898/265501.
-   *
-   * @todo. Not sure this is feasible since multiple authors will update the lockfile from different path/to/drush.
-   */
-//  $codebase = __DIR__ . '/tests/resources/codebase';
-//  // If composer.lock is missing then no need for this check.
-//  $lockfile = $codebase . '/composer.lock';
-//  if (file_exists($lockfile)) {
-//    $lock = json_decode(file_get_contents($lockfile))->{'content-hash'};
-//    $json = md5(replace_token(file_get_contents($codebase . '/composer.json')));
-//
-//    if ($lock !== $json) {
-//      fwrite(STDERR, "$lockfile file out of sync with its composer.json.\n");
-//      exit(1);
-//    }
-//
-//    fwrite(STDERR, "$lockfile file up to date with its composer.json.\n");
-//    exit(0);
-//  }
 }
 
 /**
  * Use Composer to build a Drupal codebase, with this Drush symlinked into /vendor.
- * @param $unish_sandbox Path to sandbox.
+ * @param string $unish_sandbox Path to sandbox.
  * @return integer
  *   Exit code.
  */
@@ -55,7 +31,8 @@ function unish_setup_sut($unish_sandbox) {
   drush_delete_dir($working_dir, TRUE);
   $codebase = 'tests/resources/codebase';
   drush_copy_dir($codebase, $working_dir);
-  foreach (['composer.json', 'composer.lock'] as $filename) {
+  $composer_json = getenv('COMPOSER') ?: 'composer.json';
+  foreach ([$composer_json] as $filename) {
     $path = $working_dir . "/$filename";
     if (file_exists($path)) {
       $contents = file_get_contents($path);
@@ -63,14 +40,19 @@ function unish_setup_sut($unish_sandbox) {
       file_put_contents($path, $new_contents);
     }
   }
-  // @todo Call update instead of install if specified on the CLI. Useful need we need to update composer.lock.
-  // We also need to put back the %PATH-TO-DRUSH% token by hand or automatically.
-  // For option parsing, see built-in getopt() function.
-  $cmd = 'composer install --no-interaction --no-progress --no-suggest --working-dir ' . escapeshellarg($working_dir);
+
+  // getopt() is awkward
+  $verbose = NULL;
+  foreach (['-v','-vv','-vvv','--verbose'] as $needle) {
+    if (in_array($needle, $_SERVER['argv'])) {
+      $verbose = $needle;
+    }
+  }
+  $cmd = "composer $verbose install --no-interaction --no-progress --no-suggest --working-dir " . escapeshellarg($working_dir);
   fwrite(STDERR, 'Executing: ' . $cmd . "\n");
   exec($cmd, $output, $return);
 
-  // @todo Not 100% sure this is needed, but I've seen Composer download a second Drush, instead of symlink.
+  // If requirements force it to, Composer downloads a second Drush, instead of symlink.
   $drush_sut = $working_dir . '/vendor/drush/drush';
   if (!is_link($drush_sut)) {
     fwrite(STDERR, "Drush not symlinked in the System-Under-Test.\n");
