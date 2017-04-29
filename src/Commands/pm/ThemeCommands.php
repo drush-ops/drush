70287962b6a9b885d134f62131fc2026e932505f
Moshe Weitzman <weitzman@tejasa.com>
2017-04-28 21:34:44 -0400

    Use DI in pm commands.

--- src/Commands/pm/ThemeCommands.php
+++ src/Commands/pm/ThemeCommands.php
@@ -1,9 +1,24 @@
 <?php
 namespace Drush\Commands\pm;
 
+use Drupal\Core\Extension\ThemeInstallerInterface;
 use Drush\Commands\DrushCommands;
 
 class ThemeCommands extends DrushCommands {
+
+  protected $themeInstaller;
+
+  public function __construct(ThemeInstallerInterface $themeInstaller) {
+    parent::__construct();
+    $this->themeInstaller = $themeInstaller;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getThemeInstaller() {
+    return $this->themeInstaller;
+  }
 
   /**
    * Enable one or more themes.
@@ -15,7 +30,7 @@ class ThemeCommands extends DrushCommands {
    */
   public function enable($themes) {
     $themes = _convert_csv_to_array($themes);
-    if (!\Drupal::service('theme_installer')->install($themes, TRUE)) {
+    if (!$this->getThemeInstaller()->install($themes, TRUE)) {
       throw new \Exception('Unable to install themes.');
     }
     $this->logger()->success(dt('Successfully enabled theme: !list', ['!list' => implode(', ', $themes)]));
@@ -31,7 +46,7 @@ class ThemeCommands extends DrushCommands {
    */
   public function uninstall($themes) {
     $themes = _convert_csv_to_array($themes);
-    if (!\Drupal::service('theme_installer')->uninstall($themes, TRUE)) {
+    if (!$this->getThemeInstaller()->uninstall($themes, TRUE)) {
       throw new \Exception('Unable to uninstall themes.');
     }
     $this->logger()->success(dt('Successfully uninstalled theme: !list', ['!list' => implode(', ', $themes)]));
