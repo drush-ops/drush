990a4b321a1b299c63a1e1eb7b26fdaf0e8c4a50
Moshe Weitzman <weitzman@tejasa.com>
2017-05-09 22:22:08 -0400

    Better help in SandwichCommands.

--- examples/Commands/SandwichCommands.php
+++ examples/Commands/SandwichCommands.php
@@ -7,7 +7,12 @@ use Consolidation\OutputFormatters\StructuredData\RowsOfFields;
 /**
  * Run these commands using the --include option - e.g. `drush --include=/path/to/drush/examples mmas`
  *
- * For an example of a Drupal module implementing commands, see http://cgit.drupalcode.org/devel/tree/devel_generate/src/Commands
+ * For an example of a Drupal module implementing commands, see
+ * - http://cgit.drupalcode.org/devel/tree/devel_generate/src/Commands
+ * - http://cgit.drupalcode.org/devel/tree/devel_generate/drush.services.yml
+ *
+ * This file is a good example of the first of those bullets (a commandfile) but
+ * since it isn't part of a module, it does not implement drush.services.yml.
  */
 
 class SandwichCommands extends DrushCommands {
