86402eb0c343cb50f50cba9a63e39bcfebd2dd39
Moshe Weitzman <weitzman@tejasa.com>
2017-05-24 07:49:48 -0400

    Add Drush9 Commandfile generator
    And remove example generator

---
+++ src/Commands/generate/Commands/DrushCommandFile.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Drush\Commands\generate\Commands;
+
+use DrupalCodeGenerator\Commands\BaseGenerator;
+use DrupalCodeGenerator\Utils;
+
+class DrushCommandFile extends BaseGenerator
+{
+  protected $name = 'drush-commandfile';
+  protected $description = 'Generates a Drush commandfile.';
+  protected $alias = 'dcf';
+  protected $templatePath = __DIR__;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function interact($input, $output) {
+
+    $questions = Utils::defaultQuestions();
+
+    $vars = $this->collectVars($input, $output, $questions);
+    $vars['class'] = Utils::camelize($vars['machine_name'] . 'Commands');
+    // $directoryBaseName = basename($this->destination);
+    $this->files['src/Commands/' . $vars['class'] . '.php'] = $this->render('drushcommandfile.twig', $vars);
+    // @todo Can only generate a file named [module].services.yml right now.
+    // $this->files['drush.services.yml'] = $this->render('drush.services.twig', $vars);
+  }
+}
