5c05d0c909aaa587ac621ac6bb0a2e470626d09d
Moshe Weitzman <weitzman@tejasa.com>
2017-05-31 10:26:44 -0400

    Move contructor to top of file.

--- tests/resources/modules/d8/woot/src/Generators/ExampleGenerator.php
+++ tests/resources/modules/d8/woot/src/Generators/ExampleGenerator.php
@@ -15,6 +15,12 @@ class ExampleGenerator extends BaseGenerator
     // We don't actually use this service. This illustrates how to inject a dependency into a Generator.
     protected $moduleHandler;
 
+    public function __construct($moduleHandler = null, $name = null)
+    {
+        parent::__construct($name);
+        $this->moduleHandler = $moduleHandler;
+    }
+
     /**
      * {@inheritdoc}
      */
@@ -25,11 +31,5 @@ class ExampleGenerator extends BaseGenerator
         $vars = $this->collectVars($input, $output, $questions);
         $vars['class'] = Utils::camelize('Example_' . $vars['machine_name'] . '_Commands');
         $this->files['src/Commands/' . $vars['class'] . '.php'] = $this->render('example-generator.twig', $vars);
-    }
-
-    public function __construct($moduleHandler = null, $name = null)
-    {
-        parent::__construct($name);
-        $this->moduleHandler = $moduleHandler;
     }
 }
