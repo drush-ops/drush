e6badc0b642c6e6965f89b6837d29a381fb665ca
Moshe Weitzman <weitzman@tejasa.com>
2017-06-04 23:32:12 -0400

    Enhance dcf generator so it can port old Drush commandfiles (#2794)

--- src/Commands/generate/Generators/Drush/DrushCommandFile.php
+++ src/Commands/generate/Generators/Drush/DrushCommandFile.php
@@ -23,12 +23,52 @@ class DrushCommandFile extends BaseGenerator
      */
     protected function interact(InputInterface $input, OutputInterface $output)
     {
-        $questions = Utils::defaultQuestions();
+        $questions = Utils::defaultQuestions() + [
+            'source' => ['Absolute path to legacy Drush commandfile (optional - for porting)', null, ''],
+        ];
 
         $vars = $this->collectVars($input, $output, $questions);
         $vars['class'] = Utils::camelize($vars['machine_name'] . 'Commands');
-
+        if ($vars['source']) {
+            require_once $vars['source'];
+            $filename = str_replace(['.drush.inc', '.drush8.inc'], '', basename($vars['source']));
+            $commands = call_user_func($filename . '_drush_command');
+            $vars['commands'] = $this->adjustCommands($commands);
+        }
         $this->setFile('src/Commands/' . $vars['class'] . '.php', 'drush-command-file.twig', $vars);
         $this->setServicesFile('drush.services.yml', 'drush.services.twig', $vars);
+    }
+
+    protected function adjustCommands($commands)
+    {
+        foreach ($commands as $name => &$command) {
+            $command['method'] = $name;
+            if (($pos = strpos($name, '-')) !== false) {
+                $command['method'] = substr($name, $pos + 1);
+            }
+            $command['method'] = Utils::camelize($command['method'], false);
+            if ($command['options']) {
+                foreach ($command['options'] as $oName => &$option) {
+                    // We only care about option description so make value a simple string.
+                    if (is_array($option)) {
+                        $option = $option['description'];
+                    }
+                    $oNames[] = "'$oName' => null";
+                }
+                $command['optionsConcat'] = '$options = [' . implode(', ', $oNames) . ']';
+                unset($oNames);
+            }
+            if ($command['arguments']) {
+                foreach ($command['arguments'] as $aName => $desciption) {
+                    // Prepend name with a '$'.
+                    $command['arguments']['$' . $aName] = $desciption;
+                    unset($command['arguments'][$aName]);
+                }
+                if ($concat = implode(', ', array_keys($command['arguments']))) {
+                    $command['argumentsConcat'] = $concat . ', ';
+                }
+            }
+        }
+        return $commands;
     }
 }
