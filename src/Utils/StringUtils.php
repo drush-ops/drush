c21855701055188b65a6a72e80751000fd1bf63b
Moshe Weitzman <weitzman@tejasa.com>
2017-06-19 02:42:20 +0200

    Move csv parsing function to a utility class.

---
+++ src/Utils/StringUtils.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Drush\Utils;
+
+class StringUtils {
+
+    /**
+     * Convert a csv string, or an array of items which
+     * may contain csv strings, into an array of items.
+     *
+     * @param $args
+     *   A simple csv string; e.g. 'a,b,c'
+     *   or a simple list of items; e.g. array('a','b','c')
+     *   or some combination; e.g. array('a,b','c') or array('a,','b,','c,')
+     *
+     * @returns array
+     *   A simple list of items (e.g. array('a','b','c')
+     */
+    public static function csvToArray($args) {
+        //
+        // Step 1: implode(',',$args) converts from, say, array('a,','b,','c,') to 'a,,b,,c,'
+        // Step 2: explode(',', ...) converts to array('a','','b','','c','')
+        // Step 3: array_filter(...) removes the empty items
+        // Step 4: array_map(...) trims extra whitespace from each item
+        // (handles csv strings with extra whitespace, e.g. 'a, b, c')
+        //
+        return array_map('trim', array_filter(explode(',', is_array($args) ? implode(',',$args) : $args)));
+    }
+}
