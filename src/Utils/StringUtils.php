94a613f986805860ec766ef880e2ec847c4e31e9
Greg Anderson <greg.1.anderson@greenknowe.org>
2017-07-28 15:23:22 -0700

    Factor out fallback dt() code into StringUtils::interpolate().

--- src/Utils/StringUtils.php
+++ src/Utils/StringUtils.php
@@ -28,4 +28,41 @@ class StringUtils
         //
         return array_map('trim', array_filter(explode(',', is_array($args) ? implode(',', $args) : $args)));
     }
+
+    /**
+     * Replace placeholders in a string.
+     *
+     * Examples:
+     *   interpolate('Hello, {var}', ['var' => 'world']) ==> 'Hello, world'
+     *   interpolate('Do !what', ['!what' => 'work'])    ==> 'Do work'
+     */
+    public static function interpolate($message, array $context = [])
+    {
+        // Take no action if there is no context
+        if (empty($context)) {
+            return $message;
+        }
+
+        // build a replacement array with braces around the context keys
+        $replace = array();
+        foreach ($context as $key => $val) {
+            if (!is_array($val) && (!is_object($val) || method_exists($val, '__toString'))) {
+                $replace[static::interpolationKey($key)] = $val;
+            }
+        }
+
+        // interpolate replacement values into the message and return
+        return strtr($message, $replace);
+    }
+
+    /**
+     * Wrap simple strings (with no special characters) in {}s
+     */
+    private static function interpolationKey($key)
+    {
+        if (ctype_alpha($key)) {
+            return sprintf('{%s}', $key);
+        }
+        return $key;
+    }
 }
