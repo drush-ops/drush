bf36c5f57ed2622b2911ffad9520f1ee598bff72
Moshe Weitzman <weitzman@tejasa.com>
2017-09-21 21:28:26 -0400

    Hide sha command for now.

--- src/Commands/core/ShellAliasCommands.php
+++ src/Commands/core/ShellAliasCommands.php
@@ -17,17 +17,17 @@ class ShellAliasCommands
      * @default-string-field second
      * @table-style compact
      * @usage drush shell-alias
-     *   'List all shell aliases known to Drush.'
+     *   List all shell aliases known to Drush.
      * @usage drush shell-alias pull
      *   Print the value of the shell alias 'pull'.
      * @aliases sha
-     * @todo completion not yet working for Annotated commands.
-     * @complete \Drush\Commands\core\ShellAliasCommands::complete
+     * @hidden
      *
      * @return \Consolidation\OutputFormatters\StructuredData\PropertyList
      */
     public function shellalias($alias = null, $options = ['format' => 'table'])
     {
+        // @todo Not ported to Symfony dispatch.
         $shell_aliases = drush_get_context('shell-aliases', array());
         if (!$alias) {
             return new PropertyList($shell_aliases);
@@ -35,16 +35,6 @@ class ShellAliasCommands
             return new PropertyList(array($alias => $shell_aliases[$alias]));
         } else {
             throw new \Exception(dt('Shell alias not found.'));
-        }
-    }
-
-    /*
-     * An argument provider for shell completion.
-     */
-    public static function complete()
-    {
-        if ($all = drush_get_context('shell-aliases', array())) {
-            return array('values' => array_keys($all));
         }
     }
 }
