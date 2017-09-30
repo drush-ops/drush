a62f58145f608f5005b49f29cd734b0e1e4197de
Joël Pittet <pittet@cs.ubc.ca>
2017-09-30 15:45:33 +0200

    Enable previous test coverage

--- tests/siteSetTest.php
+++ tests/siteSetTest.php
@@ -6,23 +6,28 @@ namespace Unish;
  * @group base
  * @group slow
  */
-class siteSetCommandCase extends CommandUnishTestCase {
+class SiteSetCommandCase extends CommandUnishTestCase
+{
 
-  function testSiteSet() {
-    $this->markTestSkipped('site set not implemented yet.');
-    if ($this->is_windows()) {
-      $this->markTestSkipped('Site-set not currently available on Windows.');
+    /**
+     * Test functionality of site set.
+     */
+    public function testSiteSet()
+    {
+        if ($this->is_windows()) {
+            $this->markTestSkipped('Site-set not currently available on Windows.');
+        }
+        $sites = $this->setUpDrupal(2, true);
+        $site_names = array_keys($sites);
+        $alias = '@' . $site_names[0];
+
+        $this->drush('ev', array("drush_invoke('site-set', '$alias'); print drush_sitealias_site_get();"));
+        $output = $this->getOutput();
+        $this->assertEquals($alias, $output);
+
+        $this->drush('ev', array("drush_invoke('site-set', '@none'); print drush_sitealias_site_get();"));
+        $output = $this->getOutput();
+        $this->assertEquals('', $output);
     }
-    $sites = $this->setUpDrupal(1, TRUE);
-    $site_names = array_keys($sites);
-    $alias = '@' . $site_names[0];
 
-    $this->drush('ev', array("drush_invoke('site-set', '$alias'); print drush_sitealias_site_get();"));
-    $output = $this->getOutput();
-    $this->assertEquals($alias, $output);
-
-    $this->drush('ev', array("drush_invoke('site-set', '@none'); print drush_sitealias_site_get();"));
-    $output = $this->getOutput();
-    $this->assertEquals('', $output);
-  }
 }
