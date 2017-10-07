116cb5af6dd373bcc0c1efd5da6361b486226460
Greg Anderson <greg.1.anderson@greenknowe.org>
2017-10-07 15:50:12 -0700

    Fixes #3025: Avoid validating args and options for remote proxy commands; the remote side will validate. (#3032)

--- src/Command/RemoteCommandProxy.php
+++ src/Command/RemoteCommandProxy.php
@@ -5,6 +5,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Drush\Symfony\IndiscriminateInputDefinition;
 
 use Drush\Preflight\RedispatchHook;
 
@@ -29,6 +30,9 @@ class RemoteCommandProxy extends Command
     {
         parent::__construct($name);
         $this->redispatchHook = $redispatchHook;
+
+        // Put in a special input definition to avoid option validation errors.
+        $this->setDefinition(new IndiscriminateInputDefinition());
 
         // Put in a placeholder array arguement to avoid validation errors.
         $this->addArgument(
