bd3219c2ec77da41d2c2a263b4e1357fbf938fe9
Matthew Grasmick <matt.grasmick@gmail.com>
2017-10-20 22:38:19 -0400

    Check stderr for output.

--- tests/SecurityUpdatesTest.php
+++ tests/SecurityUpdatesTest.php
@@ -15,8 +15,8 @@ class SecurityUpdatesTest extends CommandUnishTestCase {
    */
   function testInsecurePackage() {
     $this->drush('pm:security', array(), array(), NULL, NULL, self::EXIT_ERROR);
-    $this->assertContains('One or more of your dependencies has an outstanding security update. Please apply update(s) immediately.', $this->getOutput());
-    $this->assertContains('Try running: composer require drupal/alinks:^1.1 --update-with-dependencies', $this->getOutput());
+    $this->assertContains('One or more of your dependencies has an outstanding security update. Please apply update(s) immediately.', $this->getErrorOutput());
+    $this->assertContains('Try running: composer require drupal/alinks:^1.1 --update-with-dependencies', $this->getErrorOutput());
     $this->assertContains('drupal/alinks', $this->getOutput());
     $this->assertContains('1.0.0', $this->getOutput());
     $this->assertContains('1.1', $this->getOutput());
