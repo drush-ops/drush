30c35ca2ee2fd05695caa11a5bb02774869ad414
Matthew Grasmick <matt.grasmick@gmail.com>
2017-10-20 22:31:17 -0400

    Nobody expects the ... exit code of 1.

--- tests/SecurityUpdatesTest.php
+++ tests/SecurityUpdatesTest.php
@@ -14,7 +14,7 @@ class SecurityUpdatesTest extends CommandUnishTestCase {
    * Test that insecure packages are correctly identified.
    */
   function testInsecurePackage() {
-    $this->drush('pm:security');
+    $this->drush('pm:security', array(), array(), NULL, NULL, self::EXIT_ERROR);
     $this->assertContains('One or more of your dependencies has an outstanding security update. Please apply update(s) immediately.', $this->getOutput());
     $this->assertContains('Try running: composer require drupal/alinks:^1.1 --update-with-dependencies', $this->getOutput());
     $this->assertContains('drupal/alinks', $this->getOutput());
