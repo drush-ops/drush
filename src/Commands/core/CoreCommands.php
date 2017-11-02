13c667bf8705b5747fc4b1a321664c2648071aff
Moshe Weitzman <weitzman@tejasa.com>
2017-11-01 20:31:48 -0400

    Add more global options to core:global-options topic.

--- src/Commands/core/CoreCommands.php
+++ src/Commands/core/CoreCommands.php
@@ -38,6 +38,16 @@ class CoreCommands extends DrushCommands implements SiteAliasManagerAwareInterfa
                 'description' => $value->getDescription(),
             ];
         }
+
+        // Also document the key PreflightArgs. It would be possible to redundantly declare
+        // those as global options. We don't do that for now, to avoid confusion.
+        $ancient = drush_get_global_options();
+        foreach (['config', 'alias-path', 'include', 'local', 'backend', 'strict'] as $name) {
+            $rows[] = [
+                'name' => '--' . $name,
+                'description' => $ancient[$name]['description'],
+            ];
+        }
         return new RowsOfFields($rows);
     }
 
