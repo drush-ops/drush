10d6e1d94d0172d1dc657dafc4ec117ccbb74662
Moshe Weitzman <weitzman@tejasa.com>
2017-11-02 11:37:17 -0400

    Replace tilde for PreflightArgs. (#3126)

--- src/Runtime/TildeExpansionHook.php
+++ src/Runtime/TildeExpansionHook.php
@@ -5,6 +5,9 @@ namespace Drush\Runtime;
 use Consolidation\AnnotatedCommand\CommandData;
 use Consolidation\AnnotatedCommand\Hooks\ValidatorInterface;
 use Drush\Drush;
+use Drush\Utils\StringUtils;
+use Robo\Common\ConfigAwareTrait;
+use Robo\Contract\ConfigAwareInterface;
 
 /**
  * The TildeExpansionHook is installed as a preValidate hook that runs before
@@ -15,23 +18,31 @@ use Drush\Drush;
  * for commands that are redispatched to a remote site. That happens in the
  * RedispatchHook, which happens in hook init.
  */
-class TildeExpansionHook implements ValidatorInterface
+class TildeExpansionHook implements ValidatorInterface, ConfigAwareInterface
 {
+    use ConfigAwareTrait;
+
     public function validate(CommandData $commandData)
     {
+        $home_key = 'env.home';
         $input = $commandData->input();
         $args = $input->getArguments();
         $options = $input->getOptions();
-        $match = '#^~/#';
-        $replacement = Drush::config()->get('env.home') . '/';
+
         foreach ($options as $name => $value) {
-            if (is_string($value) && preg_match($match, $value)) {
-                $input->setOption($name, preg_replace($match, $replacement, $value));
+            if (is_string($value)) {
+                $replaced = StringUtils::replaceTilde($value, $this->getConfig()->get($home_key));
+                if ($value != $replaced) {
+                    $input->setOption($name, $replaced);
+                }
             }
         }
         foreach ($args as $name => $value) {
-            if (is_string($value) && preg_match($match, $value)) {
-                $input->setArgument($name, preg_replace($match, $replacement, $value));
+            if (is_string($value)) {
+                $replaced = StringUtils::replaceTilde($value, $this->getConfig()->get($home_key));
+                if ($value != $replaced) {
+                    $input->setArgument($name, $replaced);
+                }
             }
         }
     }
