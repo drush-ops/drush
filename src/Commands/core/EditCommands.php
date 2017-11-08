ff98d1c7dc409d2e3f0d61c834cfdc331b582450
Moshe Weitzman <weitzman@tejasa.com>
2017-11-08 07:26:41 -0500

    Use new home() instead of config->get(env.home)

--- src/Commands/core/EditCommands.php
+++ src/Commands/core/EditCommands.php
@@ -117,7 +117,7 @@ class EditCommands extends DrushCommands implements SiteAliasManagerAwareInterfa
                 $ini_files[$drush_ini] = $drush_ini;
             }
         }
-        foreach ([DRUSH_BASE_PATH, '/etc/drush', Drush::config()->get('env.home') . '/.drush'] as $ini_dir) {
+        foreach ([DRUSH_BASE_PATH, '/etc/drush', Drush::config()->h . '/.drush'] as $ini_dir) {
             if (file_exists($ini_dir . "/drush.ini")) {
                 $path = realpath($ini_dir . "/drush.ini");
                 $ini_files[$path] = $path;
@@ -129,7 +129,7 @@ class EditCommands extends DrushCommands implements SiteAliasManagerAwareInterfa
     public static function bashFiles()
     {
         $bashFiles = [];
-        $home = Drush::config()->get('env.home');
+        $home = Drush::config()->home();
         if ($bashrc = self::findBashrc($home)) {
             $bashFiles[$bashrc] = $bashrc;
         }
