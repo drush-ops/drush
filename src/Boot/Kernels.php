cbf94d1f05f7ddf383ef9f61f347481a0625f854
Pieter Frenssen <pieter@frenssen.be>
2017-11-29 10:59:51 +0200

    Indent with 4 spaces.

--- src/Boot/Kernels.php
+++ src/Boot/Kernels.php
@@ -2,34 +2,37 @@
 
 namespace Drush\Boot;
 
+use Drush\Drupal\DrupalKernel as DrushDrupalKernel;
+use Drush\Drupal\UpdateKernel as DrushUpdateKernel;
+
 /**
  * Defines the available kernels that can be bootstrapped.
  */
 final class Kernels {
 
-  /**
-   * The default kernel that is used on standard requests.
-   *
-   * @var string
-   */
-  const DRUPAL = 'drupal';
+    /**
+     * The default kernel that is used on standard requests.
+     *
+     * @var string
+     */
+    const DRUPAL = 'drupal';
 
-  /**
-   * The kernel that is used during database updates.
-   *
-   * @var string
-   */
-  const UPDATE = 'update';
+    /**
+     * The kernel that is used during database updates.
+     *
+     * @var string
+     */
+    const UPDATE = 'update';
 
-  /**
-   * Returns the available kernels.
-   */
-  public static function availableKernels()
-  {
-      return [
-          static::DRUPAL,
-          static::UPDATE,
-      ];
-  }
+    /**
+     * Returns the available kernels.
+     */
+    public static function availableKernels()
+    {
+        return [
+            static::DRUPAL,
+            static::UPDATE,
+        ];
+    }
 
 }
