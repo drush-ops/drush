0511043b4d9c61f36940fdff8d516dac782d56db
Pieter Frenssen <pieter@frenssen.be>
2018-01-16 10:51:39 +0200

    Test post-updates.

--- tests/resources/modules/d8/woot/woot.post_update.php
+++ tests/resources/modules/d8/woot/woot.post_update.php
@@ -1,11 +1,21 @@
 <?php
 
 /**
+ * Successful post-update.
+ */
+function woot_post_update_a()
+{
+    // Note that this is called 'a' so that it will run first. The post-updates
+    // are executed in alphabetical order.
+    return t('This is the update message from woot_post_update_a');
+}
+
+/**
  * Failing post-update.
  */
 function woot_post_update_failing()
 {
-    throw new \Exception('post update error');
+    throw new \Exception('This is the exception message thrown in woot_post_update_failing');
 }
 
 /**
