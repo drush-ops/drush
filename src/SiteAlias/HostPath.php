7026ea872f6bf12997be138e0e8a17381ee00601
Greg Anderson <greg.1.anderson@greenknowe.org>
2018-02-20 14:19:44 -0800

    Fixes #3306: Make sure that rsync with %files resolves to sites/default/files/ as it historically has, not sites/default/files.

--- src/SiteAlias/HostPath.php
+++ src/SiteAlias/HostPath.php
@@ -179,8 +179,17 @@ class HostPath
         if (empty($pathAlias)) {
             return $this;
         }
+        // Make sure that the resolved path always ends in a '\'.
+        $resolvedPath .= '/';
+        // Avoid double / in path.
+        //   $this->path: %files/foo
+        //   $pathAlias:   files
+        // We add one to the length of $pathAlias to account for the '%' in $this->path.
+        if (strlen($this->path) > (strlen($pathAlias) + 1)) {
+            $resolvedPath = rtrim($resolvedPath, '/');
+        }
         // Once the path alias is resolved, replace the alias in the $path with the result.
-        $this->path = rtrim($resolvedPath, '/') . substr($this->path, strlen($pathAlias) + 1);
+        $this->path = $resolvedPath . substr($this->path, strlen($pathAlias) + 1);
 
         // Using a path alias such as %files is equivalent to making explicit
         // use of @self:%files. We set implicit to false here so that the resolved
