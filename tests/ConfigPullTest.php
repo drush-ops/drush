7968a59fe113dc2020af8ecfe7f92ad4b8940444
Moshe Weitzman <weitzman@tejasa.com>
2018-03-06 09:58:13 -0500

    Fix #3423. Allow custom target dir for config:pull (#3436)
    * Fix #3423. Allow custom target dir for config:pull
    
    * Better usage example.

--- tests/ConfigPullTest.php
+++ tests/ConfigPullTest.php
@@ -1,6 +1,7 @@
 <?php
-
 namespace Unish;
+
+use Webmozart\PathUtil\Path;
 
 /**
  * Tests for config-pull command. Sets up two Drupal sites.
@@ -30,9 +31,15 @@ class ConfigPullCase extends CommandUnishTestCase
         $this->drush('config-set', ['system.site', 'uuid', $uuid], ['yes' => null], $destination);
 
         $this->drush('config-set', ['system.site', 'name', 'testConfigPull'], ['yes' => null], $source);
-        $this->drush('config-pull', [$source, $destination], []);
+        $this->drush('config-pull', [$source, $destination]);
         $this->drush('config-import', [], ['yes' => null], $destination);
         $this->drush('config-get', ['system.site', 'name'], [], $source);
         $this->assertEquals("'system.site:name': testConfigPull", $this->getOutput(), 'Config was successfully pulled.');
+
+        // Test that custom target dir works
+        $target = Path::join($this->getSandbox(), __CLASS__);
+        $this->mkdir($target);
+        $this->drush('config-pull', [$source, "$destination:$target"]);
+        $this->assertFileExists(Path::join($target, 'system.site.yml'));
     }
 }
