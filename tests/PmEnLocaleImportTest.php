715819813967b71752fcd531acd383faeb610b07
Erik Stielstra <info@erikstielstra.nl>
2018-03-10 23:29:56 +0100

    Use D8 command syntax

--- tests/PmEnLocaleImportTest.php
+++ tests/PmEnLocaleImportTest.php
@@ -15,17 +15,16 @@ class PmEnLocaleImportCase extends CommandUnishTestCase
     public function testBatchImportTranslations()
     {
         $this->setUpDrupal(1, true);
-        $this->drush('pm:enable', ['language', 'locale']);
-        $this->drush('pm:enable', ['dblog']);
+        $this->drush('pm-enable', ['language', 'locale', 'dblog']);
         $this->drush('config-set', ['locale.settings', 'translation.import_enabled', true]);
 
-        $this->drush('php:eval', ['\Drupal\language\Entity\ConfigurableLanguage::create([
+        $this->drush('php-eval', ['\Drupal\language\Entity\ConfigurableLanguage::create([
             "id" => "nl",
             "label" => "Dutch",
         ])->save()']);
 
-        $this->drush('pm:enable', ['devel']);
-        $this->drush('watchdog:show');
+        $this->drush('pm-enable', ['devel']);
+        $this->drush('watchdog-show');
         $this->assertContains('Translations imported:', $this->getSimplifiedOutput());
     }
 }
