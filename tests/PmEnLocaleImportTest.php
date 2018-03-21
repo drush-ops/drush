4eb42d31b65b6c2e990e59740de42923f44b19a9
Erik Stielstra <info@erikstielstra.nl>
2018-03-21 08:01:09 +0100

    Add drush command options.

--- tests/PmEnLocaleImportTest.php
+++ tests/PmEnLocaleImportTest.php
@@ -15,18 +15,25 @@ class PmEnLocaleImportCase extends CommandUnishTestCase
 
     public function testBatchImportTranslations()
     {
-        $this->setUpDrupal(1, true);
-        $this->drush('pm-enable', ['language', 'locale', 'dblog']);
-        $this->drush('config-set', ['locale.settings', 'translation.import_enabled', true]);
+        $sites = $this->setUpDrupal(1, true);
+        $uri = key($sites);
+        $root = $this->webroot();
+        $options = array(
+            'root' => $root,
+            'uri' => $uri,
+        );
+
+        $this->drush('pm-enable', ['language', 'locale', 'dblog'], $options);
+        $this->drush('config-set', ['locale.settings', 'translation.import_enabled', true], $options);
 
         // Setup the interface translation system and prepare a source translation file.
         // The test uses a local po file as translation source. This po file will be
         // imported from the translations directory when a module is enabled.
-        $this->drush('config-set', ['locale.settings', 'translation.use_source', 'locale']);
-        $this->drush('config-set', ['locale.settings', 'translation.default_filename', '%project.%language.po']);
-        $this->drush('config-set', ['locale.settings', 'translation.path', '../translations']);
+        $this->drush('config-set', ['locale.settings', 'translation.use_source', 'locale'], $options);
+        $this->drush('config-set', ['locale.settings', 'translation.default_filename', '%project.%language.po'], $options);
+        $this->drush('config-set', ['locale.settings', 'translation.path', '../translations'], $options);
         $source = Path::join(__DIR__, '/resources/devel.nl.po');
-        $translationDir = Path::join($this->getSut(), 'translations');
+        $translationDir = Path::join($root, '../translations');
         $this->mkdir($translationDir);
         copy($source, $translationDir . '/devel.nl.po');
 
@@ -35,8 +42,8 @@ class PmEnLocaleImportCase extends CommandUnishTestCase
             "label" => "Dutch",
         ])->save()']);
 
-        $this->drush('pm-enable', ['devel']);
-        $this->drush('watchdog-show');
+        $this->drush('pm-enable', ['devel'], $options);
+        $this->drush('watchdog-show', [], $options);
         $this->assertContains('Translations imported:', $this->getSimplifiedOutput());
 
         // Clean up the mess this test creates.
