b3c128b2c14ff1ab1691a65663fe0e353c88ebc2
Erik Stielstra <info@erikstielstra.nl>
2018-04-03 18:42:37 +0200

    Remove --uri and --root options from tests.

--- tests/LanguageAddTest.php
+++ tests/LanguageAddTest.php
@@ -15,65 +15,48 @@ class LanguageAddCase extends CommandUnishTestCase
 
     public function testLanguageInfo()
     {
-        $sites = $this->setUpDrupal(1, true);
-        $uri = key($sites);
-        $root = $this->webroot();
-        $options = array(
-            'root' => $root,
-            'uri' => $uri,
-        );
+        $this->setUpDrupal(1, true);
+        $this->drush('pm-enable', ['language']);
 
-        $this->drush('pm-enable', ['language'], $options);
-        $this->drush('language-info', [], $options);
+        $this->drush('language-info', []);
         $this->assertContains('English (en)', $this->getSimplifiedOutput());
     }
 
     public function testLanguageAdd()
     {
-        $sites = $this->setUpDrupal(1, true);
-        $uri = key($sites);
-        $root = $this->webroot();
-        $options = array(
-            'root' => $root,
-            'uri' => $uri,
-        );
-        $this->drush('pm-enable', ['language'], $options);
+        $this->setUpDrupal(1, true);
+        $this->drush('pm-enable', ['language']);
 
-        $this->drush('language-add', ['nl,fr'], $options + ['no-translations' => null]);
+        $this->drush('language-add', ['nl,fr'], ['no-translations' => null]);
 
-        $this->drush('language-info', [], $options);
+        $this->drush('language-info', []);
         $this->assertContains('Dutch (nl)', $this->getSimplifiedOutput());
         $this->assertContains('French (fr)', $this->getSimplifiedOutput());
     }
 
     public function testLanguageAddWithTranslations()
     {
-        $sites = $this->setUpDrupal(1, true);
-        $uri = key($sites);
+        $this->setUpDrupal(1, true);
         $root = $this->webroot();
-        $options = array(
-            'root' => $root,
-            'uri' => $uri,
-        );
 
-        $this->drush('pm-enable', ['language', 'locale', 'dblog'], $options);
-        $this->drush('config-set', ['locale.settings', 'translation.import_enabled', true], $options);
+        $this->drush('pm-enable', ['language', 'locale', 'dblog']);
+        $this->drush('config-set', ['locale.settings', 'translation.import_enabled', true]);
 
         // Setup the interface translation system and prepare a source translation file.
         // The test uses a local po file as translation source. This po file will be
         // imported from the translations directory when a module is enabled.
-        $this->drush('config-set', ['locale.settings', 'translation.use_source', 'locale'], $options);
-        $this->drush('config-set', ['locale.settings', 'translation.default_filename', '%project.%language.po'], $options);
-        $this->drush('config-set', ['locale.settings', 'translation.path', '../translations'], $options);
+        $this->drush('config-set', ['locale.settings', 'translation.use_source', 'locale']);
+        $this->drush('config-set', ['locale.settings', 'translation.default_filename', '%project.%language.po']);
+        $this->drush('config-set', ['locale.settings', 'translation.path', '../translations']);
         $source = Path::join(__DIR__, '/resources/devel.nl.po');
         $translationDir = Path::join($root, '../translations');
         $this->mkdir($translationDir);
         copy($source, $translationDir . '/devel.nl.po');
 
-        $this->drush('pm-enable', ['devel'], $options);
-        $this->drush('language-add', ['nl'], $options);
+        $this->drush('pm-enable', ['devel']);
+        $this->drush('language-add', ['nl']);
 
-        $this->drush('watchdog-show', [], $options);
+        $this->drush('watchdog-show', []);
         $this->assertContains('Translations imported:', $this->getSimplifiedOutput());
 
         // Clean up the mess this test creates.
