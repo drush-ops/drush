8f3ffed03cf3fff7f5a118e312086a8e781fe95b
Greg Anderson <greg.1.anderson@greenknowe.org>
2018-08-21 19:31:37 -0700

    Allow multipart aliases with drush sa, e.g. @location.site.env or @location.site, in addition to just @location or @site. (#3660)

--- src/Commands/core/SiteCommands.php
+++ src/Commands/core/SiteCommands.php
@@ -115,10 +115,11 @@ class SiteCommands extends DrushCommands implements SiteAliasManagerAwareInterfa
      */
     public function siteAlias($site = null, $options = ['format' => 'yaml'])
     {
-        // Check to see if the user provided a specification that matches
+        // First check to see if the user provided a specification that matches
         // multiple sites.
-        $aliasList = $this->siteAliasManager()->getMultiple($site);
-        if (is_array($aliasList)) {
+        list($locationPart, $sitePart) = $this->splitLocationFromSite($site);
+        $aliasList = $this->siteAliasManager()->getMultiple($sitePart, $locationPart);
+        if (is_array($aliasList) && !empty($aliasList)) {
             return new ListDataFromKeys($this->siteAliasExportList($aliasList, $options));
         }
 
@@ -133,6 +134,21 @@ class SiteCommands extends DrushCommands implements SiteAliasManagerAwareInterfa
         } else {
             $this->logger()->success('No site aliases found.');
         }
+    }
+
+    /**
+     * splitLocationFromSite returns the location and the site if the
+     * site alias is in the form '@location.site'. Otherwise it just
+     * returns the site unchanged, without a location.
+     */
+    protected function splitLocationFromSite($site)
+    {
+        $parts = explode('.', ltrim($site, '@'));
+
+        if (count($parts) == 2) {
+            return [$parts[0], '@' . $parts[1]];
+        }
+        return [null, $site];
     }
 
     /**
