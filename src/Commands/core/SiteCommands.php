870c3c41dca0abae520b3e15e6913d0da5ffc0b5
Greg Anderson <greg.1.anderson@greenknowe.org>
2018-09-22 04:16:59 -0700

    Fix SiteAliasManager::getMultiple (#3695)
    * Fix SiteAliasManager::getMultiple
    
    * Fix test failure caused by a test that was broken by a bugfix in site-alias 1.1.5 (test was asserting incorrect behavior)
    
    * Update isolation test composer lock files

--- src/Commands/core/SiteCommands.php
+++ src/Commands/core/SiteCommands.php
@@ -117,8 +117,7 @@ class SiteCommands extends DrushCommands implements SiteAliasManagerAwareInterfa
     {
         // First check to see if the user provided a specification that matches
         // multiple sites.
-        list($locationPart, $sitePart) = $this->splitLocationFromSite($site);
-        $aliasList = $this->siteAliasManager()->getMultiple($sitePart, $locationPart);
+        $aliasList = $this->siteAliasManager()->getMultiple($site);
         if (is_array($aliasList) && !empty($aliasList)) {
             return new ListDataFromKeys($this->siteAliasExportList($aliasList, $options));
         }
@@ -134,21 +133,6 @@ class SiteCommands extends DrushCommands implements SiteAliasManagerAwareInterfa
         } else {
             $this->logger()->success('No site aliases found.');
         }
-    }
-
-    /**
-     * splitLocationFromSite returns the location and the site if the
-     * site alias is in the form '@location.site'. Otherwise it just
-     * returns the site unchanged, without a location.
-     */
-    protected function splitLocationFromSite($site)
-    {
-        $parts = explode('.', ltrim($site, '@'));
-
-        if (count($parts) == 2) {
-            return [$parts[0], '@' . $parts[1]];
-        }
-        return [null, $site];
     }
 
     /**
