b6a08e3f6cf1de01f41b0df13dee9ede29919404
Greg Anderson <greg.1.anderson@greenknowe.org>
2018-09-26 09:23:14 -0700

    Round out the forward-compatibility with Drush 9 in the DrushCommands class

--- lib/Drush/Commands/DrushCommands.php
+++ lib/Drush/Commands/DrushCommands.php
@@ -5,7 +5,22 @@ use Psr\Log\LoggerAwareInterface;
 use Psr\Log\LoggerAwareTrait;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Input\InputOption;
+use Drush\Command\DrushInputAdapter;
+use Drush\Command\DrushOutputAdapter;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\OutputInterface;
+use Drush\DrushConfig;
 
+/**
+ * DrushCommands provides access to configuration, IO and the logger,
+ * and also provides a "printFile" convenience method for displaying
+ * the contents of a file with a pager.
+ *
+ * Drush extensions that use ONLY the facilities provided by this class,
+ * plus any APIs provided by Drupal core and the module they are bundled
+ * with should work with both Drush 8 and Drush 9.
+ */
 abstract class DrushCommands implements LoggerAwareInterface
 {
     // This is more readable.
@@ -14,8 +29,45 @@ abstract class DrushCommands implements LoggerAwareInterface
 
     use LoggerAwareTrait;
 
+    protected $io;
+
     public function __construct()
     {
+    }
+
+    /**
+     * Return an object that has the same signature as a Consolidation\Config\ConfigInterface
+     */
+    protected function getConfig()
+    {
+        return new DrushConfig();
+    }
+
+    /**
+     * @return SymfonyStyle
+     */
+    protected function io()
+    {
+        if (!$this->io) {
+            $this->io = new SymfonyStyle($this->input(), $this->output());
+        }
+        return $this->io;
+    }
+
+    /**
+     * @return InputInterface
+     */
+    protected function input()
+    {
+        return annotationcommand_adapter_input();
+    }
+
+    /**
+     * @return OutputInterface
+     */
+    protected function output()
+    {
+        return new DrushOutputAdapter();
     }
 
     /**
