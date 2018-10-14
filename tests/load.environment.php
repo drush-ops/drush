5771a34d0cba96e9adae97d613740db0920e27b5
Moshe Weitzman <weitzman@tejasa.com>
2018-10-14 10:57:09 -0400

    CS

--- tests/load.environment.php
+++ tests/load.environment.php
@@ -14,7 +14,6 @@ use Dotenv\Exception\InvalidPathException;
 $dotenv = new Dotenv(__DIR__);
 try {
     $dotenv->load();
-}
-catch (InvalidPathException $e) {
+} catch (InvalidPathException $e) {
     // Do nothing. Production environments rarely use .env files.
 }
