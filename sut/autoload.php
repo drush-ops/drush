6fc904a3580e1da88e1e4f55632beab496009794
Moshe Weitzman <weitzman@tejasa.com>
2018-10-17 13:43:17 -0400

    Fix #3741 Handle global Drush9 edge case (#3746)
    * Handle global Drush9 edge case - https://github.com/drush-ops/drush/issues/3741.
    
    * Add docs link.

--- sut/autoload.php
+++ sut/autoload.php
@@ -11,4 +11,5 @@
  * @see core/modules/statistics/statistics.php
  */
 
-return require __DIR__ . '/../vendor/autoload.php';
+// Use include instead of return because See https://github.com/drush-ops/drush/issues/3741
+return include __DIR__ . '/../vendor/autoload.php';
