7e63477fdd3e52aa1375cc95b0cf62c97e8238c4
Greg Anderson <greg.1.anderson@greenknowe.org>
2018-10-18 18:11:05 -0700

    Make batch:process return unstructured list data, and produce no output by default.

--- src/Drupal/Commands/core/BatchCommands.php
+++ src/Drupal/Commands/core/BatchCommands.php
@@ -2,10 +2,10 @@
 namespace Drush\Drupal\Commands\core;
 
 use Drush\Commands\DrushCommands;
+use Consolidation\OutputFormatters\StructuredData\UnstructuredListData;
 
 class BatchCommands extends DrushCommands
 {
-
     /**
      * Process operations in the specified batch set.
      *
@@ -13,9 +13,10 @@ class BatchCommands extends DrushCommands
      * @aliases batch-process
      * @param $batch_id The batch id that will be processed.
      * @hidden
+     * @return \Consolidation\OutputFormatters\StructuredData\UnstructuredListData
      */
-    public function process($batch_id)
+    public function process($batch_id, $options = ['format' => 'null'])
     {
-        return drush_batch_command($batch_id);
+        return new UnstructuredListData(drush_batch_command($batch_id));
     }
 }
