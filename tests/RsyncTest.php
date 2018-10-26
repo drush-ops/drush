930eec3f60c8542120bf257e71f6d48b7ca18b29
Greg Anderson <greg.1.anderson@greenknowe.org>
2018-10-26 10:54:35 -0700

    Update assertion in rsync test

--- tests/RsyncTest.php
+++ tests/RsyncTest.php
@@ -54,7 +54,7 @@ class RsyncCase extends CommandUnishTestCase
         // the remote side, at which point they will be evaluated & any needed
         // injection will be done.
         $this->drush('rsync', ['@example.dev', '@example.stage'], $options, 'user@server/path/to/drupal#sitename', null, self::EXIT_SUCCESS, '2>&1');
-        $expected = "Simulating backend invoke: ssh -o PasswordAuthentication=no user@server 'drush --root=/path/to/drupal --uri=sitename --no-interaction rsync '\''@example.dev'\'' '\''@example.stage'\'' 2>&1' 2>&1";
+        $expected = "[notice] Simulating: 'ssh' '-o PasswordAuthentication=no' 'user@server' 'drush --no-interaction rsync @example.dev @example.stage --uri=sitename --root=/path/to/drupal'";
         $this->assertOutputEquals($expected, '# --alias-path=[^ ]*#');
     }
 
