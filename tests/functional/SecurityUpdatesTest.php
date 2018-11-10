ad33027948c41b57f103e8488e8eeafb92bb95c9
Moshe Weitzman <weitzman@tejasa.com>
2018-11-09 20:22:18 -0500

    Fix #3748. Use Semver::satisfies. (#3774)
    * Fix #3748. Use Semver::satisfies.
    
    * Remove min-version. We can't support it reliably.
    
    * Back to a working url.
    
    * fix spacing.

--- tests/functional/SecurityUpdatesTest.php
+++ tests/functional/SecurityUpdatesTest.php
@@ -20,67 +20,10 @@ class SecurityUpdatesTest extends CommandUnishTestCase
     {
         $this->drush('pm:security', [], ['format' => 'json'], null, null, self::EXIT_ERROR);
         $this->assertContains('One or more of your dependencies has an outstanding security update. Please apply update(s) immediately.', $this->getErrorOutput());
-        $this->assertContains('Try running: composer require drupal/alinks:^1.1 --update-with-dependencies', $this->getErrorOutput());
+        $this->assertContains('Try running: composer require drupal/alinks --update-with-dependencies', $this->getErrorOutput());
         $security_advisories = $this->getOutputFromJSON();
         $this->assertObjectHasAttribute('drupal/alinks', $security_advisories);
         $this->assertEquals('drupal/alinks', $security_advisories->{"drupal/alinks"}->name);
         $this->assertEquals('1.0.0', $security_advisories->{"drupal/alinks"}->version);
-        $this->assertEquals('1.1', $security_advisories->{"drupal/alinks"}->{"min-version"});
-    }
-
-
-  /**
-   * Test that insecure packages are correctly identified.
-   *
-   * @dataProvider conflictConstraintParsingProvider
-   */
-    public function testConflictConstraintParsing($package, $conflict_constraint, $min_version, $updates_are_available)
-    {
-        $available_updates = SecurityUpdateCommands::determineUpdatesFromConstraint($conflict_constraint, $package, $package['name']);
-        $this->assertEquals($updates_are_available, (bool) $available_updates);
-
-        if ($available_updates) {
-            $this->assertEquals($package['version'], $available_updates['version']);
-            $this->assertEquals($min_version, $available_updates['min-version']);
-        }
-    }
-
-  /**
-   * Data provider for testConflictConstraintParsing().
-   */
-    public function conflictConstraintParsingProvider()
-    {
-        return [
-        // Test "minimum version" conflict.
-        [
-        [
-          'name' => 'Alinks',
-          'version' => '1.0.0'
-        ],
-        '<1.0.1',
-        '1.0.1',
-        true,
-        ],
-        // Test "exact version" conflict.
-        [
-        [
-          'name' => 'Alinks',
-          'version' => '1.0.0'
-        ],
-        '1.0.0',
-        '1.0.1',
-        true,
-        ],
-        // Test "exact version" conflict with 2 digits. Should not work.
-        [
-        [
-          'name' => 'Alinks',
-          'version' => '1.0.0'
-        ],
-        '1.0',
-        '1.0.1',
-        false,
-        ],
-        ];
     }
 }
