74cab9d9b85da858771b2b14676af55661259ba0
Kirill Roskolii <kirill.roskolii@catalyst.net.nz>
2019-01-07 16:24:05 +1300

    Issue #1523: Fix code style and add more checks.

--- tests/functional/PostgreSqlSchemaTest.php
+++ tests/functional/PostgreSqlSchemaTest.php
@@ -19,24 +19,31 @@ class PostgreSqlSchemaTest extends CommandUnishTestCase
 
         $db_driver = $this->dbDriver();
         if (!$db_driver == 'pgsql') {
-          $this->markTestSkipped('PostgreSQL specific test.');
-        }
-        else {
-          // Create schema.
-          $this->drush('sql:query', ["CREATE SCHEMA drupal"]);
+            $this->markTestSkipped('PostgreSQL specific test.');
+        } else {
+            // Create schema.
+            $this->drush('sql-query', ["CREATE SCHEMA drupal"]);
 
-          // Install Drupal in "drupal" schema.
-          $this->installDrupal('dev', true, ['db-prefix' => 'drupal.']);
+            // Install Drupal in "drupal" schema.
+            $this->installDrupal('dev', true, ['db-prefix' => 'drupal.']);
 
-          // Run 'core-status' and insure that we can bootstrap Drupal.
+            // Run 'core-status' and insure that we can bootstrap Drupal.
+            $this->drush('core-status', [], ['fields' => 'bootstrap']);
+            $output = $this->getOutput();
+            $this->assertContains('Successful', $output);
+
+            // Issue a query and check the result to verify the connection.
+            $this->drush('sql:query', ["SELECT uid FROM {users} where uid = 1;"], ['db-prefix']);
+            $output = $this->getOutput();
+            $this->assertContains('1', $output);
+
+          // Drop DB.
+          $this->drush('sql-drop');
+
+          // Run 'core-status' and insure that we no longer can bootstrap Drupal.
           $this->drush('core-status', [], ['fields' => 'bootstrap']);
           $output = $this->getOutput();
-          $this->assertContains('Successful', $output);
-
-          // Issue a query and check the result to verify the connection.
-          $this->drush('sql:query', ["SELECT uid FROM {users} where uid = 1;"], ['db-prefix']);
-          $output = $this->getOutput();
-          $this->assertContains('1', $output);
+          $this->assertNotContains('Successful', $output);
         }
     }
 }
