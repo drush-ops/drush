8a420b3b06f883e343e032bf1278a726a643239d
Moshe Weitzman <weitzman@tejasa.com>
2019-02-10 22:45:17 -0500

    Run drush.bat during unish on windows

--- tests/unish/UnishTestCase.php
+++ tests/unish/UnishTestCase.php
@@ -57,7 +57,7 @@ abstract class UnishTestCase extends TestCase
         self::mkdir($unish_sandbox);
         $unish_cache = Path::join($unish_sandbox, 'cache');
 
-        self::$drush = self::getComposerRoot() . '/drush';
+        self::$drush = Path::join(self::getComposerRoot(), self::isWindows() ? 'dr.bat' : 'drush');
 
         self::$sandbox = $unish_sandbox;
         self::$usergroup = isset($GLOBALS['UNISH_USERGROUP']) ? $GLOBALS['UNISH_USERGROUP'] : null;
