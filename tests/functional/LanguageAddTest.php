44c83cfda492f58e72d8d06187ff17839d48e162
Erik Stielstra <info@erikstielstra.nl>
2019-03-10 22:29:11 +0100

    Use language_test module instead of devel.

--- tests/functional/LanguageAddTest.php
+++ tests/functional/LanguageAddTest.php
@@ -2,10 +2,6 @@
 
 namespace Unish;
 
-use Drupal\Core\Language\Language;
-use Drupal\language\Entity\ConfigurableLanguage;
-use Webmozart\PathUtil\Path;
-
 /**
  *  @group slow
  *  @group pm
@@ -37,26 +33,12 @@ class LanguageAddCase extends CommandUnishTestCase
     {
         $this->drush('pm-enable', ['language', 'locale', 'dblog']);
         $this->drush('config-set', ['locale.settings', 'translation.import_enabled', true]);
-
-        // Setup the interface translation system and prepare a source translation file.
-        // The test uses a local po file as translation source. This po file will be
-        // imported from the translations directory when a module is enabled.
         $this->drush('config-set', ['locale.settings', 'translation.use_source', 'locale']);
-        $this->drush('config-set', ['locale.settings', 'translation.default_filename', '%project.%language.po']);
-        $this->drush('config-set', ['locale.settings', 'translation.path', '../translations']);
-        $source = Path::join(__DIR__, '/resources/devel.nl.po');
-        $translationDir = Path::join($this->webroot(), '../translations');
-        $this->mkdir($translationDir);
-        copy($source, $translationDir . '/devel.nl.po');
 
-        $this->drush('pm-enable', ['devel']);
+        $this->drush('pm-enable', ['language_test']);
         $this->drush('language-add', ['nl']);
 
         $this->drush('watchdog-show', []);
         $this->assertContains('Translations imported:', $this->getSimplifiedOutput());
-
-        // Clean up the mess this test creates.
-        unlink($translationDir . '/devel.nl.po');
-        rmdir($translationDir);
     }
 }
