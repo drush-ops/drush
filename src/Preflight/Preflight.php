c073932a4761dd4f491536a3aba104a49ee614ae
INTRACTO - Jeroen Tubex <jeroen.tubex@intracto.com>
2019-03-28 09:49:49 +0100

    #4109: Convert symlinked originalCwd to the realpath

--- src/Preflight/Preflight.php
+++ src/Preflight/Preflight.php
@@ -357,7 +357,7 @@ class Preflight
             if (!$foundRoot && $fallbackPath && is_dir($fallbackPath . '/sut') && is_dir($fallbackPath . '/vendor')) {
                 $foundRoot = $this->drupalFinder->locateRoot($fallbackPath);
             }
-            return realpath($this->drupalFinder()->getDrupalRoot());
+            return $this->drupalFinder()->getDrupalRoot();
         }
         return $originalSelection;
     }
