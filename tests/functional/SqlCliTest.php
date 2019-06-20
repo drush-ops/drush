57b7767816cd4d324518fd8837a90a73880a23be
Dane Powell <git@danepowell.com>
2019-06-20 13:32:25 -0700

    Fixes #4092: Adds test for db imports via sql:cli (should fail).

---
+++ tests/functional/SqlCliTest.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @file
+ *  Test sql:cli
+ */
+
+namespace Unish;
+
+/**
+ * @group slow
+ * @group commands
+ * @group sql
+ */
+class SqlCliTest extends CommandUnishTestCase
+{
+
+    public function testSqlCli()
+    {
+        $this->setUpDrupal(1, true);
+        // Ensure SQL dumps can be imported via sql:cli.
+        $this->drush('sql:cli < ' . __DIR__ . '/resources/sqlcli.sql');
+        $this->drush('sql-query', ["SHOW TABLES"]);
+        $output = $this->getOutput();
+        $this->assertContains('sqlcli', $output);
+    }
+}
