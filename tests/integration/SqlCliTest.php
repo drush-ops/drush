c378108b23f798787d5edf4b6482ba6ba37ea3b5
Dane Powell <git@danepowell.com>
2019-06-24 16:58:50 -0700

    use interation test instead (doesn't work)

--- tests/integration/SqlCliTest.php
+++ tests/integration/SqlCliTest.php
@@ -7,12 +7,14 @@
 
 namespace Unish;
 
+use Webmozart\PathUtil\Path;
+
 /**
  * @group slow
  * @group commands
  * @group sql
  */
-class SqlCliTest extends CommandUnishTestCase
+class SqlCliTest extends UnishIntegrationTestCase
 {
 
     public function testSqlCli()
@@ -21,8 +23,12 @@ class SqlCliTest extends CommandUnishTestCase
             $this->markTestSkipped('On Windows, STDIN redirection is not supported.');
         }
         $this->setUpDrupal(1, true);
+        $path = Path::join(__DIR__, 'resources/sqlcli.sql');
+        // temporary: just prove that the file exists.
+        // why doesn't it get imported below?
+        $this->assertFileExists($path);
         // Ensure SQL dumps can be imported via sql:cli.
-        $this->drush('sql:cli < ' . __DIR__ . '/resources/sqlcli.sql');
+        $this->drush('sql:cli', [], [], null, $path);
         $this->drush('sql-query', ["SHOW TABLES"]);
         $output = $this->getOutput();
         $this->assertContains('sqlcli', $output);
