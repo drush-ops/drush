cedf7e023aac3692a09cbaeceff8eae0fac3e931
Moshe Weitzman <weitzman@tejasa.com>
2019-07-18 23:25:09 -0400

    Remo unused test script and update COVERAGE docs. (#4131)

--- tests/functional/resources/testDispatchUsingAlias_script.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$valuesUsingAlias = drush_invoke_process("@unish.dev", "unit-return-argv", [], [], ["dispatch-using-alias" => true]);
-$valuesWithoutAlias = drush_invoke_process("@unish.dev", "unit-return-argv", [], [], []);
-return ['with' => $valuesUsingAlias['object'], 'without' => $valuesWithoutAlias['object']];
