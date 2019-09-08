96d0c3d74e383d34527cf7e63ce387d7488e8b40
Moshe Weitzman <weitzman@tejasa.com>
2019-09-08 08:27:36 -0400

    Make sqlcli more DB compatible.

--- tests/integration/resources/sqlcli.sql
+++ tests/integration/resources/sqlcli.sql
@@ -1,7 +1,5 @@
 DROP TABLE IF EXISTS `sqlcli`;
 CREATE TABLE `sqlcli` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Test table.';
-UNLOCK TABLES;
-commit;
+    `id` int(10) NOT NULL,
+    PRIMARY KEY (`id`)
+);
