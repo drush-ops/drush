d7d7aeeea77df2401c3ae08bd4ef534821bf83a0
Moshe Weitzman <weitzman@tejasa.com>
2019-09-08 09:06:39 -0400

    Postgres compat for sql:cli test.

--- tests/integration/resources/sqlcli.sql
+++ tests/integration/resources/sqlcli.sql
@@ -1,5 +1,5 @@
-DROP TABLE IF EXISTS `sqlcli`;
-CREATE TABLE `sqlcli` (
-    `id` int(10) NOT NULL,
-    PRIMARY KEY (`id`)
+DROP TABLE IF EXISTS sqlcli;
+CREATE TABLE sqlcli (
+  id int NOT NULL,
+  PRIMARY KEY (id)
 );
