e192fccb3055b16c0c3084dcfb1c6fc8ef277f7e
AdamPS <AdamPS@2650563.no-reply.drupal.org>
2019-11-30 18:33:06 +0000

    Drop user if exists

--- lib/Drush/Sql/Sqlmysql.php
+++ lib/Drush/Sql/Sqlmysql.php
@@ -94,6 +94,7 @@ EOT;
       //   We can't easily know what IP adderss or hostname would represent our server.
       $domain = ($this->db_spec['host'] == 'localhost') ? 'localhost' : '%';
       $user = sprintf("'%s'@'%s'", $this->db_spec['username'], $domain);
+      $sql[] = sprintf("DROP USER IF EXISTS %s;", $user);
       $sql[] = sprintf("CREATE USER %s IDENTIFIED WITH mysql_native_password BY '%s';", $user, $this->db_spec['password']);
       $sql[] = sprintf('GRANT ALL PRIVILEGES ON %s.* TO %s;', $dbname, $user);
       $sql[] = 'FLUSH PRIVILEGES;';
