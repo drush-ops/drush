f93ee995f82d7bd4aff7697e5a0c4769d5ca4207
Moshe Weitzman <weitzman@tejasa.com>
2020-01-19 22:41:09 -0500

    Change from an exception to  warning in site:install.

--- src/Commands/core/SiteInstallCommands.php
+++ src/Commands/core/SiteInstallCommands.php
@@ -381,7 +381,7 @@ class SiteInstallCommands extends DrushCommands implements SiteAliasManagerAware
         $bootstrapManager->doBootstrap(DRUSH_BOOTSTRAP_DRUPAL_SITE);
 
         if ($program_exists && !$sql->dropOrCreate()) {
-            throw new \Exception(dt('Failed to drop or create the database: @error', ['@error' => $sql->getProcess()->getErrorOutput()]));
+            $this->logger()->warning(dt('Failed to drop or create the database. Do it yourself before installing: @error', ['@error' => $sql->getProcess()->getErrorOutput()]));
         }
     }
 
