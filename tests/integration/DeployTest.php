eae88f6efbf90012640d87e028e2870b6df56096
Moshe Weitzman <weitzman@tejasa.com>
2020-03-21 12:06:18 -0400

    Add a test

---
+++ tests/integration/DeployTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Unish;
+
+/**
+ * @group commands
+ */
+class DeployTest extends UnishIntegrationTestCase
+{
+
+    /**
+     * A simple test since all the sub-commands are tested elsewhere.
+     */
+    public function testDeploy()
+    {
+        // Prep a config directory that will be imported later.
+        $this->drush('config:export', []);
+        
+        $this->drush('deploy', []);
+        $expecteds = ["Database updates start.", 'Config import start.', 'Deploy hook start.', 'Cache rebuild start.'];
+        foreach ($expecteds as $expected) {
+            $this->assertContains($expected, $this->getErrorOutput());
+        }
+    }
+
+
+}
