a93f8c3f500b171367138b88ee4ee3ef44fd52f8
Moshe Weitzman <weitzman@tejasa.com>
2020-03-24 23:20:03 -0400

    Add debug

--- tests/integration/DeployTest.php
+++ tests/integration/DeployTest.php
@@ -16,7 +16,7 @@ class DeployTest extends UnishIntegrationTestCase
         // Prep a config directory that will be imported later.
         $this->drush('config:export');
         
-        $this->drush('deploy');
+        $this->drush('deploy', [], ['debug' => true]);
         $expecteds = ["Database updates start.", 'Config import start.', 'Deploy hook start.', 'Cache rebuild start.'];
         foreach ($expecteds as $expected) {
             $this->assertContains($expected, $this->getErrorOutput());
