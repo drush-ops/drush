cb1c5ea0d76a9f5c0996aa15ad3c164a03b54396
Erik Stielstra <info@erikstielstra.nl>
2020-04-13 16:00:04 +0200

    Replace deprecated constant REQUEST_TIME.

--- src/Drupal/Commands/core/LocaleCommands.php
+++ src/Drupal/Commands/core/LocaleCommands.php
@@ -143,7 +143,7 @@ class LocaleCommands extends DrushCommands
         // translation updates. If the status is expired we clear it an run a batch to
         // update the status and then fetch the translation updates.
         $last_checked = $this->getState()->get('locale.translation_last_checked');
-        if ($last_checked < REQUEST_TIME - LOCALE_TRANSLATION_STATUS_TTL) {
+        if ($last_checked < $_SERVER['REQUEST_TIME'] - LOCALE_TRANSLATION_STATUS_TTL) {
             locale_translation_clear_status();
             $batch = locale_translation_batch_update_build([], $langcodes, $translationOptions);
             batch_set($batch);
