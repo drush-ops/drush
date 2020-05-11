5c985399a76c33ed5d62b9d13f8156352e0a054a
Lucas D Hedding <lucashedding@1463982.no-reply.drupal.org>
2020-05-11 15:15:31 -0600

    swallow trigger errors, we already handle them

--- src/TestTraits/CliTestTrait.php
+++ src/TestTraits/CliTestTrait.php
@@ -97,7 +97,9 @@ trait CliTestTrait
             // Handle BC method of making env variables inherited. The default
             // in later versions is always inherit and this method disappears.
             if (method_exists($this->process, 'inheritEnvironmentVariables')) {
+                set_error_handler(null);
                 $this->process->inheritEnvironmentVariables();
+                restore_error_handler();
             }
             if ($this->timeout) {
                 $this->process->setTimeout($this->timeout)
