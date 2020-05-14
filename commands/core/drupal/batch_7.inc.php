230924af9d858dffb381b4db8a55f4b060363000
Greg Anderson <greg.1.anderson@greenknowe.org>
2020-05-14 10:14:54 -0700

    Refactor batch operations to use engine mechanism.

--- commands/core/drupal/batch_7.inc.php
+++ commands/core/drupal/batch_7.inc.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @file
- *   Drupal 8 and 9 engine for the Batch API
+ *   Drupal 7 engine for the Batch API
  */
 
 include __DIR__ . '/batch_common.php';
@@ -9,5 +9,5 @@ include __DIR__ . '/batch_common.php';
 // TODO: Make an engine just for db operations
 
 function drush_db_next_id() {
-  return \Drupal::database()->nextId();
+  return \db_next_id();
 }
