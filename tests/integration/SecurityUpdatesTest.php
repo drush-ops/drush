0cf78eff5cc6ccfeb0ec8c0514c74bde01b60651
Moshe Weitzman <weitzman@tejasa.com>
2020-06-29 09:52:55 -0400

    More assertions for drupal/core insecure

--- tests/integration/SecurityUpdatesTest.php
+++ tests/integration/SecurityUpdatesTest.php
@@ -18,12 +18,18 @@ class SecurityUpdatesTest extends UnishIntegrationTestCase
         list($expected_package, $expected_version) = $this->isDrupalGreaterThanOrEqualTo('9.0.0') ? ['drupal/semver_example', '2.2.0'] : ['drupal/alinks', '1.0.0'];
         $this->drush('pm:security', [], ['format' => 'json'], self::EXIT_ERROR);
         $this->assertContains('One or more of your dependencies has an outstanding security update.', $this->getErrorOutput());
-        $this->assertContains("Try running: composer require $expected_package", $this->getErrorOutput());
-        $this->assertContains("Try running: composer require drupal/core", $this->getErrorOutput());
+        $this->assertContains("$expected_package", $this->getErrorOutput());
         $security_advisories = $this->getOutputFromJSON();
         $this->arrayHasKey($expected_package, $security_advisories);
         $this->assertEquals($expected_package, $security_advisories[$expected_package]['name']);
         $this->assertEquals($expected_version, $security_advisories[$expected_package]['version']);
+        // Remove this clause if we don't have an insecure release to use.
+        if ($this->isDrupalGreaterThanOrEqualTo('9.0.0')) {
+          $this->assertContains("Try running: composer require drupal/core", $this->getErrorOutput());
+          $this->arrayHasKey('drupal/core', $security_advisories);
+          $this->assertEquals('drupal/core', $security_advisories['drupal/core']['name']);
+          $this->assertEquals('9.0.0', $security_advisories['drupal/core']['version']);
+        }
     }
 
     /**
