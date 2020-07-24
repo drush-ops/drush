4799b0e5f924f8d44cedc0afb65093fde6ad0b27
Moshe Weitzman <weitzman@tejasa.com>
2020-07-24 14:05:46 -0400

    No need for Versions stuff in Sami.

--- sami-config.php
+++ sami-config.php
@@ -18,16 +18,16 @@ $iterator = Finder::create()
 ;
 
 // generate documentation for all v2.0.* tags, the 2.0 branch, and the master one
-$versions = GitVersionCollection::create($dir)
-  // ->addFromTags('8.*')
- //  ->add('8.x', '8.x branch')
- // Sami actively checks out each ref listed here.
-  ->add('no-travis', 'Master branch')
-;
+// $versions = GitVersionCollection::create($dir)
+//   ->addFromTags('8.*')
+//   ->add('8.x', '8.x branch')
+// Sami actively checks out each ref listed here.
+//   ->add('no-travis', 'Master branch')
+//;
 
 return new Sami($iterator, array(
   // 'theme'                => 'symfony',
-  'versions'             => $versions,
+  //'versions'             => $versions,
   'title'                => 'Drush API',
   'build_dir'            => __DIR__.'/gh-pages/api/%version%',
   'cache_dir'            => __DIR__.'/.sami-cache/%version%',
