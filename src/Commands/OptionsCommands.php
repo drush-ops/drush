48df851a5d101b4d6363ed178614a22493cbb56d
Moshe Weitzman <weitzman@tejasa.com>
2020-07-26 10:46:12 -0400

    Add example of Drupal6 style db-url

--- src/Commands/OptionsCommands.php
+++ src/Commands/OptionsCommands.php
@@ -40,7 +40,7 @@ class OptionsCommands
     /**
      * @hook option @optionset_sql
      * @option database The DB connection key if using multiple connections in settings.php.
-     * @option db-url A Drupal 6 style database URL.
+     * @option db-url A Drupal 6 style database URL. For example <info>mysql://root:pass@localhost:port/dbname</info>
      * @option target The name of a target within the specified database connection.
      * @option show-passwords Show password on the CLI. Useful for debugging.
      */
