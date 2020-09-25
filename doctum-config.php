338242bddd313ef063f14e5ba616d45045c86952
William Desportes <williamdes@wdes.fr>
2020-09-25 20:22:32 +0200

    Replace Sami by Doctum (#4539)

--- doctum-config.php
+++ doctum-config.php
@@ -1,12 +1,12 @@
 <?php
 
 /**
- * A Sami config file. See https://github.com/FriendsOfPHP/Sami.
+ * A Doctum config file. See https://github.com/code-lts/doctum#readme.
  */
 
-use Sami\Sami;
-use Sami\RemoteRepository\GitHubRemoteRepository;
-use Sami\Version\GitVersionCollection;
+use Doctum\Doctum;
+use Doctum\RemoteRepository\GitHubRemoteRepository;
+use Doctum\Version\GitVersionCollection;
 use Symfony\Component\Finder\Finder;
 
 $dir = __DIR__.'/src';
@@ -22,15 +22,14 @@ $iterator = Finder::create()
 // Generate documentation for the main branch only
 $versions = GitVersionCollection::create($dir)
 //   ->addFromTags('10.*') // Also generate documentation for 10.x semver releases
-   ->add('10.x', 'Main branch')
-   ;
+   ->add('10.x', 'Main branch');
 
-return new Sami($iterator, array(
+return new Doctum($iterator, [
   // 'theme'                => 'symfony',
   'versions'             => $versions,
   'title'                => 'Drush API',
   'build_dir'            => __DIR__.'/gh-pages/api/%version%',
-  'cache_dir'            => __DIR__.'/.sami-cache/%version%',
+  'cache_dir'            => __DIR__.'/.doctum-cache/%version%',
   'remote_repository'    => new GitHubRemoteRepository('drush-ops/drush', dirname($dir)),
   'default_opened_level' => 2,
-));
+]);
