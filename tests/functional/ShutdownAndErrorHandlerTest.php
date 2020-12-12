bb0480029c7175c419f58c9bdfed52511d6fabe9
Moshe Weitzman <weitzman@tejasa.com>
2020-12-12 06:31:14 -0500

    Update docker-compose and circle to support php8 (#4602)
    * Update compose and circle to use php8
    
    * Update test assertions
    
    * Tweak assertion again.
    
    * flip it real good.
    
    * Skip the troublesome test
    
    * Cause an exit code via subprocess instead of in main process with php:eval
    
    * CS

--- tests/functional/ShutdownAndErrorHandlerTest.php
+++ tests/functional/ShutdownAndErrorHandlerTest.php
@@ -25,15 +25,15 @@ class ShutdownAndErrorHandlerTest extends CommandUnishTestCase
 
     /**
      * Check to see if the shutdown function is working
-     * and the exit code is passed through when script exist
+     * and the exit code is passed through when script exits
      * with a specific exit code.
      */
     public function testShutdownFunctionExitCodePassedThrough()
     {
-        // Run some garbage php with a syntax error.
-        $this->drush('ev', ['exit(123);'], [], null, null, 123);
-
-        $this->assertStringContainsString("Drush command terminated abnormally.", $this->getErrorOutput(), 'Error handler did not log a message.');
+        // script command passes along an exit code nicely for our purposes.
+        $this->drush('php:script', ['exit.php'], ['script-path' => __DIR__ . '/resources'], null, null, 123);
+        // Placate phpunit. If above succeeds we are done here.
+        $this->addToAssertionCount(1);
     }
 
     /**
@@ -56,10 +56,11 @@ class ShutdownAndErrorHandlerTest extends CommandUnishTestCase
         // Access a missing array element
         $this->drush('ev', ['$a = []; print $a["b"];']);
 
-        if (empty($this->logLevel())) {
+        if (empty($this->logLevel()) && PHP_MAJOR_VERSION <= 7) {
             $this->assertEquals('', $this->getErrorOutput(), 'Error handler did not suppress deprecated message.');
         } else {
-            $this->assertStringContainsString('Undefined index: b PhpCommands.php', $this->getErrorOutput());
+            $msg = PHP_MAJOR_VERSION >= 8 ? 'Undefined array key "b" PhpCommands.php' : 'Undefined index: b PhpCommands.php';
+            $this->assertStringContainsString($msg, $this->getErrorOutput());
         }
     }
 }
