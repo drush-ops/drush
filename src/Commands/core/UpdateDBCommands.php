b72a1382671a6370ec06d6ea5bbd747b699cb9ec
Moshe Weitzman <weitzman@tejasa.com>
2021-04-11 23:30:47 -0400

    Add topics for cim and updatedb

--- src/Commands/core/UpdateDBCommands.php
+++ src/Commands/core/UpdateDBCommands.php
@@ -31,6 +31,7 @@ class UpdateDBCommands extends DrushCommands implements SiteAliasManagerAwareInt
      * @option entity-updates Run automatic entity schema updates at the end of any update hooks. Not supported in Drupal >= 8.7.0.
      * @option post-updates Run post updates after hook_update_n and entity updates.
      * @bootstrap full
+     * @topics docs:deploy
      * @kernel update
      * @aliases updb
      */
