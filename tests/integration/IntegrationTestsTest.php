7450700c833a1fa050f3b83ce30363056ac20df3
Claudiu Cristea <clau.cristea@gmail.com>
2021-09-19 12:03:43 +0300

    Debug info preserved in stderr

---
+++ tests/integration/IntegrationTestsTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Unish;
+
+/**
+ * @coversDefaultClass \Unish\UnishIntegrationTestCase
+ * @group tests
+ */
+class IntegrationTestsTest extends UnishIntegrationTestCase
+{
+    /**
+     * @covers ::drush
+     */
+    public function testStdErr(): void
+    {
+        $this->drush('version', [], ['debug' => null]);
+        $this->assertStringContainsString('[debug] Starting bootstrap to none', $this->getErrorOutput());
+        $this->drush('version');
+        $this->assertStringNotContainsString('[debug] Starting bootstrap to none', $this->getErrorOutput());
+    }
+}
