208b960285fa0892edc80ebf6249f953a9823d42
Moshe Weitzman <weitzman@tejasa.com>
2021-10-31 22:42:01 -0400

    Remove custom log levels. (#4883)
    * Use standard log levels.
    
    * One more.
    
    * Just get rid of custom log levels now.
    
    * Bring back level translation as Drupal is unfortunate.
    
    * Less levels
    
    * Remove cruft.
    
    * Bring back Drush LogLevel class, as deprecated.

--- src/Drupal/DrushServiceModifier.php
+++ src/Drupal/DrushServiceModifier.php
@@ -3,7 +3,6 @@
 namespace Drush\Drupal;
 
 use Drush\Drush;
-use Drush\Log\LogLevel;
 use Drupal\Core\DependencyInjection\ServiceModifierInterface;
 use Drupal\Core\DependencyInjection\ContainerBuilder;
 
@@ -23,7 +22,7 @@ class DrushServiceModifier implements ServiceModifierInterface
      */
     public function alter(ContainerBuilder $container)
     {
-        Drush::logger()->log(LogLevel::DEBUG_NOTIFY, dt("Service modifier alter."));
+        Drush::logger()->debug(dt("Service modifier alter."));
         // http://symfony.com/doc/2.7/components/dependency_injection/tags.html#register-the-pass-with-the-container
         $container->register(self::DRUSH_CONSOLE_SERVICES, 'Drush\Command\ServiceCommandlist');
         $container->addCompilerPass(new FindCommandsCompilerPass(self::DRUSH_CONSOLE_SERVICES, 'console.command'));
