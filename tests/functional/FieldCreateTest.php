82d26c3c4dec6e37e65e7803776645fd98835e17
Moshe Weitzman <weitzman@tejasa.com>
2021-12-02 09:44:23 -0500

    PHPCS

--- tests/functional/FieldCreateTest.php
+++ tests/functional/FieldCreateTest.php
@@ -25,11 +25,11 @@ class FieldCreateTest extends CommandUnishTestCase
         // Arguments.
         $this->drush('field:create', [], [], null, null, self::EXIT_ERROR);
         $this->assertStringContainsString('Not enough arguments (missing: "entityType")', $this->getErrorOutputRaw());
-        $this->drush('field:create', ['foo'], [], null, null,self::EXIT_ERROR);
+        $this->drush('field:create', ['foo'], [], null, null, self::EXIT_ERROR);
         $this->assertStringContainsString('Entity type with id \'foo\' does not exist.', $this->getErrorOutputRaw());
         $this->drush('field:create', ['user'], [], null, null, self::EXIT_ERROR);
         $this->assertStringNotContainsString('The bundle argument is required.', $this->getErrorOutputRaw());
-        $this->drush('field:create', ['user', 'user'], [], null, null,self::EXIT_ERROR);
+        $this->drush('field:create', ['user', 'user'], [], null, null, self::EXIT_ERROR);
         $this->assertStringNotContainsString('bundle', $this->getErrorOutputRaw());
 
         // New field storage
