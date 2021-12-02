e543c3b9547707cbfb1368bb54b17ba4ab07e2da
Moshe Weitzman <weitzman@tejasa.com>
2021-12-02 11:56:47 -0500

    Revert unneeded test case changes.

--- tests/unish/UnishIntegrationTestCase.php
+++ tests/unish/UnishIntegrationTestCase.php
@@ -129,7 +129,7 @@ abstract class UnishIntegrationTestCase extends UnishTestCase
                     if (!isset($value)) {
                         $cmd[] = "--$key";
                     } else {
-                        $cmd[] = "--$key=" . self::escapeshellarg($value);
+                        $cmd[] = "--$key=" . $value;
                     }
                 }
             }
@@ -157,7 +157,7 @@ abstract class UnishIntegrationTestCase extends UnishTestCase
                 if (!isset($value) || $value === true) {
                     $cmd[] = "--$key";
                 } else {
-                    $cmd[] = "--$key=" . self::escapeshellarg($value);
+                    $cmd[] = "--$key=" . $value;
                 }
             }
         }
