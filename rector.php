2baa047f8eaf18745d000c4843a9b5343240ccd1
Dieter Holvoet <dieter.holvoet@gmail.com>
2021-12-31 16:50:43 +0100

    Update Rector config

--- rector.php
+++ rector.php
@@ -2,11 +2,19 @@
 
 declare(strict_types=1);
 
+use Rector\Core\Configuration\Option;
 use Rector\TypeDeclaration\Rector\FunctionLike\ParamTypeDeclarationRector;
 use Rector\TypeDeclaration\Rector\FunctionLike\ReturnTypeDeclarationRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return function (ContainerConfigurator $containerConfigurator): void {
+    $parameters = $containerConfigurator->parameters();
+    $parameters->set(Option::AUTO_IMPORT_NAMES, true);
+    $parameters->set(Option::IMPORT_SHORT_CLASSES, false);
+    $parameters->set(Option::PATHS, [
+        __DIR__ . '/src',
+    ]);
+
     $services = $containerConfigurator->services();
     $services->set(ParamTypeDeclarationRector::class);
     $services->set(ReturnTypeDeclarationRector::class);
