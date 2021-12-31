85eea99ec3cad3906a916bb392756b85b1444010
Greg Anderson <greg.1.anderson@greenknowe.org>
2021-12-30 18:29:37 -0600

    Test on Drupal 9.3, stop testing on Drupal 8 (EOL) (#4962)

--- tests/archiveDumpTest.php
+++ tests/archiveDumpTest.php
@@ -72,7 +72,8 @@ class archiveDumpCase extends CommandUnishTestCase {
     $untar_dest = $this->unTar($dump_dest);
 
     if (strpos(UNISH_DB_URL, 'mysql') !== FALSE) {
-      $this->execute(sprintf('head %s/unish_%s.sql | grep "MySQL dump"', $untar_dest, self::uri));
+      $this->assertFileExists($untar_dest . '/unish_' . self::uri . '.sql');
+      $this->execute(sprintf('head %s/unish_%s.sql | grep " dump"', $untar_dest, self::uri));
     }
     $this->assertFileExists($untar_dest . '/MANIFEST.ini');
     $this->assertFileExists($untar_dest . '/' . $docroot);
