4950e7b088116e5e7c349abbf78855578ccbed4d
Claudiu Cristea <clau.cristea@gmail.com>
2022-01-03 13:13:59 +0200

    Try to avoid memory overflow

--- tests/integration/MigrateRunnerTest.php
+++ tests/integration/MigrateRunnerTest.php
@@ -20,7 +20,7 @@ class MigrateRunnerTest extends UnishIntegrationTestCase
     {
         parent::setUp();
         $this->setupModulesForTests(['woot'], Path::join(__DIR__, '/../fixtures/modules'));
-        $this->drush('pm:enable', ['migrate', 'node', 'woot']);
+        \Drupal::service('module_installer')->install(['migrate', 'node', 'woot']);
     }
 
     /**
