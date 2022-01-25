8a1b0ad0b4ffbd9731e8a8c065d36ad81d776b1b
Moshe Weitzman <weitzman@tejasa.com>
2022-01-24 20:00:44 -0500

    Add back semver_example test on highest. (#5031)
    * Add back semver_example test on highest.
    
    * 3.0
    
    * Bring back CI for highest

--- tests/integration/SecurityUpdatesTest.php
+++ tests/integration/SecurityUpdatesTest.php
@@ -16,10 +16,7 @@ class SecurityUpdatesTest extends UnishIntegrationTestCase
    */
     public function testInsecureDrupalPackage()
     {
-        if (Semver::satisfies(\Drupal::VERSION, '^10')) {
-            $this->markTestSkipped('drupal/semver_example not yet compatible.');
-        }
-        list($expected_package, $expected_version) = ['drupal/semver_example', '2.2.0'];
+        list($expected_package, $expected_version) = ['drupal/semver_example', '2.3.0'];
         $this->drush('pm:security', [], ['format' => 'json'], self::EXIT_ERROR_WITH_CLARITY);
         $this->assertStringContainsString('One or more of your dependencies has an outstanding security update.', $this->getErrorOutput());
         $this->assertStringContainsString("$expected_package", $this->getErrorOutput());
