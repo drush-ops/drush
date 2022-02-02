948c7feec66c833c5b57adef95ed3894536a43ff
Sergei Churilo <sergei.churilo@pantheon.io>
2022-02-02 11:40:00 +0300

    Add scenarios to testArchiveDumpCommand()

--- tests/functional/ArchiveTest.php
+++ tests/functional/ArchiveTest.php
@@ -2,6 +2,8 @@
 
 namespace Unish;
 
+use Webmozart\PathUtil\Path;
+
 /**
  * @group slow
  * @group commands
@@ -11,6 +13,69 @@ class ArchiveTest extends CommandUnishTestCase
 {
     public function testArchiveDumpCommand()
     {
-        $this->markTestSkipped('Not implemented');
+        $options = [
+            'db' => null,
+            'exclude-code-paths' => 'sites/.+/settings.php',
+        ];
+
+        // Create an archive.
+        $this->drush(
+            'archive:dump',
+            [],
+            $options
+        );
+        $actualArchivePath = $this->getOutput();
+        $this->assertTrue((bool) preg_match('#\/archives\/\d+\/archive\.tar\.gz$#', $actualArchivePath));
+
+        // Create an archive with "--destination".
+        $expectedArchivePath = Path::join($this->getSandbox(), 'archive.tar.gz');
+        $this->drush(
+            'archive:dump',
+            [],
+            array_merge($options, [
+                'destination' => $expectedArchivePath,
+            ])
+        );
+        $actualArchivePath = $this->getOutput();
+        $this->assertEquals($expectedArchivePath, $actualArchivePath);
+
+        // Try to overwrite the existing archive with "--destination".
+        $this->drush(
+            'archive:dump',
+            [],
+            array_merge($options, [
+                'destination' => $expectedArchivePath,
+            ]),
+            null,
+            null,
+            self::EXIT_ERROR
+        );
+        $this->assertStringContainsString('The destination file already exists.', $this->getErrorOutput());
+
+        // Overwrite the existing archive with "--destination" and "--override".
+        $this->drush(
+            'archive:dump',
+            [],
+            array_merge($options, [
+                'destination' => $expectedArchivePath,
+                'overwrite' => null,
+            ])
+        );
+        $actualArchivePath = $this->getOutput();
+        $this->assertEquals($expectedArchivePath, $actualArchivePath);
+
+        // Validate database credentials are present in settings.php file.
+        $this->drush(
+            'archive:dump',
+            [],
+            [],
+            null,
+            null,
+            self::EXIT_ERROR
+        );
+        $this->assertStringContainsString(
+            'Found database connection settings in sites/default/settings.php',
+            $this->getErrorOutput()
+        );
     }
 }
