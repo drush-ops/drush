0384d95602f782e5fbdb80470048b9deeaf1cae3
Claudiu Cristea <clau.cristea@gmail.com>
2022-04-15 17:43:41 +0300

    Test coverage

--- tests/integration/CommandsFromConfigurationTest.php
+++ tests/integration/CommandsFromConfigurationTest.php
@@ -19,9 +19,11 @@ class CommandsFromConfigurationTest extends UnishIntegrationTestCase
             'config' => getenv('FIXTURES_DIR') . '/drush-extensions/drush.yml',
         ]);
         $this->assertStringContainsString('Command to load from this file using drush config.', $this->getOutput());
+        $this->assertStringContainsString('[warning] Discovering commands by configuration is deprecated in Drush 11.0.9 and is scheduled for removal in a future major version. The following command classes should be converted to PSR4 discovery (see docs/commands.md): Drush\Commands\drush_extensions\DrushExtensionsCommands', $this->getErrorOutputRaw());
         $this->drush('drush-extensions-hello', [], [
             'config' => getenv('FIXTURES_DIR') . '/drush-extensions/drush.yml',
         ]);
         $this->assertStringContainsString('Hello world!', $this->getOutput());
+        $this->assertStringContainsString('[warning] Discovering commands by configuration is deprecated in Drush 11.0.9 and is scheduled for removal in a future major version. The following command classes should be converted to PSR4 discovery (see docs/commands.md): Drush\Commands\drush_extensions\DrushExtensionsCommands', $this->getErrorOutputRaw());
     }
 }
