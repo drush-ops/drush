9f92c826bc34867e25d0f50fb62f4a8e00b5b964
Lucio Waßill <lucio@undpaul.de>
2022-07-11 10:12:36 +0200

    Allow to set the URL for the security advisories composer url via environment variable

--- src/Commands/pm/SecurityUpdateCommands.php
+++ src/Commands/pm/SecurityUpdateCommands.php
@@ -102,7 +102,8 @@ class SecurityUpdateCommands extends DrushCommands
     protected function fetchAdvisoryComposerJson()
     {
         $client = new Client(['handler' => $this->getStack()]);
-        $response = $client->get('https://raw.githubusercontent.com/drupal-composer/drupal-security-advisories/9.x/composer.json');
+        $security_advisories_composer_url = getenv('DRUSH_SECURITY_ADVISORIES_URL') ? getenv('DRUSH_SECURITY_ADVISORIES_URL') : 'https://raw.githubusercontent.com/drupal-composer/drupal-security-advisories/9.x/composer.json';
+        $response = $client->get($security_advisories_composer_url);
         $security_advisories_composer_json = json_decode($response->getBody(), true);
         return $security_advisories_composer_json;
     }
