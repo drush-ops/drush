c4b71046b2724e60de3156c14bd1574a4a26c47b
Moshe Weitzman <weitzman@tejasa.com>
2022-11-10 08:18:18 -0500

    Avoid errors in DDEV shell when using Symfony 4 (#5308)

--- .ddev/homeadditions/.bashrc.d/drush_completion.sh
+++ .ddev/homeadditions/.bashrc.d/drush_completion.sh
@@ -2,4 +2,5 @@
 #@todo make this not fail on Symfony 5-.
 # Make ./drush available on PATH.
 export PATH=/var/www/html:$PATH
-eval "$(drush completion bash)"
+# Suppress error output because this command is not present on Symfony 4.
+eval "$(drush completion bash 2>/dev/null)"
