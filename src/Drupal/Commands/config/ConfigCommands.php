5d954c5e29d61f3e19daa3031b0a8fbce744f9a5
Andreas Hennings <andreas@dqxtech.net>
2023-01-26 23:30:52 +0100

    Fix #3793: Don't validate config name on config:set.

--- src/Drupal/Commands/config/ConfigCommands.php
+++ src/Drupal/Commands/config/ConfigCommands.php
@@ -132,7 +132,6 @@ class ConfigCommands extends DrushCommands implements StdinAwareInterface, SiteA
      * Save a config value directly. Does not perform a config import.
      *
      * @command config:set
-     * @validate-config-name
      * @todo @interact-config-name deferred until we have interaction for key.
      * @param $config_name The config object name, for example <info>system.site</info>.
      * @param $key The config key, for example <info>page.front</info>. Use <info>?</info> if you are updating multiple top-level keys.
