0e44b180337ac02f26190568a52508dbc7ab01c6
Moshe Weitzman <weitzman@tejasa.com>
2023-02-27 14:30:56 -0500

    Attributes - JSONAPICommands

--- src/Drupal/Commands/core/JsonapiCommands.php
+++ src/Drupal/Commands/core/JsonapiCommands.php
@@ -3,6 +3,7 @@
 namespace Drush\Drupal\Commands\core;
 
 use Consolidation\OutputFormatters\StructuredData\UnstructuredData;
+use Drush\Attributes as CLI;
 use Drush\Commands\DrushCommands;
 use Drush\Drush;
 use Symfony\Component\HttpFoundation\Request;
@@ -22,6 +23,11 @@ class JsonapiCommands extends DrushCommands
      *   Pretty print JSON by piping to jq. See https://stedolan.github.io/jq/ for lots more jq features.
      * @validate-module-enabled jsonapi
      */
+    #[CLI\Command(name: 'jn:get')]
+    #[CLI\Argument(name: 'url', description: 'The JSONAPI URL to request.')]
+    #[CLI\Usage(name: 'drush jn:get jsonapi/node/article', description: 'Get a list of articles back as JSON.')]
+    #[CLI\Usage(name: 'drush jn:get jsonapi/node/article | jq', description: 'Pretty print JSON by piping to jq. See https://stedolan.github.io/jq/ for lots more jq features.')]
+    #[CLI\ValidateModulesEnabled(modules: ['jsonapi'])]
     public function get($url, $options = ['format' => 'json']): UnstructuredData
     {
         $kernel = Drush::bootstrap()->getKernel();
