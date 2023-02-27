c592f01a802b289214307dba760cc4d26568b37a
Moshe Weitzman <weitzman@tejasa.com>
2023-02-27 06:29:31 -0500

    Attrbutes in BatchCommands (#5399)

--- src/Drupal/Commands/core/BatchCommands.php
+++ src/Drupal/Commands/core/BatchCommands.php
@@ -3,18 +3,17 @@
 namespace Drush\Drupal\Commands\core;
 
 use Consolidation\OutputFormatters\StructuredData\UnstructuredListData;
+use Drush\Attributes as CLI;
 use Drush\Commands\DrushCommands;
 
 class BatchCommands extends DrushCommands
 {
     /**
      * Process operations in the specified batch set.
-     *
-     * @command batch:process
-     * @aliases batch-process
-     * @param $batch_id The batch id that will be processed.
-     * @hidden
      */
+    #[CLI\Command(name: 'batch:process', aliases: ['batch-process'])]
+    #[CLI\Argument(name: 'batch_id', description: 'The batch id that will be processed.')]
+    #[CLI\Help(hidden: true)]
     public function process($batch_id, $options = ['format' => 'json']): UnstructuredListData
     {
         $return = drush_batch_command($batch_id);
