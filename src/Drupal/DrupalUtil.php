8a7e99fc5969da9f1ba8ba2c06a9284603bf6575
Moshe Weitzman <weitzman@tejasa.com>
2023-03-15 11:25:18 -0400

    Render

--- src/Drupal/DrupalUtil.php
+++ src/Drupal/DrupalUtil.php
@@ -5,18 +5,19 @@ declare(strict_types=1);
 namespace Drush\Drupal;
 
 use Drupal\Core\Mail\MailFormatHelper;
+use Drupal\Core\Render\Markup;
 
 class DrupalUtil
 {
     /**
      * Output a Drupal render array, object or string as plain text.
      *
-     * @param string|array $data
+     * @param string|array|Markup $data
      *   Data to render.
      *
      *   The plain-text representation of the input.
      */
-    public static function drushRender(string|array $data): string
+    public static function drushRender($data): string
     {
         if (is_array($data)) {
             $data = \Drupal::service('renderer')->renderRoot($data);
