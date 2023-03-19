a46e66da50fbf08208cccda1a40235dee9a97554
Ivan <Chi-teck@users.noreply.github.com>
2023-03-19 17:49:35 +0500

    Handle missing URI in Drush alias file generator (#5484)
    * Handle missing URI in Drush alias file generator
    
    * Fix code style
    
    * Add generator namespace

--- src/Drupal/Commands/generate/Generators/Drush/DrushAliasFile.php
+++ src/Drupal/Commands/generate/Generators/Drush/DrushAliasFile.php
@@ -5,11 +5,12 @@ declare(strict_types=1);
 namespace Drush\Drupal\Commands\generate\Generators\Drush;
 
 use DrupalCodeGenerator\Asset\AssetCollection as Assets;
+use DrupalCodeGenerator\Attribute\Generator;
 use DrupalCodeGenerator\Command\BaseGenerator;
 use DrupalCodeGenerator\GeneratorType;
 use Drush\Drush;
 
-#[\DrupalCodeGenerator\Attribute\Generator(
+#[Generator(
     name: 'drush:alias-file',
     description: 'Generates a Drush site alias file.',
     aliases: ['daf'],
@@ -27,7 +28,7 @@ class DrushAliasFile extends BaseGenerator
 
         $vars['prefix'] = $ir->ask('File prefix (one word)', 'self');
         $vars['root'] = $ir->ask('Path to Drupal root', Drush::bootstrapManager()->getRoot());
-        $vars['uri'] = $ir->ask('Drupal uri', Drush::bootstrapManager()->getUri());
+        $vars['uri'] = $ir->ask('Drupal uri', Drush::bootstrapManager()->getUri() ?: null);
         $vars['host'] = $ir->ask('Remote host');
 
         if ($vars['host']) {
