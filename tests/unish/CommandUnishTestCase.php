34daefedff14bf023da6c3fbf50220b24f179e11
Moshe Weitzman <weitzman@tejasa.com>
2023-03-21 09:30:16 -0400

    No need for ocde coverage anymore.

--- tests/unish/CommandUnishTestCase.php
+++ tests/unish/CommandUnishTestCase.php
@@ -139,14 +139,14 @@ abstract class CommandUnishTestCase extends UnishTestCase
         // Insert code coverage argument before command, in order for it to be
         // parsed as a global option. This matters for commands like ssh and rsync
         // where options after the command are passed along to external commands.
-        $coverage_file = null;
-        $result = $this->getTestResultObject();
-        if ($result->getCollectCodeCoverageInformation()) {
-            $coverage_file = tempnam($this->getSandbox(), 'drush_coverage');
-            if ($coverage_file) {
-                $cmd[] = "--drush-coverage=" . $coverage_file;
-            }
-        }
+//        $coverage_file = null;
+//        $result = $this->getTestResultObject();
+//        if ($result->getCollectCodeCoverageInformation()) {
+//            $coverage_file = tempnam($this->getSandbox(), 'drush_coverage');
+//            if ($coverage_file) {
+//                $cmd[] = "--drush-coverage=" . $coverage_file;
+//            }
+//        }
 
         // Insert site specification and drush command.
         $cmd[] = empty($site_specification) ? null : self::escapeshellarg($site_specification);
