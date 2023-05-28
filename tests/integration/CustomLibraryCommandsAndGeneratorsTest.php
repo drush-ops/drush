eb6262ad7268d26bc53d0fcaed163d734561616b
Moshe Weitzman <weitzman@tejasa.com>
2023-05-28 10:59:44 -0400

    hide testing generator (#5594)
    * hide testing generator
    
    * Hide birds command
    
    * hide testing-generator from drush.org - cant test list command presence

--- tests/integration/CustomLibraryCommandsAndGeneratorsTest.php
+++ tests/integration/CustomLibraryCommandsAndGeneratorsTest.php
@@ -20,8 +20,10 @@ class CustomLibraryCommandsAndGeneratorsTest extends UnishIntegrationTestCase
         $this->assertStringContainsString('Auto-discoverable custom command. Used for Drush testing.', $this->getOutput());
         $this->drush('custom_cmd');
         $this->assertStringContainsString('Hello world!', $this->getOutput());
-        $this->drush(GenerateCommands::GENERATE, ['list']);
-        $this->assertStringContainsString('drush:testing-generator', $this->getOutput());
-        $this->assertStringContainsString('An internal generator', $this->getOutput());
+
+        // This is now a hidden command so it stays off the www.drush.org site.
+//        $this->drush(GenerateCommands::GENERATE, ['list']);
+//        $this->assertStringContainsString('drush:testing-generator', $this->getOutput());
+//        $this->assertStringContainsString('An internal generator', $this->getOutput());
     }
 }
