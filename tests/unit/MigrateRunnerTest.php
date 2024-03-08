e1924ae72e092ae21577b9f585c508452e18ec93
mondrake <mondrake.org@gmail.com>
2024-03-08 10:09:02 +0000

    use Database API in MigrateRunnerTest

--- tests/unit/MigrateRunnerTest.php
+++ tests/unit/MigrateRunnerTest.php
@@ -5,9 +5,9 @@ declare(strict_types=1);
 namespace Drush\Drupal\Migrate;
 
 use Composer\Semver\Comparator;
+use Drupal\Core\Database\Database;
 use Drupal\migrate\Plugin\MigrationInterface;
 use Drupal\migrate\Plugin\MigrationPluginManagerInterface;
-use Drupal\sqlite\Driver\Database\sqlite\Connection;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\Filesystem\Path;
@@ -153,15 +153,13 @@ class MigrateRunnerTest extends TestCase
         if (!extension_loaded('pdo_sqlite')) {
             $this->markTestSkipped('The pdo_sqlite extension is not available.');
         }
-        $options['database'] = ':memory:';
-        $pdo = new \PDO("sqlite::memory:");
-        /** @var \Composer\Autoload\ClassLoader $loader */
-        $loader = require PHPUNIT_COMPOSER_INSTALL;
-        if (!isset($loader->getPrefixesPsr4()['Drupal\\sqlite\\'])) {
-            // @todo this needs to fetch the base path from somewhere, no hardcoding.
-            $loader->addPsr4('Drupal\\sqlite\\', '/home/wodby/drush/vendor/composer/../../sut/core/modules/sqlite/src');
-        }
-        $connection = new Connection($pdo, $options);
+
+        $cwd = getcwd();
+        chdir($this->webroot());
+        $info = Database::convertDbUrlToConnectionInfo('sqlite://localhost/:memory:?module=sqlite', $this->webroot());
+        Database::addConnectionInfo('default', 'default', $info);
+        $connection = Database::getConnection();
+        chdir($cwd);
 
         // Create the table and load it with data.
         $mapTableSchema = $this->getMapTableSchema();
@@ -182,6 +180,11 @@ class MigrateRunnerTest extends TestCase
         $insert->execute();
 
         return $connection;
+    }
+
+    public static function webroot(): string
+    {
+        return Path::join(Path::canonicalize(dirname(__DIR__, 2)), 'sut');
     }
 
     /**
