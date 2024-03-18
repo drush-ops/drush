f8b06a88295e2a61ecf988883d9c9d28cf513872
Ryan Price <rprice@ryanpricemedia.com>
2024-03-18 09:33:18 -0700

    Update ArchiveDumpCommands.php

--- src/Commands/core/ArchiveDumpCommands.php
+++ src/Commands/core/ArchiveDumpCommands.php
@@ -97,7 +97,7 @@ final class ArchiveDumpCommands extends DrushCommands
         'generator' => InputOption::VALUE_REQUIRED,
         'generatorversion' => InputOption::VALUE_REQUIRED,
         'exclude-code-paths' => InputOption::VALUE_REQUIRED,
-        'extra-dump' => InputOption::VALUE_REQUIRED,
+        'extra-dump' => false,
     ]): string
     {
         $this->prepareArchiveDir();
