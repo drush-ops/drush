01148c09360f1dc55fe2dc16134e8d906120e381
Moshe Weitzman <weitzman@tejasa.com>
2024-09-05 14:59:21 -0400

    entity:save can now read ids from Stdin and can publish/unpublish before re-saving

--- src/Commands/core/EntityCommands.php
+++ src/Commands/core/EntityCommands.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Drush\Commands\core;
 
+use Consolidation\AnnotatedCommand\Input\StdinAwareInterface;
+use Consolidation\AnnotatedCommand\Input\StdinAwareTrait;
 use Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException;
 use Drupal\Component\Plugin\Exception\PluginNotFoundException;
 use Drupal\Core\Entity\EntityStorageException;
@@ -14,9 +16,10 @@ use Drush\Commands\AutowireTrait;
 use Drush\Commands\DrushCommands;
 use Drush\Utils\StringUtils;
 
-final class EntityCommands extends DrushCommands
+final class EntityCommands extends DrushCommands implements StdinAwareInterface
 {
     use AutowireTrait;
+    use StdinAwareTrait;
 
     const DELETE = 'entity:delete';
     const SAVE = 'entity:save';
@@ -86,23 +89,39 @@ final class EntityCommands extends DrushCommands
     }
 
     /**
-     * Load and save entities.
+     * Re-save entities, and publish/unpublish as specified.
      */
     #[CLI\Command(name: self::SAVE, aliases: ['esav', 'entity-save'])]
     #[CLI\Argument(name: 'entity_type', description: 'An entity machine name.')]
-    #[CLI\Argument(name: 'ids', description: 'A comma delimited list of Ids.')]
+    #[CLI\Argument(name: 'ids', description: 'A comma delimited list of Ids. The list is read from Stdin if a value of <info>-</info> is provided for this option.')]
     #[CLI\Option(name: 'bundle', description: 'Restrict to the specified bundle. Ignored when ids is specified.')]
     #[CLI\Option(name: 'exclude', description: 'Exclude certain entities. Ignored when ids is specified.')]
     #[CLI\Option(name: 'chunks', description: 'Define how many entities will be loaded in the same step.')]
+    #[CLI\Option(name: 'publish', description: 'Publish entities as they are saved.')]
+    #[CLI\Option(name: 'unpublish', description: 'Unpublish entities as they are saved.')]
     #[CLI\Usage(name: 'drush entity:save node --bundle=article', description: 'Re-save all article entities.')]
     #[CLI\Usage(name: 'drush entity:save shortcut', description: 'Re-save all shortcut entities.')]
     #[CLI\Usage(name: 'drush entity:save node 22,24', description: 'Re-save nodes 22 and 24.')]
+    #[CLI\Usage(name: 'echo 22,24 | drush entity:save node -', description: 'Re-save nodes 22 and 24. Ids are passed via Stdin.')]
     #[CLI\Usage(name: 'drush entity:save node --exclude=9,14,81', description: 'Re-save all nodes except node 9, 14 and 81.')]
     #[CLI\Usage(name: 'drush entity:save user', description: 'Re-save all users.')]
     #[CLI\Usage(name: 'drush entity:save node --chunks=5', description: 'Re-save all node entities in steps of 5.')]
     #[CLI\Version(version: '11.0')]
-    public function loadSave(string $entity_type, $ids = null, array $options = ['bundle' => self::REQ, 'exclude' => self::REQ, 'chunks' => 50]): void
+    public function loadSave(string $entity_type, $ids = null, array $options = ['bundle' => self::REQ, 'exclude' => self::REQ, 'chunks' => 50, 'publish' => false, 'unpublish' => false]): void
     {
+        if ($options['publish'] && $options['unpublish']) {
+            throw new \InvalidArgumentException(dt('You cannot specify both --publish and --unpublish.'));
+        }
+
+        $action = null;
+        if ($options['publish']) {
+            $action = 'publish';
+        } elseif ($options['unpublish']) {
+            $action = 'unpublish';
+        }
+        if ($ids === '-') {
+            $ids = $this->stdin()->contents();
+        }
         $query = $this->getQuery($entity_type, $ids, $options);
         $result = $query->execute();
 
@@ -113,11 +132,14 @@ final class EntityCommands extends DrushCommands
             $progress = $this->io()->progress('Saving entities', count($chunks));
             $progress->start();
             foreach ($chunks as $chunk) {
-                drush_op([$this, 'doSave'], $entity_type, $chunk);
+                drush_op([$this, 'doSave'], $entity_type, $chunk, $action);
                 $progress->advance();
             }
             $progress->finish();
             $this->logger()->success(dt("Saved !type entity ids: !ids", ['!type' => $entity_type, '!ids' => implode(', ', array_values($result))]));
+            if ($action) {
+                $this->logger()->success(dt("Entities have been !actioned.", ['!action' => $action]));
+            }
         }
     }
 
@@ -129,11 +151,16 @@ final class EntityCommands extends DrushCommands
      * @throws PluginNotFoundException
      * @throws EntityStorageException
      */
-    public function doSave(string $entity_type, array $ids): void
+    public function doSave(string $entity_type, array $ids, ?string $action): void
     {
         $storage = $this->entityTypeManager->getStorage($entity_type);
         $entities = $storage->loadMultiple($ids);
         foreach ($entities as $entity) {
+            if ($action === 'publish') {
+                $entity->setPublished(true);
+            } elseif ($action === 'unpublish') {
+                $entity->setPublished(false);
+            }
             $entity->save();
         }
     }
