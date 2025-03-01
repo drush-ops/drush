ae9d1bf98679de3ca39e09cbb433385e7ce9d119
Moshe Weitzman <weitzman@tejasa.com>
2025-03-01 07:10:01 -0500

    Fix WootCommandInfoAlterer
    Command Info Altering will change anyway in Drush 14 so just use old approach until then

--- src/Runtime/DependencyInjection.php
+++ src/Runtime/DependencyInjection.php
@@ -12,7 +12,9 @@ use Consolidation\SiteAlias\SiteAliasManagerAwareInterface;
 use Consolidation\SiteAlias\SiteAliasManagerInterface;
 use Consolidation\SiteProcess\ProcessManagerAwareInterface;
 use Drush\Application;
+use Drush\Boot\BootstrapHook;
 use Drush\Boot\BootstrapManager;
+use Drush\Boot\DrupalBoot8;
 use Drush\Cache\CommandCache;
 use Drush\Command\DrushCommandInfoAlterer;
 use Drush\Command\GlobalOptionsEventListener;
@@ -116,25 +118,25 @@ class DependencyInjection
             ->addMethodCall('addSimplifier', [new EntityToArraySimplifier()]);
 
         // Add some of our own objects to the container
-        Robo::addShared($container, 'service.manager', 'Drush\Runtime\ServiceManager')
+        Robo::addShared($container, 'service.manager', ServiceManager::class)
             ->addArgument(self::LOADER)
             ->addArgument('config')
             ->addArgument('logger');
-        Robo::addShared($container, 'bootstrap.drupal8', 'Drush\Boot\DrupalBoot8')
+        Robo::addShared($container, 'bootstrap.drupal8', DrupalBoot8::class)
             ->addArgument('service.manager')
             ->addArgument(self::LOADER);
-        Robo::addShared($container, self::BOOTSTRAP_MANAGER, 'Drush\Boot\BootstrapManager')
+        Robo::addShared($container, self::BOOTSTRAP_MANAGER, BootstrapManager::class)
             ->addMethodCall('setDrupalFinder', [$drupalFinder])
             ->addMethodCall('add', ['bootstrap.drupal8']);
         Robo::addShared($container, BootstrapManager::class, self::BOOTSTRAP_MANAGER); // For autowiring
-        Robo::addShared($container, 'bootstrap.hook', 'Drush\Boot\BootstrapHook')
+        Robo::addShared($container, 'bootstrap.hook', BootstrapHook::class)
           ->addArgument(self::BOOTSTRAP_MANAGER);
-        Robo::addShared($container, 'tildeExpansion.hook', 'Drush\Runtime\TildeExpansionHook');
+        Robo::addShared($container, 'tildeExpansion.hook', TildeExpansionHook::class);
         Robo::addShared($container, 'process.manager', ProcessManager::class)
             ->addMethodCall('setConfig', ['config'])
             ->addMethodCall('setConfigRuntime', ['config.runtime'])
             ->addMethodCall('setDrupalFinder', [$drupalFinder]);
-        Robo::addShared($container, 'redispatch.hook', 'Drush\Runtime\RedispatchHook')
+        Robo::addShared($container, 'redispatch.hook', RedispatchHook::class)
             ->addArgument('process.manager');
 
         // Robo does not manage the command discovery object in the container,
@@ -146,8 +148,8 @@ class DependencyInjection
             ->addMethodCall('setSearchPattern', ['#.*(Commands|CommandFile).php$#']);
 
         // Error and Shutdown handlers
-        Robo::addShared($container, 'errorHandler', 'Drush\Runtime\ErrorHandler');
-        Robo::addShared($container, 'shutdownHandler', 'Drush\Runtime\ShutdownHandler');
+        Robo::addShared($container, 'errorHandler', ErrorHandler::class);
+        Robo::addShared($container, 'shutdownHandler', ShutdownHandler::class);
 
         // Add inflectors. @see \Drush\Boot\BaseBoot::inflect
         $container->inflector(SiteAliasManagerAwareInterface::class)
