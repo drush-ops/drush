4a179186a8f5e6ba8051321c399b9b4e46747b2e
Moshe Weitzman <weitzman@tejasa.com>
2025-03-26 15:05:14 -0400

    sql:dump - better topics and help links

--- src/Attributes/OptionsetTableSelection.php
+++ src/Attributes/OptionsetTableSelection.php
@@ -13,8 +13,8 @@ class OptionsetTableSelection
 {
     public static function handle(\ReflectionAttribute $attribute, CommandInfo $commandInfo)
     {
-        $commandInfo->addOption('skip-tables-key', 'A key in the $skip_tables array. @see [Site aliases](../site-aliases.md)', [], DrushCommands::REQ);
-        $commandInfo->addOption('structure-tables-key', 'A key in the $structure_tables array. @see [Site aliases](../site-aliases.md)', [], DrushCommands::REQ);
+        $commandInfo->addOption('skip-tables-key', 'A key in the $skip_tables array. @see [Drush config](../using-drush-configuration.md)', [], DrushCommands::REQ);
+        $commandInfo->addOption('structure-tables-key', 'A key in the $structure_tables array. @see [Drush config](../using-drush-configuration.md)', [], DrushCommands::REQ);
         $commandInfo->addOption('tables-key', 'A key in the $tables array.', [], DrushCommands::REQ);
         $commandInfo->addOption('skip-tables-list', 'A comma-separated list of tables to exclude completely.', [], DrushCommands::REQ);
         $commandInfo->addOption('structure-tables-list', 'A comma-separated list of tables to include for structure, but not data.', [], DrushCommands::REQ);
