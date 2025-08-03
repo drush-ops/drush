35294a68abdfb0494fec9aa6ea09844b09187724
Moshe Weitzman <weitzman@tejasa.com>
2025-08-02 23:28:33 -0400

    rm

--- src/Attributes/Bootstrap.php
+++ src/Attributes/Bootstrap.php
@@ -7,7 +7,6 @@ namespace Drush\Attributes;
 use Attribute;
 use Consolidation\AnnotatedCommand\Parser\CommandInfo;
 use Drush\Boot\DrupalBootLevels;
-use Exception;
 use JetBrains\PhpStorm\ExpectedValues;
 
 #[Attribute(Attribute::TARGET_METHOD | Attribute::TARGET_CLASS)]
