0a3d976445a474c4f1ebddd2a47c81e0f4efbc31
Moshe Weitzman <weitzman@tejasa.com>
2025-09-29 07:26:31 -0400

    Port remaining config commands to Console style

--- src/Commands/config/ConfigCommands.php
+++ src/Commands/config/ConfigCommands.php
@@ -9,32 +9,18 @@ use Consolidation\AnnotatedCommand\CommandError;
 use Consolidation\AnnotatedCommand\Hooks\HookManager;
 use Consolidation\AnnotatedCommand\Input\StdinAwareInterface;
 use Consolidation\AnnotatedCommand\Input\StdinAwareTrait;
-use Consolidation\OutputFormatters\StructuredData\RowsOfFields;
 use Consolidation\SiteAlias\SiteAliasManagerInterface;
-use Consolidation\SiteProcess\Util\Escape;
-use Drupal\Core\Config\ConfigDirectoryNotDefinedException;
 use Drupal\Core\Config\ConfigFactoryInterface;
-use Drupal\Core\Config\FileStorage;
 use Drupal\Core\Config\ImportStorageTransformer;
-use Drupal\Core\Config\StorageComparer;
 use Drupal\Core\Config\StorageInterface;
 use Drupal\Core\Config\StorageManagerInterface;
-use Drupal\Core\Site\Settings;
 use Drush\Attributes as CLI;
 use Drush\Commands\AutowireTrait;
 use Drush\Commands\DrushCommands;
-use Drush\Drush;
 use Drush\Exec\ExecTrait;
-use Drush\Utils\FsUtils;
 use Drush\Utils\StringUtils;
 use JetBrains\PhpStorm\Deprecated;
-use RuntimeException;
-use Symfony\Component\Console\Completion\CompletionInput;
-use Symfony\Component\Console\Completion\CompletionSuggestions;
-use Symfony\Component\Console\Helper\Table;
-use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
-use Symfony\Component\Filesystem\Path;
 
 final class ConfigCommands extends DrushCommands implements StdinAwareInterface
 {
@@ -48,8 +34,11 @@ final class ConfigCommands extends DrushCommands implements StdinAwareInterface
     const GET = 'config:get';
     #[Deprecated(reason: 'Use ConfigSetCommand::NAME')]
     const SET = 'config:set';
+    #[Deprecated(reason: 'Use ConfigEditCommand::NAME')]
     const EDIT = 'config:edit';
+    #[Deprecated(reason: 'Use ConfigDeleteCommand::NAME')]
     const DELETE = 'config:delete';
+    #[Deprecated(reason: 'Use ConfigStatusCommand::NAME')]
     const STATUS = 'config:status';
 
     public function getConfigFactory(): ConfigFactoryInterface
@@ -68,284 +57,7 @@ final class ConfigCommands extends DrushCommands implements StdinAwareInterface
         parent::__construct();
     }
 
-    public function hasImportTransformer(): bool
-    {
-        return isset($this->importStorageTransformer);
-    }
-
-    public function getImportTransformer(): ImportStorageTransformer
-    {
-        return $this->importStorageTransformer;
-    }
-
-    /**
-     * Open a config file in a text editor. Edits are imported after closing editor.
-     */
-    #[CLI\Command(name: self::EDIT, aliases: ['cedit', 'config-edit'])]
-    #[CLI\Argument(name: 'config_name', description: 'The config object name, for example <info>system.site</info>.')]
-    #[CLI\Usage(name: 'drush config:edit image.style.large', description: 'Edit the image style configurations.')]
-    #[CLI\Usage(name: 'drush config:edit', description: 'Choose a config file to edit.')]
-    #[CLI\Usage(name: 'drush --bg config-edit image.style.large', description: 'Return to shell prompt as soon as the editor window opens.')]
-    #[CLI\OptionsetGetEditor()]
-    #[CLI\ValidateModulesEnabled(modules: ['config'])]
-    #[CLI\ValidateConfigName()]
-    #[CLI\InteractConfigName()]
-    #[CLI\Complete(method_name_or_callable: 'configComplete')]
-    public function edit($config_name, $options = []): void
-    {
-        $config = $this->getConfigFactory()->get($config_name);
-        $active_storage = $config->getStorage();
-        $contents = $active_storage->read($config_name);
-
-        // Write tmp YAML file for editing
-        $temp_dir = drush_tempdir();
-        $temp_storage = new FileStorage($temp_dir);
-        $temp_storage->write($config_name, $contents);
-
-        // Note that `getEditor()` returns a string that contains a
-        // %s placeholder for the config file path.
-        $exec = self::getEditor($options['editor']);
-        $cmd = sprintf($exec, Escape::shellArg($temp_storage->getFilePath($config_name)));
-        $process = $this->processManager()->shell($cmd);
-        $process->setTty(true);
-        $process->mustRun();
-
-        // Perform import operation if user did not immediately exit editor.
-        if (!$options['bg']) {
-            $redispatch_options = Drush::redispatchOptions() + ['strict' => 0, 'partial' => true, 'source' => $temp_dir];
-            $self = $this->siteAliasManager->getSelf();
-            $process = $this->processManager()->drush($self, ConfigImportCommands::IMPORT, [], $redispatch_options);
-            $process->mustRun($process->showRealtime());
-        }
-    }
-
-    /**
-     * Delete a configuration key, or a whole object(s).
-     */
-    #[CLI\Command(name: self::DELETE, aliases: ['cdel', 'config-delete'])]
-    #[CLI\Argument(name: 'config_name', description: 'The config object name(s). Delimit multiple with commas.')]
-    #[CLI\Argument(name: 'key', description: 'A config key to clear, May not be used with multiple config names.')]
-    #[CLI\Usage(name: 'drush config:delete system.site,system.rss', description: 'Delete the system.site and system.rss config objects.')]
-    #[CLI\Usage(name: 'drush config:delete system.site page.front', description: "Delete the 'page.front' key from the system.site object.")]
-    #[CLI\Complete(method_name_or_callable: 'configComplete')]
-    #[CLI\ValidateConfigName()]
-    #[CLI\InteractConfigName()]
-    public function delete($config_name, $key = null): void
-    {
-        if ($key) {
-            $config = $this->getConfigFactory()->getEditable($config_name);
-            if ($config->get($key) === null) {
-                throw new \Exception(dt('Configuration key !key not found.', ['!key' => $key]));
-            }
-            $config->clear($key)->save();
-        } else {
-            $names = StringUtils::csvToArray($config_name);
-            foreach ($names as $name) {
-                $config = $this->getConfigFactory()->getEditable($name);
-                $config->delete();
-            }
-        }
-    }
-
-    /**
-     * Display status of configuration (differences between the filesystem and database).
-     */
-    #[CLI\Command(name: self::STATUS, aliases: ['cst', 'config-status'])]
-    #[CLI\Option(name: 'state', description: 'A comma-separated list of states to filter results.')]
-    #[CLI\Option(name: 'prefix', description: 'The config prefix. For example, <info>system</info>. No prefix will return all names in the system.')]
-    #[CLI\Usage(name: 'drush config:status', description: 'Display configuration items that need to be synchronized.')]
-    #[CLI\Usage(name: 'drush config:status --state=Identical', description: 'Display configuration items that are in default state.')]
-    #[CLI\Usage(name: "drush config:status --state='Only in sync dir' --prefix=node.type.", description: 'Display all content types that would be created in active storage on configuration import.')]
-    #[CLI\Usage(name: 'drush config:status --state=Any --format=list', description: 'List all config names.')]
-    #[CLI\Usage(name: 'drush config:status 2>&amp;1 | grep "No differences"', description: 'Check there are no differences between database and exported config. Useful for CI.')]
-    #[CLI\FieldLabels(labels: ['name' => 'Name', 'state' => 'State'])]
-    #[CLI\DefaultTableFields(fields: ['name', 'state'])]
-    #[CLI\FilterDefaultField(field: 'name')]
-    public function status($options = ['state' => 'Only in DB,Only in sync dir,Different', 'prefix' => self::REQ]): ?RowsOfFields
-    {
-        $config_list = array_fill_keys(
-            $this->configFactory->listAll($options['prefix']),
-            'Identical'
-        );
-
-        $directory = $this->getDirectory();
-        $storage = $this->getStorage($directory);
-        $state_map = [
-            'create' => 'Only in DB',
-            'update' => 'Different',
-            'delete' => 'Only in sync dir',
-        ];
-        foreach ($this->getChanges($storage) as $collection) {
-            foreach ($collection as $operation => $configs) {
-                foreach ($configs as $config) {
-                    if (!$options['prefix'] || str_starts_with($config, $options['prefix'])) {
-                        $config_list[$config] = $state_map[$operation];
-                    }
-                }
-            }
-        }
-
-        if ($options['state']) {
-            $allowed_states = explode(',', $options['state']);
-            if (!in_array('Any', $allowed_states)) {
-                $config_list = array_filter($config_list, function ($state) use ($allowed_states) {
-                     return in_array($state, $allowed_states);
-                });
-            }
-        }
-
-        ksort($config_list);
-
-        $rows = [];
-        $color_map = [
-            'Only in DB' => 'green',
-            'Only in sync dir' => 'red',
-            'Different' => 'yellow',
-            'Identical' => 'white',
-        ];
-
-        foreach ($config_list as $config => $state) {
-            if ($options['format'] == 'table' && $state != 'Identical') {
-                $state = "<fg={$color_map[$state]};options=bold>$state</>";
-            }
-            $rows[$config] = [
-                'name' => $config,
-                'state' => $state,
-            ];
-        }
-
-        if (!$rows) {
-            $this->logger()->notice(dt('No differences between DB and sync directory.'));
-
-            // Suppress output if there are no differences and we are using the
-            // human readable "table" formatter so that we not uselessly output
-            // empty table headers.
-            if ($options['format'] === 'table') {
-                return null;
-            }
-        }
-
-        return new RowsOfFields($rows);
-    }
-
-    /**
-     * Determine which configuration directory to use and return directory path.
-     *
-     * Directory path is determined based on the following precedence:
-     *   1. User-provided $directory.
-     *   2. Default sync directory
-     *
-     * @note: $directory param can be boolean.
-     */
-    public static function getDirectory(mixed $directory = null): string
-    {
-        $return = null;
-        // If the user provided a directory, use it.
-        if (!empty($directory)) {
-            if ($directory === true) {
-                // The user did not pass a specific directory, make one.
-                $return = FsUtils::prepareBackupDir('config-import-export');
-            } else {
-                // The user has specified a directory.
-                drush_mkdir($directory);
-                $return = $directory;
-            }
-        } else {
-            // If a directory isn't specified, use default sync directory.
-            $return = Settings::get('config_sync_directory', false);
-            if ($return === false) {
-                throw new ConfigDirectoryNotDefinedException('The config sync directory is not defined in $settings["config_sync_directory"]');
-            }
-        }
-        return Path::canonicalize($return);
-    }
-
-    /**
-     * Returns the difference in configuration between active storage and target storage.
-     */
-    public function getChanges($target_storage): array
-    {
-        if ($this->hasImportTransformer()) {
-            $target_storage = $this->getImportTransformer()->transform($target_storage);
-        }
-
-        $config_comparer = new StorageComparer($this->configStorage, $target_storage);
-
-        $change_list = [];
-        if ($config_comparer->createChangelist()->hasChanges()) {
-            foreach ($config_comparer->getAllCollectionNames() as $collection) {
-                $change_list[$collection] = $config_comparer->getChangelist(null, $collection);
-            }
-        }
-        return $change_list;
-    }
-
-    /**
-     * Get storage corresponding to a configuration directory.
-     */
-    public function getStorage($directory)
-    {
-        if ($directory == Path::canonicalize(Settings::get('config_sync_directory'))) {
-            return \Drupal::service('config.storage.sync');
-        } else {
-            return new FileStorage($directory);
-        }
-    }
-
-    /**
-     * Build a table of config changes.
-     *
-     * @param array $config_changes
-     *   An array of changes keyed by collection.
-     */
-    public static function configChangesTable(array $config_changes, OutputInterface $output, $use_color = true): Table
-    {
-        $rows = [];
-        foreach ($config_changes as $collection => $changes) {
-            foreach ($changes as $change => $configs) {
-                switch ($change) {
-                    case 'delete':
-                        $colour = '<fg=white;bg=red>';
-                        break;
-                    case 'update':
-                        $colour = '<fg=black;bg=yellow>';
-                        break;
-                    case 'create':
-                        $colour = '<fg=white;bg=green>';
-                        break;
-                    default:
-                        $colour = "<fg=black;bg=cyan>";
-                        break;
-                }
-                if ($use_color) {
-                    $prefix = $colour;
-                    $suffix = '</>';
-                } else {
-                    $prefix = $suffix = '';
-                }
-                foreach ($configs as $config) {
-                    $rows[] = [
-                        $collection,
-                        $config,
-                        $prefix . ucfirst($change) . $suffix,
-                    ];
-                }
-            }
-        }
-        $table = new Table($output);
-        $table->setHeaders(['Collection', 'Config', 'Operation']);
-        $table->addRows($rows);
-        return $table;
-    }
-
-    public function configComplete(CompletionInput $input, CompletionSuggestions $suggestions): void
-    {
-        if ($input->mustSuggestArgumentValuesFor('config_name')) {
-            $suggestions->suggestValues($this->getConfigFactory()->listAll());
-        }
-    }
-
-    #[Deprecated('Use CLI/InteractConfigName Attribute instead')]
+    #[Deprecated('Use an interact() method instead')]
     #[CLI\Hook(type: HookManager::INTERACT, selector: self::INTERACT_CONFIG_NAME)]
     public function interactConfigName($input, $output): void
     {
@@ -374,76 +86,5 @@ final class ConfigCommands extends DrushCommands implements StdinAwareInterface
             }
         }
         return null;
-    }
-
-    /**
-     * Copies configuration objects from source storage to target storage.
-     *
-     * @param StorageInterface $source
-     *   The source config storage service.
-     * @param StorageInterface $destination
-     *   The destination config storage service.
-     * @throws \Exception
-     */
-    public static function copyConfig(StorageInterface $source, StorageInterface $destination): void
-    {
-        // Make sure the source and destination are on the default collection.
-        if ($source->getCollectionName() != StorageInterface::DEFAULT_COLLECTION) {
-            $source = $source->createCollection(StorageInterface::DEFAULT_COLLECTION);
-        }
-        if ($destination->getCollectionName() != StorageInterface::DEFAULT_COLLECTION) {
-            $destination = $destination->createCollection(StorageInterface::DEFAULT_COLLECTION);
-        }
-
-        // Export all the configuration.
-        foreach ($source->listAll() as $name) {
-            try {
-                $destination->write($name, $source->read($name));
-            } catch (\TypeError $e) {
-                throw new \Exception(dt('Source not found for @name.', ['@name' => $name]), $e->getCode(), $e);
-            }
-        }
-
-        // Export configuration collections.
-        foreach ($source->getAllCollectionNames() as $collection) {
-            $source = $source->createCollection($collection);
-            $destination = $destination->createCollection($collection);
-            foreach ($source->listAll() as $name) {
-                $destination->write($name, $source->read($name));
-            }
-        }
-    }
-
-    /**
-     * Get diff between two config sets.
-     */
-    public static function getDiff(StorageInterface $destination_storage, StorageInterface $source_storage, OutputInterface $output): string
-    {
-        // Copy active storage to a temporary directory.
-        $temp_destination_dir = drush_tempdir();
-        $temp_destination_storage = new FileStorage($temp_destination_dir);
-        self::copyConfig($destination_storage, $temp_destination_storage);
-
-        // Copy source storage to a temporary directory as it could be
-        // modified by the partial option or by decorated sync storages.
-        $temp_source_dir = drush_tempdir();
-        $temp_source_storage = new FileStorage($temp_source_dir);
-        self::copyConfig($source_storage, $temp_source_storage);
-
-        $prefix = ['diff'];
-        if (self::programExists('git')) {
-            $prefix = ['git', 'diff', '--no-index', '--exit-code'];
-            if ($output->isDecorated()) {
-                $prefix[] = '--color=always';
-            }
-        }
-        $args = array_merge($prefix, ['-u', $temp_destination_dir, $temp_source_dir]);
-        $process = Drush::process($args);
-        $process->run();
-        // An exit code of 1 merely indicates that there is a diff.
-        if ($process->getExitCode() >= 2) {
-            throw new RuntimeException($process->getExitCodeText());
-        }
-        return $process->getOutput();
     }
 }
