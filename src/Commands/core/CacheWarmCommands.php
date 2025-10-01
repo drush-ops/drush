3855f6172a20af8d7c3979957d2e3655e58f71cd
Moshe Weitzman <weitzman@tejasa.com>
2025-10-01 10:24:00 -0400

    Console - cache:warm (#6366)
    * Console - cache:warm
    
    * PHPCS

--- src/Commands/core/CacheWarmCommands.php
+++ src/Commands/core/CacheWarmCommands.php
@@ -6,18 +6,35 @@ namespace Drush\Commands\core;
 
 use Drupal\Core\PreWarm\CachePreWarmerInterface;
 use Drush\Attributes as CLI;
-use Drush\Commands\DrushCommands;
+use Drush\Style\DrushStyle;
 use Psr\Container\ContainerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
-final class CacheWarmCommands extends DrushCommands
+#[AsCommand(
+    name: self::NAME,
+    description: 'Pre-warm all caches.',
+    aliases: ['cw', 'cache-warm']
+)]
+#[CLI\Version(version: '13.5')]
+final class CacheWarmCommands extends Command
 {
-    const WARM = 'cache:warm';
+    const NAME = 'cache:warm';
 
     public function __construct(
-        private CachePreWarmerInterface $warmer,
+        private readonly CachePreWarmerInterface $warmer,
     ) {
         parent::__construct();
     }
+
+    protected function configure()
+    {
+        $this
+            ->setHelp('Requires Drupal 11.2+. See https://www.drupal.org/node/3386853');
+    }
+
 
     public static function create(ContainerInterface $container)
     {
@@ -27,16 +44,10 @@ final class CacheWarmCommands extends DrushCommands
         // Do nothing. Command never gets added to the Application.
     }
 
-    /**
-     * Pre-warm all caches.
-     *
-     * Requires Drupal 11.2+. See https://www.drupal.org/node/3386853
-     */
-    #[CLI\Command(name: self::WARM, aliases: ['warm', 'cache-warm'])]
-    #[CLI\Version(version: '13.5')]
-    public function warm(): void
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $this->warmer->preWarmAllCaches();
-        $this->logger()->success(dt('Warmed caches.'));
+        (new DrushStyle($input, $output))->success(dt('Warmed caches.'));
+        return self::SUCCESS;
     }
 }
