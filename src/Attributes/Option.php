f81b5d2d9a024c53f2cba3849e4e5653d70c07d7
Moshe Weitzman <weitzman@tejasa.com>
2025-10-30 08:56:14 -0400

    Tweaks

--- src/Attributes/Option.php
+++ src/Attributes/Option.php
@@ -7,7 +7,7 @@ namespace Drush\Attributes;
 use Attribute;
 use JetBrains\PhpStorm\Deprecated;
 
-#[Deprecated('Use #[AsCommand] instead. See https://www.drush.org/latest/commands/')]
+#[Deprecated('Use a native Console command and its configure() method instead. See https://www.drush.org/latest/commands/')]
 #[Attribute(Attribute::TARGET_METHOD | \Attribute::IS_REPEATABLE)]
 class Option extends \Consolidation\AnnotatedCommand\Attributes\Option
 {
