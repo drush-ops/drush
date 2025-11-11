e9454974e0f2f0fdc81e82ee773220f0278959e4
Moshe Weitzman <weitzman@tejasa.com>
2025-11-10 22:45:01 -0500

    Add test

--- src/Commands/core/WorkspacesCommands.php
+++ src/Commands/core/WorkspacesCommands.php
@@ -15,49 +15,48 @@ final class WorkspacesCommands extends DrushCommands
 
     const PUBLISH = 'workspaces:publish';
 
-    private ?WorkspaceOperationFactory $workspacesOperationFactory;
+    protected ?WorkspaceOperationFactory $workspacesOperationFactory = null;
 
     /**
-   * Constructs a WorkspacesCommands object.
-   */
+     * Constructs a WorkspacesCommands object.
+     */
     public function __construct(
         private readonly EntityTypeManagerInterface $entityTypeManager,
     ) {
         parent::__construct();
-       /**
-       * Since we use Autowire and our service is in a non-required module, we
-       *     - Get the container ourselves.
-       *     - Our service variable can be null.
-       */
+
         $container = Drush::getContainer();
-        if ($container->has('plugin.manager.migration')) {
+        if ($container->has('workspaces.operation_factory')) {
             $this->workspacesOperationFactory = $container->get('workspaces.operation_factory');
         }
     }
 
-  /**
+    /**
    * Publish a workspace.
    */
     #[CLI\Command(name: self::PUBLISH)]
     #[CLI\Argument(name: 'id', description: 'The workspace to publish.')]
     #[CLI\Usage(name: 'workspaces:publish stage', description: 'Publish the stage workspace')]
     #[CLI\ValidateModulesEnabled(modules: ['workspaces'])]
-    public function commandName($id)
+    public function publish($id)
     {
-
+        /** @var \Drupal\workspaces\Entity\Workspace $workspace */
         $workspace = $this->entityTypeManager->getStorage('workspace')->load($id);
+        if (!$workspace) {
+            throw new \Exception(dt('Workspace @id not found.', ['@id' => $id]));
+        }
 
-        $workspace_publisher = $this->factory->getPublisher($workspace);
+        $workspace_publisher = $this->workspacesOperationFactory->getPublisher($workspace);
 
         $args = [
-        '%source_label' => $workspace->label(),
-        '%target_label' => $workspace_publisher->getTargetLabel(),
+            '%source_label' => $workspace->label(),
+            '%target_label' => $workspace_publisher->getTargetLabel(),
         ];
 
       // Does this workspace have any content to publish?
         $diff = $workspace_publisher->getDifferringRevisionIdsOnSource();
         if (empty($diff)) {
-            $this->io()->warning(dt('There are no changes that can be published from %source_label to %target_label.', $args));
+            $this->logger()->success(dt('There are no changes that can be published from %source_label to %target_label.', $args));
             return;
         }
 
--- tests/integration/resources/workspaces.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$workspace = \Drupal\workspaces\Entity\Workspace::load('stage');
-Drupal::service('workspaces.manager')->setActiveWorkspace($workspace);
