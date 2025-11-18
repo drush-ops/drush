076a0bd0d21e4a710e5c0f714cc321abede7737e
Moshe Weitzman <weitzman@tejasa.com>
2025-11-18 11:18:36 -0500

    Couple more

--- src/Application.php
+++ src/Application.php
@@ -135,7 +135,7 @@ class Application extends SymfonyApplication implements LoggerAwareInterface, Co
     /**
      * Return the framework uri selected by the user.
      */
-    public function getUri()
+    public function getUri(): string|false
     {
         if (!$this->bootstrapManager) {
             return 'default';
@@ -204,7 +204,7 @@ class Application extends SymfonyApplication implements LoggerAwareInterface, Co
     /**
      * Look up a command. Bootstrap further if necessary.
      */
-    protected function bootstrapAndFind($name)
+    protected function bootstrapAndFind($name): \Symfony\Component\Console\Command\Command|\Drush\Command\RemoteCommandProxy
     {
         try {
             return parent::find($name);
