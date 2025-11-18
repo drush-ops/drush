7ad3d52d8975b909ffc12e854d551c4b70498cca
Fran Garcia-Linares <fjgarlin@gmail.com>
2025-11-18 12:46:13 +0000

    Add new logic for themes.

--- src/Commands/pm/ThemeInstallCommand.php
+++ src/Commands/pm/ThemeInstallCommand.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace Drush\Commands\pm;
 
+use Drupal\Core\Extension\ModuleInstallerInterface;
 use Drupal\Core\Extension\ThemeInstallerInterface;
 use Drush\Commands\AutowireTrait;
+use Drush\Exceptions\UserAbortException;
 use Drush\Style\DrushStyle;
 use Drush\Utils\StringUtils;
 use Symfony\Component\Console\Attribute\AsCommand;
@@ -22,11 +24,13 @@ use Symfony\Component\Console\Output\OutputInterface;
 final class ThemeInstallCommand extends Command
 {
     use AutowireTrait;
+    use PmTrait;
 
     const string NAME = 'theme:install';
 
     public function __construct(
         private readonly ThemeInstallerInterface $themeInstaller,
+        private readonly ModuleInstallerInterface $moduleInstaller,
     ) {
         parent::__construct();
     }
@@ -44,7 +48,23 @@ final class ThemeInstallCommand extends Command
         $themes = $input->getArgument('themes');
         $themes = StringUtils::csvToArray($themes);
 
-        if (!$this->themeInstaller->install($themes)) {
+        $todo = $this->addInstallDependencies($themes, 'themes');
+        $todo_str = ['!list' => implode(', ', $todo)];
+        if (!empty($todo)) {
+            $this->output()->writeln(dt('The following module(s) and themes(s) will be installed: !list', $todo_str));
+            if (!$this->io()->confirm(dt('Do you want to continue?'))) {
+                throw new UserAbortException();
+            }
+
+            $modules = array_diff(array_values($todo), array_values($themes));
+            if (!empty($modules)) {
+                if (!$this->moduleInstaller->install($modules, true)) {
+                    throw new \Exception('Unable to install modules.');
+                }
+            }
+        }
+
+        if (!$this->themeInstaller->install($themes, true)) {
             throw new \Exception('Unable to install themes.');
         }
 
