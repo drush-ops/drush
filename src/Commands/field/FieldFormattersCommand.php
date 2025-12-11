9e9966060c69b8597656fb67aab30c1e215824fe
Dieter Holvoet <dieter.holvoet@gmail.com>
2025-12-11 17:16:47 +0100

    Convert field:types, field:widgets and field:formatters

---
+++ src/Commands/field/FieldFormattersCommand.php
@@ -0,0 +1,132 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drush\Commands\field;
+
+use Consolidation\OutputFormatters\FormatterManager;
+use Consolidation\OutputFormatters\Options\FormatterOptions;
+use Consolidation\OutputFormatters\StructuredData\RowsOfFields;
+use Drupal\Core\Field\FieldTypePluginManagerInterface;
+use Drupal\Core\Field\FormatterPluginManager;
+use Drush\Attributes as CLI;
+use Drush\Commands\AutowireTrait;
+use Drush\Commands\IoTrait;
+use Drush\Formatters\FormatterTrait;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Completion\CompletionInput;
+use Symfony\Component\Console\Completion\CompletionSuggestions;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\Yaml\Yaml;
+
+#[AsCommand(
+    name: self::NAME,
+    description: 'Lists field formatters.',
+)]
+#[CLI\DefaultFields(fields: [
+    'id',
+    'label',
+    'field_types',
+])]
+#[CLI\FieldLabels(labels: [
+    'id' => 'ID',
+    'label' => 'Label',
+    'default_settings' => 'Default Settings',
+    'field_types' => 'Field types',
+    'settings' => 'Settings',
+    'class' => 'Class',
+    'provider' => 'Provider',
+])]
+#[CLI\FilterDefaultField(field: 'id')]
+#[CLI\Formatter(returnType: RowsOfFields::class, defaultFormatter: 'table')]
+final class FieldFormattersCommand extends Command
+{
+    use AutowireTrait;
+    use FormatterTrait;
+    use IoTrait;
+
+    const string NAME = 'field:formatters';
+
+    public function __construct(
+        private readonly FieldTypePluginManagerInterface $typePluginManager,
+        // @todo These attributes should not be needed but services aren't found otherwise.
+        #[Autowire(service: 'plugin.manager.field.formatter')]
+        private readonly FormatterPluginManager $formatterPluginManager,
+        private readonly FormatterManager $formatterManager
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addOption(name: 'field-type', mode: InputOption::VALUE_REQUIRED, description: 'Applicable field type')
+            ->addUsage('field:formatters --field-type=entity_reference');
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $this->setIo($input, $output);
+
+        $processor = static fn (array $definition): array => [
+            'id' => $definition['id'],
+            'label' => (string) $definition['label'],
+            'default_settings' => $definition['class']::defaultSettings(),
+            'field_types' => $definition['field_types'],
+            'class' => $definition['class'],
+            'provider' => $definition['provider'],
+        ];
+
+        $definitions = \array_map($processor, $this->formatterPluginManager->getDefinitions());
+        $definitions = \array_values($definitions);
+
+        if ($fieldType = $input->getOption('field-type')) {
+            $definitions = $this->filterByFieldType($definitions, $fieldType);
+        }
+
+        $result = new RowsOfFields($definitions);
+        $result->addRendererFunction([$this, 'renderArray']);
+        $this->writeFormattedOutput($input, $output, $result);
+
+        return Command::SUCCESS;
+    }
+
+    public function complete(CompletionInput $input, CompletionSuggestions $suggestions): void
+    {
+        if ($input->getCompletionType() === CompletionInput::TYPE_OPTION_VALUE) {
+            if ($input->getCompletionName() === 'field-type') {
+                $fieldTypes = $this->typePluginManager->getDefinitions();
+                $suggestions->suggestValues(array_keys($fieldTypes));
+            }
+        }
+    }
+
+    public function renderArray($key, $value, FormatterOptions $options)
+    {
+        if (is_array($value)) {
+            if (array_is_list($value)) {
+                return implode("\n", $value);
+            }
+
+            return Yaml::dump($value);
+        }
+
+        return $value;
+    }
+
+    /**
+     * Filters definitions by applicable field types.
+     */
+    private function filterByFieldType(array $definitions, string $search): array
+    {
+        $match = static fn (string $field_type): bool => \str_contains($field_type, $search);
+        $totalMatches = static fn (array $field_types): int => \count(\array_filter($field_types, $match));
+        $hasMatches = static fn (array $definition): bool => $totalMatches($definition['field_types']) > 0;
+
+        return \array_filter($definitions, $hasMatches);
+    }
+}
---
+++ src/Commands/field/FieldTypesCommand.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drush\Commands\field;
+
+use Consolidation\OutputFormatters\FormatterManager;
+use Consolidation\OutputFormatters\Options\FormatterOptions;
+use Consolidation\OutputFormatters\StructuredData\RowsOfFields;
+use Drupal\Core\Field\BaseFieldDefinition;
+use Drupal\Core\Field\FieldTypePluginManagerInterface;
+use Drush\Attributes as CLI;
+use Drush\Commands\AutowireTrait;
+use Drush\Commands\IoTrait;
+use Drush\Formatters\FormatterTrait;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Yaml\Yaml;
+
+#[AsCommand(
+    name: self::NAME,
+    description: 'Lists field types.',
+)]
+#[CLI\DefaultFields(fields: [
+    'id',
+    'label',
+])]
+#[CLI\FieldLabels(labels: [
+    'id' => 'ID',
+    'label' => 'Label',
+    'default_widget' => 'Default Widget',
+    'default_formatter' => 'Default Formatter',
+    'settings' => 'Settings',
+])]
+#[CLI\FilterDefaultField(field: 'id')]
+#[CLI\Formatter(returnType: RowsOfFields::class, defaultFormatter: 'table')]
+final class FieldTypesCommand extends Command
+{
+    use AutowireTrait;
+    use FormatterTrait;
+    use IoTrait;
+
+    const string NAME = 'field:types';
+
+    public function __construct(
+        private readonly FieldTypePluginManagerInterface $typePluginManager,
+        private readonly FormatterManager $formatterManager
+    ) {
+        parent::__construct();
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $this->setIo($input, $output);
+
+        $processor = static fn (array $definition): array => [
+          'id' => $definition['id'],
+          'label' => (string) $definition['label'],
+          'default_widget' => $definition['default_widget'] ?? null,
+          'default_formatter' => $definition['default_formatter'] ?? null,
+          'settings' => BaseFieldDefinition::create($definition['id'])->getSettings(),
+        ];
+
+        $definitions = \array_map($processor, $this->typePluginManager->getDefinitions());
+        $definitions = \array_values($definitions);
+
+        $result = new RowsOfFields($definitions);
+        $result->addRendererFunction([$this, 'renderArray']);
+        $this->writeFormattedOutput($input, $output, $result);
+
+        return Command::SUCCESS;
+    }
+
+    public function renderArray($key, $value, FormatterOptions $options)
+    {
+        if (is_array($value)) {
+            if (array_is_list($value)) {
+                return implode("\n", $value);
+            }
+
+            return Yaml::dump($value);
+        }
+
+        return $value;
+    }
+}
