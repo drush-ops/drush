386e568a5fa76ed2f89084922661f77ad9a91260
Dieter Holvoet <dieter.holvoet@gmail.com>
2025-12-16 14:40:24 +0100

    Fix test failure

--- tests/integration/FieldDefinitionTest.php
+++ tests/integration/FieldDefinitionTest.php
@@ -14,13 +14,13 @@ class FieldDefinitionTest extends UnishIntegrationTestCase
 {
     public function testFieldDefinition(): void
     {
-        $this->drush(FieldTypesCommand::NAME, [], ['format' => 'json']);
+        $this->drush(FieldTypesCommand::NAME, [], ['format' => 'json', 'fields' => 'id,label,settings']);
         $json = $this->getOutputFromJSON();
         $this->assertArrayHasKey('boolean', $json);
         $this->assertEquals('On', $json['boolean']['settings']['on_label']);
 
         $this->drush(PmInstallCommand::NAME, ['file'], ['yes' => true]);
-        $this->drush(FieldWidgetsCommand::NAME, [], ['format' => 'json']);
+        $this->drush(FieldWidgetsCommand::NAME, [], ['format' => 'json', 'fields' => 'id,label,default_settings']);
         $json = $this->getOutputFromJSON();
         $this->assertArrayHasKey('file_generic', $json);
         $this->assertEquals('throbber', $json['file_generic']['default_settings']['progress_indicator']);
@@ -31,7 +31,7 @@ class FieldDefinitionTest extends UnishIntegrationTestCase
         $this->assertArrayHasKey('file_generic', $json);
         $this->assertArrayNotHasKey('number', $json);
 
-        $this->drush(FieldFormattersCommand::NAME, [], ['format' => 'json']);
+        $this->drush(FieldFormattersCommand::NAME, [], ['format' => 'json', 'fields' => 'id,label,default_settings']);
         $json = $this->getOutputFromJSON();
         $this->assertArrayHasKey('file_video', $json);
         $this->assertFalse($json['file_video']['default_settings']['muted']);
