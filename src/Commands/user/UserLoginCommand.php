5309cd9b6751b0120f0de042443ce2a8ba2c3c58
Moshe Weitzman <weitzman@tejasa.com>
2026-01-31 16:57:31 -0500

    Simplify user:login with less options and dependencies (#6501)
    More DI

--- src/Commands/user/UserLoginCommand.php
+++ src/Commands/user/UserLoginCommand.php
@@ -4,16 +4,12 @@ declare(strict_types=1);
 
 namespace Drush\Commands\user;
 
-use Consolidation\SiteAlias\SiteAliasManagerInterface;
+use Drupal\Component\Datetime\TimeInterface;
+use Drupal\Core\Language\LanguageManagerInterface;
 use Drupal\Core\Url;
 use Drupal\user\Entity\User;
-use Drush\Attributes as CLI;
-use Drush\Boot\BootstrapManager;
-use Drush\Boot\DrupalBootLevels;
 use Drush\Commands\AutowireTrait;
-use Drush\Drush;
 use Drush\Exec\ExecTrait;
-use Drush\SiteAlias\ProcessManager;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -26,8 +22,6 @@ use Symfony\Component\Console\Output\OutputInterface;
     description: 'Display a one time login link for user ID 1, or another user.',
     aliases: ['uli', 'user-login'],
 )]
-#[CLI\Bootstrap(DrupalBootLevels::NONE)]
-#[CLI\HandleRemoteCommands]
 final class UserLoginCommand extends Command
 {
     use AutowireTrait;
@@ -36,9 +30,8 @@ final class UserLoginCommand extends Command
     public const string NAME = 'user:login';
 
     public function __construct(
-        private readonly BootstrapManager $bootstrapManager,
-        protected readonly ProcessManager $processManager,
-        private readonly SiteAliasManagerInterface $siteAliasManager
+        protected readonly LanguageManagerInterface $languageManager,
+        protected readonly TimeInterface $time,
     ) {
         parent::__construct();
     }
@@ -50,8 +43,6 @@ final class UserLoginCommand extends Command
             ->addOption('name', null, InputOption::VALUE_REQUIRED, 'A user name to log in as.')
             ->addOption('uid', null, InputOption::VALUE_REQUIRED, 'A user ID to log in as.')
             ->addOption('mail', null, InputOption::VALUE_REQUIRED, 'A user email to log in as.')
-            ->addOption('browser', null, InputOption::VALUE_NEGATABLE, 'Open the URL in the default browser. Use --no-browser to avoid opening a browser.', true)
-            ->addOption('redirect-port', null, InputOption::VALUE_REQUIRED, 'A custom port for redirecting to (e.g., when running within a Vagrant environment)')
             ->addUsage('user:login --name=ryan node/add/blog')
             ->addUsage('user:login --uid=123')
             ->addUsage('user:login --mail=foo@bar.com')
@@ -61,62 +52,43 @@ final class UserLoginCommand extends Command
     public function execute(InputInterface $input, OutputInterface $output): int
     {
         $path = $input->getArgument('path');
-        // Redispatch if called against a remote-host so a browser is started on the *local* machine.
-        $aliasRecord = $this->siteAliasManager->getSelf();
-        if ($this->processManager->hasTransport($aliasRecord)) {
-            $process = $this->processManager->drush($aliasRecord, self::NAME, [$path], Drush::redispatchOptions());
-            $process->mustRun();
-            $link = $process->getOutput();
-        } else {
-            if (!$this->bootstrapManager->doBootstrap(DrupalBootLevels::FULL)) {
-                throw new \Exception('Unable to bootstrap Drupal.');
-            }
-
-            $account = null;
-            if ($input->getOption('name') && !$account = user_load_by_name($input->getOption('name'))) {
-                throw new \Exception(sprintf('Unable to load user by name: %s', $input->getOption('name')));
-            }
-
-            if ($input->getOption('uid') && !$account = User::load($input->getOption('uid'))) {
-                throw new \Exception(sprintf('Unable to load user by uid: %s', $input->getOption('uid')));
-            }
-
-            if ($input->getOption('mail') && !$account = user_load_by_mail($input->getOption('mail'))) {
-                throw new \Exception(sprintf('Unable to load user by mail: %s', $input->getOption('mail')));
-            }
-
-            if (empty($account)) {
-                $account = User::load(1);
-            }
-
-            if ($account->isBlocked()) {
-                throw new \InvalidArgumentException('Account %s is blocked and thus cannot login. The user:unblock command may be helpful.', $account->getAccountName());
-            }
-
-            // Can't inject dependency because this command instantiates without a bootstrap.
-            $timestamp = \Drupal::time()->getRequestTime();
-            $link = Url::fromRoute(
-                'user.reset.login',
-                [
-                  'uid' => $account->id(),
-                  'timestamp' => $timestamp,
-                  'hash' => user_pass_rehash($account, $timestamp),
-                ],
-                [
-                  'absolute' => true,
-                  'query' => $path ? ['destination' => $path] : [],
-                  // Can't inject dependency because this command instantiates without a bootstrap.
-                  'language' => \Drupal::languageManager()->getLanguage($account->getPreferredLangcode()),
-                ]
-            )->toString();
+        $account = null;
+        if ($input->getOption('name') && !$account = user_load_by_name($input->getOption('name'))) {
+            throw new \Exception(sprintf('Unable to load user by name: %s', $input->getOption('name')));
         }
-        $port = $input->getOption('redirect-port');
-//        $browser = $input->getOption('browser');
-//        if (is_null($browser) && !str_contains(strval($input), '--browser')) {
-//            // WHen user doesn't specify, we have always represented that with true.
-//            $browser = true;
-//        }
-        $this->startBrowser($link, 0, $port, $input->getOption('browser'));
+
+        if ($input->getOption('uid') && !$account = User::load($input->getOption('uid'))) {
+            throw new \Exception(sprintf('Unable to load user by uid: %s', $input->getOption('uid')));
+        }
+
+        if ($input->getOption('mail') && !$account = user_load_by_mail($input->getOption('mail'))) {
+            throw new \Exception(sprintf('Unable to load user by mail: %s', $input->getOption('mail')));
+        }
+
+        if (empty($account)) {
+            $account = User::load(1);
+        }
+
+        if ($account->isBlocked()) {
+            throw new \InvalidArgumentException('Account %s is blocked and thus cannot login. The user:unblock command may be helpful.', $account->getAccountName());
+        }
+
+        // Can't inject dependency because this command instantiates without a bootstrap.
+        $timestamp = $this->time->getRequestTime();
+        $link = Url::fromRoute(
+            'user.reset.login',
+            [
+              'uid' => $account->id(),
+              'timestamp' => $timestamp,
+              'hash' => user_pass_rehash($account, $timestamp),
+            ],
+            [
+              'absolute' => true,
+              'query' => $path ? ['destination' => $path] : [],
+              // Can't inject dependency because this command instantiates without a bootstrap.
+              'language' => $this->languageManager->getLanguage($account->getPreferredLangcode()),
+            ]
+        )->toString();
         $output->writeln($link);
         return self::SUCCESS;
     }
