29936977bf1db9fc5ff3d1821fb30349345339c9
Moshe Weitzman <weitzman@tejasa.com>
2026-07-07 10:47:12 -0400

    Give up on bash completion in DDEV for now

--- .ddev/homeadditions/.bashrc.d/drush_completion.sh
+++ .ddev/homeadditions/.bashrc.d/drush_completion.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
-#@todo make this not fail on Symfony 5-.
 # Make ./drush available on PATH.
 export PATH=/var/www/html:$PATH
+#@todo make this not fail on Symfony 5-.
 # Suppress error output because this command is not present on Symfony 4.
-eval "$(drush completion bash 2>/dev/null)"
+# eval "$(drush completion bash 2>/dev/null)"
